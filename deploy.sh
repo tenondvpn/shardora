fcdfe5605684faf9553ed3bbde55f17982c5e31b
tenondvpn <jhsx5679@gmail.com>
2023-03-29 18:16:06 +0800

    update cmake

--- deploy.sh
+++ deploy.sh
@@ -32,32 +32,32 @@ mkdir -p /root/zjnodes/r5/log
 mkdir -p /root/zjnodes/r6/log
 mkdir -p /root/zjnodes/r7/log
 
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/zjchain
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s1
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s2
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s3
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s4
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s5
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s6
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s7
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s8
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s9
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s10
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s11
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s12
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s13
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s14
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s15
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s16
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s17
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/s18
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/r1
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/r2
-cp -rf ./cbuild_Debug/zjchain /root/zjnodes/r3
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/r4
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/r5
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/r6
-#cp -rf ./cbuild_Debug/zjchain /root/zjnodes/r7
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/zjchain
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/s1
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/s2
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/s3
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/s4
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s5
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s6
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s7
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s8
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s9
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s10
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s11
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s12
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s13
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s14
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s15
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s16
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s17
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/s18
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/r1
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/r2
+cp -rf ./cbuild_Release/zjchain /root/zjnodes/r3
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/r4
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/r5
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/r6
+#cp -rf ./cbuild_Release/zjchain /root/zjnodes/r7
 
 cd /root/zjnodes/zjchain && ./zjchain -U -1 031d29587f946b7e57533725856e3b2fc840ac8395311fea149642334629cd5757:127.0.0.1:1,03a6f3b7a4a3b546d515bfa643fc4153b86464543a13ab5dd05ce6f095efb98d87:127.0.0.1:2,031e886027cdf3e7c58b9e47e8aac3fe67c393a155d79a96a0572dd2163b4186f0:127.0.0.1:2 -2 0315a968643f2ada9fd24f0ca92ae5e57d05226cfe7c58d959e510b27628c1cac0:127.0.0.1:3,030d62d31adf3ccbc6283727e2f4493a9228ef80f113504518c7cae46931115138:127.0.0.1:4,028aa5aec8f1cbcd995ffb0105b9c59fd76f29eaffe55521aad4f7a54e78f01e58:127.0.0.1:5
 cd /root/zjnodes/zjchain && ./zjchain -S
