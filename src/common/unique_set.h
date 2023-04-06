47c45e501050759f851f2b10d8fd6fd758ac33de
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-04-06 11:55:16 +0800

    support fixed queue with unitest

--- src/common/unique_set.h
+++ src/common/unique_set.h
@@ -43,6 +43,74 @@ private:
     common::FixedQueue<T, EachBucketSize>* buckets_ = nullptr;
 };
 
+template<uint32_t BucketSize, uint8_t EachBucketSize>
+class StringUniqueSet {
+public:
+    explicit StringUniqueSet() {
+        buckets_ = new common::FixedQueue<std::string*, EachBucketSize>[BucketSize];
+    }
+
+    ~UniqueSet() {
+        delete[] buckets_;
+    }
+
+    bool add(const std::string& key) {
+        uint32_t idx = Hash32(key) % BucketSize;
+        if (Exists(idx, key)) {
+            return false;
+        }
+
+        if (buckets_[idx].IsFull()) {
+            auto* item = buckets_[idx].Front();
+            buckets_[idx].Dequeue();
+            delete item;
+        }
+
+        buckets_[idx].Enqueue(new std::string(key));
+        return true;
+    }
+
+    bool exists(const std::string& key) {
+        uint32_t idx = Hash32(key) % BucketSize;
+        return Exists(idx, key);
+    }
+
+private:
+    bool Exists(uint32_t idx, const std::string& key) {
+        if (!buckets_[idx].IsEmpty()) {
+            if (buckets_[idx].rear_ == buckets_[idx].front_) {
+                for (uint8_t i = 0; i < EachBucketSize; ++i) {
+                    if (*buckets_[idx].data_[i] == key) {
+                        return true;
+                    }
+                }
+            } else if (buckets_[idx].rear_ > buckets_[idx].front_) {
+                for (uint8_t i = buckets_[idx].front_; i < buckets_[idx].rear_; ++i) {
+                    if (*buckets_[idx].data_[i] == key) {
+                        return true;
+                    }
+                }
+            } else {
+                for (uint8_t i = buckets_[idx].front_; i < EachBucketSize; ++i) {
+                    if (*buckets_[idx].data_[i] == key) {
+                        return true;
+                    }
+                }
+
+                for (uint8_t i = 0; i < buckets_[idx].rear_; ++i) {
+                    if (*buckets_[idx].data_[i] == key) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    common::FixedQueue<std::string*, EachBucketSize>* buckets_ = nullptr;
+};
+
 }  // namespace common
 
 }  // namespace zjchain
