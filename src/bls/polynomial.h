80c84ed9ff8699e831cad832f24b8dcea4d34222
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-04-13 11:16:36 +0800

    support fast polinomial and verification

--- src/bls/polynomial.h
+++ src/bls/polynomial.h
@@ -37,9 +37,10 @@ public:
             std::string file = std::string("saved_verify_one_") + std::to_string(thread_idx);
             FILE* saved_verify_fd = fopen(file.c_str(), "a+");
             for (int32_t idx = b; idx < e; ++idx) {
+                libff::alt_bn128_G2 value = libff::alt_bn128_G2::zero();
                 bls::protobuf::BlsVerifyValue verify_val;
                 for (size_t tidx = 1; tidx < t; ++tidx) {
-                    auto value = power(libff::alt_bn128_Fr(idx + 1), tidx) * libff::alt_bn128_G2::one();
+                    value = value + power(libff::alt_bn128_Fr(idx + 1), tidx) * libff::alt_bn128_G2::one();
                     bls::protobuf::VerifyVecItem& verify_item = *verify_val.add_verify_vec();
                     verify_item.set_x_c0(common::Encode::HexDecode(
                         libBLS::ThresholdUtils::fieldElementToString(value.X.c0)));
