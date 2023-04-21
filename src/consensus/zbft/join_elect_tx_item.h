a8f4383a46606008a51ebbc6d81a6381010f913f
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-04-21 09:57:27 +0800

    add join elect tx item

--- src/consensus/zbft/join_elect_tx_item.h
+++ src/consensus/zbft/join_elect_tx_item.h
@@ -11,13 +11,22 @@ namespace consensus {
 class JoinElectTxItem : public TxItemBase {
 public:
     JoinElectTxItem(
-        const transport::MessagePtr& msg,
-        std::shared_ptr<block::AccountManager>& account_mgr,
-        std::shared_ptr<security::Security>& sec_ptr)
-        : TxItemBase(msg, account_mgr, sec_ptr) {}
+            const transport::MessagePtr& msg,
+            std::shared_ptr<block::AccountManager>& account_mgr,
+            std::shared_ptr<security::Security>& sec_ptr)
+            : TxItemBase(msg, account_mgr, sec_ptr) {
+    }
+
     virtual ~JoinElectTxItem() {}
+    virtual int HandleTx(
+        uint8_t thread_idx,
+        const block::protobuf::Block& block,
+        std::shared_ptr<db::DbWriteBatch>& db_batch,
+        std::unordered_map<std::string, int64_t>& acc_balance_map,
+        block::protobuf::BlockTx& block_tx);
 
 private:
+
     DISALLOW_COPY_AND_ASSIGN(JoinElectTxItem);
 };
 
