931d86a557e49796fad293e75184f4962465c45b
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-04-23 09:15:17 +0800

    support waiting nodes send join elect transaction

--- src/pools/tests/test_tx_pool_manager.cc
+++ src/pools/tests/test_tx_pool_manager.cc
@@ -27,7 +27,36 @@ static std::shared_ptr<protos::PrefixDb> prefix_db = nullptr;
 
 class TestTxPoolManager : public testing::Test {
 public:
-    static void SetUpTestCase() {    
+    static void WriteDefaultLogConf(
+        const std::string& log_conf_path,
+        const std::string& log_path) {
+        FILE* file = NULL;
+        file = fopen(log_conf_path.c_str(), "w");
+        if (file == NULL) {
+            return;
+        }
+        std::string log_str = ("# log4cpp.properties\n"
+            "log4cpp.rootCategory = DEBUG\n"
+            "log4cpp.category.sub1 = DEBUG, programLog\n"
+            "log4cpp.appender.rootAppender = ConsoleAppender\n"
+            "log4cpp.appender.rootAppender.layout = PatternLayout\n"
+            "log4cpp.appender.rootAppender.layout.ConversionPattern = %d [%p] %m%n\n"
+            "log4cpp.appender.programLog = RollingFileAppender\n"
+            "log4cpp.appender.programLog.fileName = ") + log_path + "\n" +
+            std::string("log4cpp.appender.programLog.maxFileSize = 1073741824\n"
+                "log4cpp.appender.programLog.maxBackupIndex = 1\n"
+                "log4cpp.appender.programLog.layout = PatternLayout\n"
+                "log4cpp.appender.programLog.layout.ConversionPattern = %d [%p] %m%n\n");
+        fwrite(log_str.c_str(), log_str.size(), 1, file);
+        fclose(file);
+    }
+
+    static void SetUpTestCase() {
+        std::string config_path_ = "./";
+        std::string log_conf_path = config_path_ + "/log4cpp.properties";
+        std::string log_path = config_path_ + "/zjc.log";
+        WriteDefaultLogConf(log_conf_path, log_path);
+        log4cpp::PropertyConfigurator::configure(log_conf_path);
         common::GlobalInfo::Instance()->set_network_id(2);
         db_ptr = std::make_shared<db::Db>();
         system((std::string("rm -rf ./tx_pool_mgr").c_str()));
