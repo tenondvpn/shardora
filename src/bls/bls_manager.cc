251494689378aa1c6df4c3632ab709aedeea7295
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-04-25 15:36:18 +0800

    support select leader

--- src/bls/bls_manager.cc
+++ src/bls/bls_manager.cc
@@ -92,6 +92,8 @@ void BlsManager::OnNewElectBlock(
 
     elect_item->members = members;
     elect_members_[sharding_id] = elect_item;
+    BLS_DEBUG("sharding: %u, success add new bls dkg, elect_height: %lu",
+        sharding_id, elect_height);
     if (sharding_id != common::GlobalInfo::Instance()->network_id()) {
         return;
     }
