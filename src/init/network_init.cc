c5d8b99e8279bfa4a0bb10c037eae3c8bb839202
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-04-28 11:41:33 +0800

    support broadcast elect block

--- src/init/network_init.cc
+++ src/init/network_init.cc
@@ -965,7 +965,7 @@ void NetworkInit::HandleElectionBlock(
 
     auto members = elect_mgr_->OnNewElectBlock(thread_idx, block->height(), elect_block, db_batch);
     if (members == nullptr) {
-        ZJC_FATAL("elect manager handle elect block failed!");
+        ZJC_ERROR("elect manager handle elect block failed!");
         return;
     }
 
