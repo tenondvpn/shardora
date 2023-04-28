c6e56a9b54446356a7dd5386e5fc62bb7b9e26c1
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-04-28 11:44:17 +0800

    support broadcast elect block

--- src/consensus/zbft/bft_manager.cc
+++ src/consensus/zbft/bft_manager.cc
@@ -410,9 +410,9 @@ void BftManager::HandleMessage(const transport::MessagePtr& msg_ptr) {
                 header.zbft().elect_height(),
                 elect_items_[elect_item_idx_]->elect_height,
                 elect_items_[(elect_item_idx_ + 1) % 2]->elect_height);
-            if (!header.zbft().sync_block()) {
-                assert(false);
-            }
+//             if (!header.zbft().sync_block()) {
+//                 assert(false);
+//             }
 
             return;
         }
