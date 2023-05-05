a9b934a155cc5169d786daa0ddd770e9b0d65c1a
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-05 15:57:16 +0800

    support broadcast elect block

--- src/consensus/zbft/bft_manager.cc
+++ src/consensus/zbft/bft_manager.cc
@@ -196,6 +196,9 @@ void BftManager::OnNewElectBlock(
             thread_set[j] = thread_item;  // ptr change, multi-thread safe
         }
 
+        thread_set[0]->member_ips[elect_item.local_node_member_index] = common::IpToUint32(
+            common::GlobalInfo::Instance()->config_local_ip());
+        thread_set[0]->valid_ip_count = 1;
         minimal_node_count_to_consensus_ = members->size() * 2 / 3;
         if (minimal_node_count_to_consensus_ + 1 < members->size()) {
             ++minimal_node_count_to_consensus_;
