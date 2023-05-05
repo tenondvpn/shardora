7e0991c016b9bd229d58dbfe7d98db8af9bdc594
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-05 09:37:30 +0800

    support broadcast elect block

--- src/pools/tx_pool.cc
+++ src/pools/tx_pool.cc
@@ -214,7 +214,7 @@ void TxPool::RemoveTx(const std::string& gid) {
     gid_map_.erase(giter);
     ZJC_DEBUG("remove tx success gid: %s, tx hash: %s",
         common::Encode::HexEncode(giter->second->gid).c_str(),
-        common::Encode::HexEncode(giter->second->tx_hash).c_str())
+        common::Encode::HexEncode(giter->second->tx_hash).c_str());
 }
 
 void TxPool::TxOver(const google::protobuf::RepeatedPtrField<block::protobuf::BlockTx>& tx_list) {
