9515cb6956f543c3f10a3294575971b48331dafc
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-06 16:15:44 +0800

    support broadcast elect block

--- src/pools/shard_statistic.cc
+++ src/pools/shard_statistic.cc
@@ -121,6 +121,7 @@ void ShardStatistic::HandleStatisticBlock(
                     height_idx < elect_statistic.heights().heights_size(); ++height_idx) {
                 if (elect_statistic.heights().heights(height_idx) > pool_consensus_heihgts_[height_idx]) {
                     pool_consensus_heihgts_[height_idx] = elect_statistic.heights().heights(height_idx);
+                    ZJC_DEBUG("pool: %u, set new consensus height: %lu", block.pool_index(), elect_statistic.heights().heights(height_idx))
                     while (pool_consensus_heihgts_[block.pool_index()] <= pool_max_heihgts_[block.pool_index()]) {
                         auto iter = added_heights_[block.pool_index()].find(
                             pool_consensus_heihgts_[block.pool_index()] + 1);
