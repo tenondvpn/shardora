39c27c31501b417b001595c3b88b3821a289e58c
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-08 11:43:10 +0800

    support broadcast elect block

--- src/consensus/zbft/bft_manager.cc
+++ src/consensus/zbft/bft_manager.cc
@@ -511,8 +511,6 @@ void BftManager::HandleSyncConsensusBlock(
                     req_bft_msg.block().pool_index(),
                     req_bft_msg.block().height(),
                     common::Encode::HexEncode(GetBlockHash(req_bft_msg.block())).c_str());
-
-                assert(false);
                 return;
             }
 
