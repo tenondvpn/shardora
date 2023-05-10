a8f2af917d8171a82297faff262eefc5db1f5304
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-10 13:59:33 +0800

    add test solidity library

--- src/consensus/zbft/contract_call.cc
+++ src/consensus/zbft/contract_call.cc
@@ -95,8 +95,9 @@ int ContractCall::HandleTx(
     int call_res = ContractExcute(msg_ptr->address_info, to_balance, zjc_host, block_tx, &res);
     if (call_res != kConsensusSuccess || res.status_code != EVMC_SUCCESS) {
         block_tx.set_status(EvmcStatusToZbftStatus(res.status_code));
-        ZJC_DEBUG("create contract failed, call_res: %d, evmc res: %d!",
-            call_res, res.status_code);
+        ZJC_DEBUG("call contract failed, call_res: %d, evmc res: %d, bytes: %s, input: %s!",
+            call_res, res.status_code, common::Encode::HexEncode(msg_ptr->address_info->bytes_code()).c_str(),
+            common::Encode::HexEncode(block_tx.contract_input()).c_str());
     }
 
     gas_used += block_tx.gas_limit() - res.gas_left;
