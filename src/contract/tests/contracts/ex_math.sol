e4714d718bf732bbc7e223a53280d5bceb89ef49
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-10 14:21:04 +0800

    add test solidity library

--- src/contract/tests/contracts/ex_math.sol
+++ src/contract/tests/contracts/ex_math.sol
@@ -13,31 +13,3 @@ contract ERC20 {
         return true;
     }
 }
-
-library SafeMath {
-
-    function mul(uint256 a, uint256 b) external pure returns (uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
-
-
-    function div(uint256 a, uint256 b) external pure returns (uint256) {
-        uint256 c = a / b;
-        return c;
-    }
-
-
-    function sub(uint256 a, uint256 b) external pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-
-    function add(uint256 a, uint256 b) external pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
