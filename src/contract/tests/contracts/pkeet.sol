f40ea5619da0065cd2ed9bd17c2baae43ad8f7cd
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-10 15:20:52 +0800

    add test solidity library

--- src/contract/tests/contracts/pkeet.sol
+++ src/contract/tests/contracts/pkeet.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+import "./ex_math_lib.sol";
+
 contract Pkeet {
+    using SafeMath for uint256;
+    uint256 test_res;
     mapping(bytes32 => address payable[]) public verifications;
     uint verification_valid_count;
     uint award_each;
@@ -14,6 +18,9 @@ contract Pkeet {
 
     function callAbe(bytes memory params) public payable {
         bytes32 v1_hash = ripemd160(params);
+        uint256 a = 2;
+        uint256 b = 3;
+        test_res = a.add(b)
         verifications[v1_hash].push(payable(msg.sender));
         if (verifications[v1_hash].length >= verification_valid_count) {
             for (uint i = 0; i < verification_valid_count; i++) {
