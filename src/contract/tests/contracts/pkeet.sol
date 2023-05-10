9b1a249fab76e792d4967e43885deac1a6218cee
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-10 16:32:58 +0800

    add test solidity library

--- src/contract/tests/contracts/pkeet.sol
+++ src/contract/tests/contracts/pkeet.sol
@@ -5,26 +5,23 @@ import "./ex_math_lib.sol";
 
 contract Pkeet {
     using SafeMath for uint256;
-    uint256 test_res;
+    mapping(address => uint256) balances;
+
     mapping(bytes32 => address payable[]) public verifications;
     uint verification_valid_count;
     uint award_each;
     address payable public creator;
     constructor(uint valid_count, uint award) payable {
-        uint256 a = 2;
         uint256 b = 3;
-        test_res = a.add(b);
-
         verification_valid_count = valid_count;
         award_each = award;
         creator = payable(msg.sender);
+        balances[msg.sender] = award.add(b);
     }
 
     function callAbe(bytes memory params) public payable {
         bytes32 v1_hash = ripemd160(params);
-        uint256 a = 2;
-        uint256 b = 3;
-        test_res = a.add(b);
+        balances[msg.sender] = balances[msg.sender].add(b);
         verifications[v1_hash].push(payable(msg.sender));
         if (verifications[v1_hash].length >= verification_valid_count) {
             for (uint i = 0; i < verification_valid_count; i++) {
