c670b7ccbc3ffa2685afd93ce4e9e07ec892fe5f
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-17 17:24:07 +0800

    add test solidity library

--- src/bls/bls_manager.cc
+++ src/bls/bls_manager.cc
@@ -173,6 +173,18 @@ int BlsManager::Sign(
         const libff::alt_bn128_G1& g1_hash,
         libff::alt_bn128_G1* bn_sign) {
     BlsSign::Sign(t, n, local_sec_key, g1_hash, bn_sign);
+
+    std::string sec_key = libBLS::ThresholdUtils::fieldElementToString(local_sec_key);
+    BLSPublicKeyShare pkey(local_sec_key, t, n);
+    std::shared_ptr< std::vector< std::string > > strs = pkey.toString();
+    BLS_DEBUG("sign t: %u, , n: %u, , pk: %s,%s,%s,%s, sign x: %s, sign y: %s, sign msg: %s,%s,%s",
+        t, n, strs->at(0).c_str(), strs->at(1).c_str(),
+        strs->at(2).c_str(), strs->at(3).c_str(), (*sign_x).c_str(), (*sign_y).c_str(),
+        libBLS::ThresholdUtils::fieldElementToString(g1_hash.X).c_str(),
+        libBLS::ThresholdUtils::fieldElementToString(g1_hash.Y).c_str(),
+        libBLS::ThresholdUtils::fieldElementToString(g1_hash.Z).c_str());
+    std::string verify_hash;
+    assert(Verify(t, n, *pkey.getPublicKey(), *bn_sign, g1_hash, &verify_hash) == kBlsSuccess);
     return kBlsSuccess;
 }
 
