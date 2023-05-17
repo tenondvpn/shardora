9ac0bef2009079221ff0cc3a51db71050ec0a294
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-17 12:40:07 +0800

    add test solidity library

--- src/bls/bls_dkg.cc
+++ src/bls/bls_dkg.cc
@@ -915,7 +915,7 @@ void BlsDkg::CreateContribution(uint32_t valid_n, uint32_t valid_t) {
             old_val = libff::alt_bn128_G2(x_coord, y_coord, z_coord);
         }
 
-        assert(old_val == old_g2);
+        assert(old_val != old_g2);
         auto midx = local_member_index_ / common::kElectNodeMinMemberIndex;
         if (verfy_final_vals.verify_req().verify_vec_size() <= midx) {
             assert(false);
@@ -941,9 +941,9 @@ void BlsDkg::CreateContribution(uint32_t valid_n, uint32_t valid_t) {
             << ", verified: " << common::Encode::HexEncode(item.x_c0())
             << ", polynomial: " << common::Encode::HexEncode(local_poly.polynomial(0))
             << ", contribution: " << libBLS::ThresholdUtils::fieldElementToString(
-                local_src_secret_key_contribution_[local_member_index_]) << std::endl;
+                local_src_secret_key_contribution_[mem_idx]) << std::endl;
 //         all_verified_val = all_verified_val - old_g2_val + new_g2_val;
-        assert(all_verified_val == local_src_secret_key_contribution_[local_member_index_] * libff::alt_bn128_G2::zero());
+        assert(all_verified_val == local_src_secret_key_contribution_[mem_idx] * libff::alt_bn128_G2::zero());
     }
 
 #ifdef ZJC_UNITTEST
