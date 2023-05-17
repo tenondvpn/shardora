b957315e15be082cb6d3a4342fb8a4653262a1b8
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-17 11:13:33 +0800

    add test solidity library

--- src/bls/bls_dkg.cc
+++ src/bls/bls_dkg.cc
@@ -933,6 +933,7 @@ void BlsDkg::CreateContribution(uint32_t valid_n, uint32_t valid_t) {
         auto z_c1 = libff::alt_bn128_Fq(common::Encode::HexEncode(item.z_c1()).c_str());
         auto z_coord = libff::alt_bn128_Fq2(z_c0, z_c1);
         auto all_verified_val = libff::alt_bn128_G2(x_coord, y_coord, z_coord);
+
 //         auto old_g2_val = power(libff::alt_bn128_Fr(mem_idx + 1), change_idx) * old_val;
 //         auto new_g2_val = power(libff::alt_bn128_Fr(mem_idx + 1), change_idx) * new_g2;
         std::cout << "node " << mem_idx << " success get " << local_member_index_ << " " << common::Encode::HexEncode((*members_)[local_member_index_]->id)
