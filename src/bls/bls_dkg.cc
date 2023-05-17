d384021237eeb7c9daf69c3165661625b0213b98
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-17 13:45:53 +0800

    add test solidity library

--- src/bls/bls_dkg.cc
+++ src/bls/bls_dkg.cc
@@ -937,8 +937,11 @@ void BlsDkg::CreateContribution(uint32_t valid_n, uint32_t valid_t) {
         std::cout << "node " << mem_idx << " success get " << local_member_index_ << " " << common::Encode::HexEncode((*members_)[local_member_index_]->id)
             << ", verified: " << common::Encode::HexEncode(item.x_c0())
             << ", polynomial: " << common::Encode::HexEncode(local_poly.polynomial(0))
-            << ", contribution: " << libBLS::ThresholdUtils::fieldElementToString(
-                local_src_secret_key_contribution_[mem_idx]) << std::endl;
+            << ", 0: " << (all_verified_val == local_src_secret_key_contribution_[local_member_index_] * libff::alt_bn128_G2::zero())
+            << ", 1: " << (all_verified_val == local_src_secret_key_contribution_[mem_idx] * libff::alt_bn128_G2::zero())
+            << ", contribution0: " << libBLS::ThresholdUtils::fieldElementToString(local_src_secret_key_contribution_[local_member_index_])
+            << ", contribution1: " << libBLS::ThresholdUtils::fieldElementToString(local_src_secret_key_contribution_[mem_idx])
+            << std::endl;
 //         all_verified_val = all_verified_val - old_g2_val + new_g2_val;
         assert(all_verified_val == local_src_secret_key_contribution_[mem_idx] * libff::alt_bn128_G2::zero());
     }
