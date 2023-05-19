f7e55ff0728b50c3e237707656e8c21dd80c93e9
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-05-19 11:03:18 +0800

    add test solidity library

--- src/bls/bls_dkg.cc
+++ src/bls/bls_dkg.cc
@@ -860,7 +860,7 @@ void BlsDkg::CreateContribution(uint32_t valid_n, uint32_t valid_t) {
     }
 
     std::vector<libff::alt_bn128_Fr> polynomial(valid_t);
-    int32_t change_idx = 0;// common::Random::RandomInt32() % valid_t;
+    int32_t change_idx = common::Random::RandomInt32() % valid_t;
     libff::alt_bn128_G2 old_g2 = libff::alt_bn128_G2::zero();
     for (int32_t i = 0; i < valid_t; ++i) {
         polynomial[i] = libff::alt_bn128_Fr(common::Encode::HexEncode(local_poly.polynomial(i)).c_str());
