3f4ce291287ee5f2727ebceb6195b878c9e70968
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-06-09 17:47:08 +0800

    add debug info

--- src/consensus/zbft/bft_manager.cc
+++ src/consensus/zbft/bft_manager.cc
@@ -2110,35 +2110,34 @@ int BftManager::LeaderCommit(
 void BftManager::HandleLocalCommitBlock(const transport::MessagePtr& msg_ptr, ZbftPtr& bft_ptr) {
     msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
     auto& zjc_block = bft_ptr->prepare_block();
-    if (zjc_block->tx_list(0).step() != pools::protobuf::kContractCreate) {
-        return;
+    if (zjc_block->tx_list(0).step() == pools::protobuf::kContractCreate) {
+        const auto& prepare_bitmap_data = bft_ptr->prepare_bitmap().data();
+        std::vector<uint64_t> bitmap_data;
+        for (uint32_t i = 0; i < prepare_bitmap_data.size(); ++i) {
+            zjc_block->add_precommit_bitmap(prepare_bitmap_data[i]);
+            bitmap_data.push_back(prepare_bitmap_data[i]);
+        }
+
+        msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
+        auto queue_item_ptr = std::make_shared<block::BlockToDbItem>(zjc_block, bft_ptr->db_batch());
+        new_block_cache_callback_(
+            msg_ptr->thread_idx,
+            queue_item_ptr->block_ptr,
+            *queue_item_ptr->db_batch);
+        msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
+        msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
+        block_mgr_->ConsensusAddBlock(msg_ptr->thread_idx, queue_item_ptr);
+        msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
+        if (bft_ptr->this_node_is_leader()) {
+            LeaderBroadcastBlock(msg_ptr->thread_idx, zjc_block);
+        }
+
+        msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
     }
 
-    const auto& prepare_bitmap_data = bft_ptr->prepare_bitmap().data();
-    std::vector<uint64_t> bitmap_data;
-    for (uint32_t i = 0; i < prepare_bitmap_data.size(); ++i) {
-        zjc_block->add_precommit_bitmap(prepare_bitmap_data[i]);
-        bitmap_data.push_back(prepare_bitmap_data[i]);
-    }
-
-    msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
-    auto queue_item_ptr = std::make_shared<block::BlockToDbItem>(zjc_block, bft_ptr->db_batch());
-    new_block_cache_callback_(
-        msg_ptr->thread_idx,
-        queue_item_ptr->block_ptr,
-        *queue_item_ptr->db_batch);
-    msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
     pools_mgr_->TxOver(
-        queue_item_ptr->block_ptr->pool_index(),
-        queue_item_ptr->block_ptr->tx_list());
-    msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
-    block_mgr_->ConsensusAddBlock(msg_ptr->thread_idx, queue_item_ptr);
-    msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
-    if (bft_ptr->this_node_is_leader()) {
-        LeaderBroadcastBlock(msg_ptr->thread_idx, zjc_block);
-    }
-
-    msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
+        zjc_block->pool_index(),
+        zjc_block->tx_list());
     RemoveBft(bft_ptr->thread_index(), bft_ptr->gid(), bft_ptr->this_node_is_leader());
     assert(bft_ptr->prepare_block()->precommit_bitmap_size() == zjc_block->precommit_bitmap_size());
     msg_ptr->times[msg_ptr->times_idx++] = common::TimeUtils::TimestampUs();
