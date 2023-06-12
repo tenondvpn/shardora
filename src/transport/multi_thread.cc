fac8c052b6b3ba2a73a1897ed0a33578be7e8e42
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-06-12 15:25:23 +0800

    fix

--- src/transport/multi_thread.cc
+++ src/transport/multi_thread.cc
@@ -49,7 +49,7 @@ void ThreadHandler::HandleMessage() {
             msg_ptr->thread_idx = thread_idx_;
             auto btime = common::TimeUtils::TimestampUs();
             msg_ptr->times[msg_ptr->times_idx++] = btime;
-//             ZJC_INFO("message handled msg hash: %lu, thread idx: %d", msg_ptr->header.hash64(), msg_ptr->thread_idx);
+            ZJC_INFO("message handled msg hash: %lu, thread idx: %d", msg_ptr->header.hash64(), msg_ptr->thread_idx);
             Processor::Instance()->HandleMessage(msg_ptr);
             auto etime = common::TimeUtils::TimestampUs();
             if (etime - btime > 100000) {
