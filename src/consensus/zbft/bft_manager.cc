4bcdb5f5f58b3bb7cc597b3b67e15e8819a08c2f
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-06-13 19:22:51 +0800

    fix

--- src/consensus/zbft/bft_manager.cc
+++ src/consensus/zbft/bft_manager.cc
@@ -1210,6 +1210,7 @@ ZbftPtr BftManager::CreateBftPtr(
     }
 
     if (txs_ptr == nullptr) {
+        ZJC_INFO("get tx failed leader tx size: %d, type: %d", bft_msg.tx_bft().tx_hash_list_size(), bft_msg.tx_bft().tx_type());
         assert(false);
         return nullptr;
     }
