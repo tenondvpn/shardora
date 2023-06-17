21c73bbaf7459a6ff019dfdb17c0a90dcea4d4eb
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-06-17 16:08:04 +0800

    fix pool

--- src/consensus/zbft/root_to_tx_item.h
+++ src/consensus/zbft/root_to_tx_item.h
@@ -22,6 +22,32 @@ public:
         std::shared_ptr<security::Security>& sec_ptr);
     virtual ~RootToTxItem();
 
+    virtual int TxToBlockTx(
+            const pools::protobuf::TxMessage& tx_info,
+            std::shared_ptr<db::DbWriteBatch>& db_batch,
+            block::protobuf::BlockTx* block_tx) {
+        DefaultTxItem(tx_info, block_tx);
+        // change
+        if (tx_info.key().empty() ||
+                tx_info.key() != protos::kNormalTos ||
+                tx_info.value().empty() ||
+                tx_info.value().size() % 32 != 0) {
+            return consensus::kConsensusError;
+        }
+
+        uint32_t offset = 0;
+        uint32_t count = tx_info.value().size() / 32;
+        for (uint32_t i = 0; i < count; ++i) {
+            auto storage = block_tx->add_storages();
+            std::string tmp(tx_info.value().c_str() + offset, 32);
+            storage->set_key(protos::kNormalToShards);
+            storage->set_val_hash(tmp);
+            offset += 32;
+        }
+
+        return consensus::kConsensusSuccess;
+    }
+
     virtual int HandleTx(
         uint8_t thread_idx,
         const block::protobuf::Block& block,
