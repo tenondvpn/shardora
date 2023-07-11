d390cc09494184a247ce420afbaf8791b0e71966
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-07-11 09:06:46 +0800

    fix

--- src/network/route.cc
+++ src/network/route.cc
@@ -64,7 +64,7 @@ int Route::Send(const transport::MessagePtr& msg_ptr) {
         if (message.has_broadcast()) {
             assert(message.broadcast().bloomfilter_size() < 64);
 //             broadcast_->Broadcasting(msg_ptr->thread_idx, dht_ptr, msg_ptr);
-            ZJC_DEBUG("0 broadcast: %lu, now size: %u", header_ptr->header.hash64(), broadcast_queue_[msg_ptr->thread_idx].size());
+            ZJC_DEBUG("0 broadcast: %lu, now size: %u", msg_ptr->header.hash64(), broadcast_queue_[msg_ptr->thread_idx].size());
             broadcast_queue_[msg_ptr->thread_idx].push(msg_ptr);
             broadcast_con_.notify_one();
         } else {
