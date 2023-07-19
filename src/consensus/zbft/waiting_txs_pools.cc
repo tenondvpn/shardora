8408b4ce6364ecc6275cd2cbc301bf9397b646a2
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-07-19 17:00:46 +0800

    fix

--- src/consensus/zbft/waiting_txs_pools.cc
+++ src/consensus/zbft/waiting_txs_pools.cc
@@ -155,6 +155,10 @@ std::shared_ptr<WaitingTxsItem> WaitingTxsPools::GetStatisticTx(uint32_t pool_in
         }
     }
 
+    if (leader) {
+        ZJC_DEBUG("leader now get statistic tx.");
+    }
+
     auto tx_ptr = block_mgr_->GetStatisticTx(pool_index, leader);
     if (tx_ptr != nullptr) {
         if (leader) {
