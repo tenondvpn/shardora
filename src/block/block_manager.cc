db920dc06bd5ec6a11b2a0daab721cabf1c1896f
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-07-19 15:30:09 +0800

    fix

--- src/block/block_manager.cc
+++ src/block/block_manager.cc
@@ -1533,7 +1533,7 @@ bool BlockManager::ShouldStopConsensus() {
     auto now_tm_ms = common::TimeUtils::TimestampMs();
     auto tmp_to_txs = latest_to_tx_;
     if (tmp_to_txs != nullptr) {
-        if (tmp_to_txs->stop_consensus_timeout > now_tm_ms) {
+        if (tmp_to_txs->stop_consensus_timeout < now_tm_ms) {
             ZJC_DEBUG("to tx stop consensus timeout: %lu, %lu", tmp_to_txs->stop_consensus_timeout, now_tm_ms);
             return true;
         }
@@ -1541,7 +1541,7 @@ bool BlockManager::ShouldStopConsensus() {
 
     auto& cross_statistic_tx = latest_cross_statistic_tx_;
     if (cross_statistic_tx != nullptr) {
-        if (cross_statistic_tx->stop_consensus_timeout > now_tm_ms) {
+        if (cross_statistic_tx->stop_consensus_timeout < now_tm_ms) {
             ZJC_DEBUG("shard cross tx stop consensus timeout: %lu, %lu", cross_statistic_tx->stop_consensus_timeout, now_tm_ms);
             return true;
         }
@@ -1549,7 +1549,7 @@ bool BlockManager::ShouldStopConsensus() {
 
     auto shard_statistic_tx = latest_shard_statistic_tx_;
     if (shard_statistic_tx != nullptr) {
-        if (shard_statistic_tx->stop_consensus_timeout > now_tm_ms) {
+        if (shard_statistic_tx->stop_consensus_timeout < now_tm_ms) {
             ZJC_DEBUG("shard statistic tx stop consensus timeout: %lu, %lu", shard_statistic_tx->stop_consensus_timeout, now_tm_ms);
             return true;
         }
@@ -1558,7 +1558,7 @@ bool BlockManager::ShouldStopConsensus() {
     for (uint32_t i = network::kRootCongressNetworkId; i <= max_consensus_sharding_id_; ++i) {
         auto shard_elect_tx = shard_elect_tx_[i];
         if (shard_elect_tx != nullptr) {
-            if (shard_elect_tx->stop_consensus_timeout > now_tm_ms) {
+            if (shard_elect_tx->stop_consensus_timeout < now_tm_ms) {
                 ZJC_DEBUG("shard elect tx stop consensus timeout: %lu, %lu", shard_statistic_tx->stop_consensus_timeout, now_tm_ms);
                 return true;
             }
