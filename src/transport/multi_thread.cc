613bf39900ed09ba5789fa78928ef75430ede823
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-07-24 19:17:36 +0800

    remove bitmap

--- src/transport/multi_thread.cc
+++ src/transport/multi_thread.cc
@@ -393,7 +393,7 @@ MessagePtr MultiThreadHandler::GetMessageFromQueue(uint32_t thread_idx) {
                     }
 
                     ZJC_DEBUG("pop valid message hash: %lu, size: %u, thread: %u",
-                        msg_obj->header.hash64(), threads_message_queues_[i][pri]->size(), thread_idx);
+                        msg_obj->header.hash64(), threads_message_queues_[i][pri].size(), thread_idx);
                     return msg_obj;
                 }
             }
