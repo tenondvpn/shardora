bc0a60a554821ed374e481ceb2d4bfb9493073e1
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-07-31 15:11:43 +0800

    fix

--- src/consensus/zbft/contract_call.cc
+++ src/consensus/zbft/contract_call.cc
@@ -222,12 +222,14 @@ int ContractCall::HandleTx(
     acc_balance_map["pre_" + block_tx.from()] = from_balance;
     block_tx.set_balance(from_balance);
     block_tx.set_gas_used(gas_used);
-    ZJC_DEBUG("contract called %s, user: %s, prepament: %lu, gas used: %lu, gas_price: %lu",
+    ZJC_DEBUG("contract called %s, user: %s, prepament: %lu, "
+        "gas used: %lu, gas_price: %lu, status: %d",
         common::Encode::HexEncode(block_tx.to()).c_str(),
         common::Encode::HexEncode(block_tx.from()).c_str(),
         from_balance,
         gas_used,
-        block_tx.gas_price());
+        block_tx.gas_price(),
+        block_tx.status());
     return kConsensusSuccess;
 }
 
