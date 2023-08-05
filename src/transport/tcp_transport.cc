852f43c48c60f245801090bc12f8432dfbef3e78
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-08-05 10:50:11 +0800

    fix

--- src/transport/tcp_transport.cc
+++ src/transport/tcp_transport.cc
@@ -211,7 +211,8 @@ int TcpTransport::Send(
         tnet::TcpInterface* tcp_conn,
         const transport::protobuf::Header& message) {
     assert(message.broadcast().bloomfilter_size() < 64);
-    message.set_from_public_port(common::GlobalInfo::Instance()->config_public_port());
+    auto tmpHeader = const_cast<transport::protobuf::Header*>(&message);
+    tmpHeader->set_from_public_port(common::GlobalInfo::Instance()->config_public_port());
     std::string msg;
     if (!message.has_hash64() || message.hash64() == 0) {
         SetMessageHash(message, thread_idx);
@@ -235,7 +236,8 @@ int TcpTransport::Send(
         uint16_t des_port,
         const transport::protobuf::Header& message) {
     assert(thread_idx < common::kMaxThreadCount);
-    message.set_from_public_port(common::GlobalInfo::Instance()->config_public_port());
+    auto tmpHeader = const_cast<transport::protobuf::Header*>(&message);
+    tmpHeader->set_from_public_port(common::GlobalInfo::Instance()->config_public_port());
     std::string msg;
     assert(message.broadcast().bloomfilter_size() < 64);
     if (!message.has_hash64() || message.hash64() == 0) {
