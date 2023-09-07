20e7e8356bbc428730cc565fd2cdfbbeea3c57a8
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-09-07 12:17:39 +0800

    fix

--- src/init/genesis_block_init.cc
+++ src/init/genesis_block_init.cc
@@ -64,7 +64,7 @@ int GenesisBlockInit::CreateGenesisBlocks(
     pools_mgr_ = std::make_shared<pools::TxPoolManager>(security, db_, kv_sync, nullptr);
     std::shared_ptr<pools::ShardStatistic> statistic_mgr = nullptr;
     account_mgr_->Init(1, db_, pools_mgr_);
-    block_mgr_->Init(account_mgr_, db_, pools_mgr_, statistic_mgr, security, "", nullptr, nullptr);
+    block_mgr_->Init(account_mgr_, db_, pools_mgr_, statistic_mgr, security, "", nullptr, nullptr, nullptr);
     if (net_id == network::kRootCongressNetworkId) {
         res = CreateRootGenesisBlocks(root_genesis_nodes, cons_genesis_nodes);
     } else {
