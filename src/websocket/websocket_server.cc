9e2480eb14ea0f04d3f04b20082183a8ae9c9840
tenondvpn <jhsx5679@gmail.com>
2023-11-28 16:06:14 +0800

    fix contract addr

--- src/websocket/websocket_server.cc
+++ src/websocket/websocket_server.cc
@@ -23,8 +23,7 @@ int32_t WebSocketServer::Init(const char* ip, uint16_t port, WebsocketCloseCallb
     server_.set_close_handler(websocketpp::lib::bind(
         &WebSocketServer::OnClose,
         this,
-        websocketpp::lib::placeholder
-        s::_1));
+        websocketpp::lib::placeholders::_1));
     server_.set_message_handler(websocketpp::lib::bind(
         &WebSocketServer::OnMessage,
         this,
