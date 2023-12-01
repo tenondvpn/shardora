c664862a667e1e9124985acb2dce9d961b7a035e
许飞 <xufei1990@gmail.com>
2023-12-01 19:18:36 +0800

    fix

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ps -ef | grep zjchain | awk -F' ' '{print $2}' | xargs kill -9
+# ps -ef | grep zjchain | awk -F' ' '{print $2}' | xargs kill -9
+
+sudo systemctl stop zjchain@r1.service &
+sudo systemctl stop zjchain@r2.service &
+sudo systemctl stop zjchain@r3.service &
+sudo systemctl stop zjchain@s1.service &
+sudo systemctl stop zjchain@s2.service &
+sudo systemctl stop zjchain@s3.service &
