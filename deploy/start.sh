e94e21e058c04e439b43b47401d6cb71d3a20a14
许飞 <xufei1990@gmail.com>
2023-12-04 10:19:08 +0800

    deploy

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,14 +2,16 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-8.3.0/lib64/
 # ps -ef | grep zjchain | awk -F' ' '{print $2}' | xargs kill -9
 
-sudo systemctl start zjchain@r1.service &
+sh stop.sh
+
+sudo systemctl start zjchain@r1.service
+sleep 5
+
+sudo systemctl start zjchain@r2.service
+sudo systemctl start zjchain@r3.service
+sudo systemctl start zjchain@s1.service
+sudo systemctl start zjchain@s2.service
+sudo systemctl start zjchain@s3.service
+
 sleep 3
 
-sudo systemctl start zjchain@r2.service &
-sudo systemctl start zjchain@r3.service &
-sudo systemctl start zjchain@s1.service &
-sudo systemctl start zjchain@s2.service &
-sudo systemctl start zjchain@s3.service &
-
-
-exit 0
--- deploy/stop.sh
+++ deploy/stop.sh
@@ -2,9 +2,9 @@
 
 # ps -ef | grep zjchain | awk -F' ' '{print $2}' | xargs kill -9
 
-sudo systemctl stop zjchain@r1.service &
-sudo systemctl stop zjchain@r2.service &
-sudo systemctl stop zjchain@r3.service &
-sudo systemctl stop zjchain@s1.service &
-sudo systemctl stop zjchain@s2.service &
-sudo systemctl stop zjchain@s3.service &
+sudo systemctl stop zjchain@r1.service
+sudo systemctl stop zjchain@r2.service
+sudo systemctl stop zjchain@r3.service
+sudo systemctl stop zjchain@s1.service
+sudo systemctl stop zjchain@s2.service
+sudo systemctl stop zjchain@s3.service
