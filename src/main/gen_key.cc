0bfcc15744f5e114edda2cf42c14ae777b766528
许飞 <feixu@zhejianglab.com>
2023-12-25 20:16:34 +0800

    gen key

--- src/main/gen_key.cc
+++ src/main/gen_key.cc
@@ -2,6 +2,7 @@
 #include "security/ecdsa/private_key.h"
 #include "security/ecdsa/public_key.h"
 #include "security/ecdsa/curve.h"
+#include <common/encode.h>
 
 int main(int argc, char** argv) {
     zjchain::security::Curve curve;
@@ -9,7 +10,7 @@ int main(int argc, char** argv) {
     zjchain::security::PrivateKey prikey(argv[0]);
 
     pubkey.FromPrivateKey(curve, prikey);
-    std::cout << pubkey.str_pubkey_uncompressed().c_str() << std::endl;
+    std::cout << zjchain::common::Encode::HexEncode(pubkey.str_pubkey()) << std::endl;
     
     return 0;
 }
