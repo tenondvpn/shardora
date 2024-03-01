89e487269b9c839391a0579b6a15bf62c3d14dd6
许飞 <feixu@zhejianglab.com>
2024-03-01 14:05:03 +0800

    fix

--- src/pools/tx_utils.h
+++ src/pools/tx_utils.h
@@ -18,7 +18,7 @@ namespace zjchain {
 
 namespace pools {
 
-static const uint64_t kBftStartDeltaTime = 1000000lu;
+static const uint64_t kBftStartDeltaTime = 3000000lu;
 static const uint32_t kTxPoolTimeoutUs = 10u * 1000u * 1000u;
 static const uint32_t kTxStorageKeyMaxSize = 12u;
 static const uint32_t kMaxToTxsCount = 10000u;
