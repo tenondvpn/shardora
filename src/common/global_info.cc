f8cf6a4b840d2d04c238b7acf7ffef6772bb16e2
tenondvpn <jhsx5679@gmail.com>
2024-03-11 10:21:42 +0800

    fix

--- src/common/global_info.cc
+++ src/common/global_info.cc
@@ -27,6 +27,7 @@ GlobalInfo::~GlobalInfo() {
 }
 
 int GlobalInfo::Init(const common::Config& config) {
+    begin_run_timestamp_ms_ = common::TimeUtils::TimestampMs();
     message_handler_thread_count_ = 4;
     config.Get("zjchain", "consensus_thread_count", message_handler_thread_count_);
     ++message_handler_thread_count_;
--- src/common/global_info.h
+++ src/common/global_info.h
@@ -164,12 +164,44 @@ public:
         return ++now_valid_thread_index_;
     }
 
+    // After running for a period of time, ensure that all threads have been created successfully and cancel the lock.
+    uint8_t get_thread_index() {
+        auto now_thread_id = std::thread::get_id();
+        uint8_t thread_idx = 0;
+        if (should_check_thread_all_valid_) {
+            std::lock_guard<std::mutex> g(now_valid_thread_index_mutex_);
+            auto iter = thread_with_index_.find(now_thread_id);
+            if (iter == thread_with_index_.end()) {
+                thread_idx = now_valid_thread_index_++;
+                thread_with_index_[now_thread_id] = thread_idx;
+            } else {
+                thread_idx = iter->second;
+            }
+            
+            auto now_tm = common::TimeUtils::TimestampMs();
+            if (begin_run_timestamp_ms_ + kWaitingAllThreadValidPeriodMs < now_tm) {
+                should_check_thread_all_valid_ = false;
+            }
+        } else {
+            uint8_t thread_idx = 0;
+            auto iter = thread_with_index_.find(now_thread_id);
+            if (iter == thread_with_index_.end()) {
+                ZJC_FATAL("invalid get new thread index.");
+            }
+                
+            thread_idx = iter->second;
+        }
+
+        return thread_idx;
+    }
+
 private:
     GlobalInfo();
     ~GlobalInfo();
 
     static const uint32_t kDefaultTestNetworkShardId = 4u;
     static const uint32_t kTickThreadPoolCount = 1U;
+    static const uint64_t kWaitingAllThreadValidPeriodMs = 10000lu;
 
     std::string config_local_ip_;
     uint16_t config_local_port_{ 0 };
@@ -202,6 +234,9 @@ private:
     uint32_t pools_with_thread_[common::kInvalidPoolIndex] = { 0 };
     uint8_t now_valid_thread_index_ = 0;
     std::mutex now_valid_thread_index_mutex_;
+    uint64_t begin_run_timestamp_ms_ = 0;
+    volatile bool should_check_thread_all_valid_ = true;
+    std::unordered_map<int, uint8_t> valid_thread_index_;
 
     DISALLOW_COPY_AND_ASSIGN(GlobalInfo);
 };
