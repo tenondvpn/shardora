9b93256792a0ca57259b06d545648f8b067a29a2
许飞 <feixu@zhejianglab.com>
2024-03-12 09:24:54 +0800

    fix

--- deploy_genesis.sh
+++ deploy_genesis.sh
@@ -33,7 +33,7 @@ sleep 3
 echo "[$server0]"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-8.3.0/lib64
 for node in r2 r3 s3_1 s3_2 s3_3 s3_4 s3_5 s3_6; do
-cd /root/xf/zjnodes/$node/ && nohup ASAN_OPTIONS=detect_leaks=1 ./zjchain -f 0 -g 0 $node xf> /dev/null 2>&1 &
+cd /root/xf/zjnodes/$node/ && ASAN_OPTIONS=detect_leaks=1 nohup ./zjchain -f 0 -g 0 $node xf> /dev/null 2>asan.txt &
 done
 
 
