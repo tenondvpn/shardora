5374ae01ad89b7d14ef91a0836cb9b5352044892
tenondvpn <jhsx5679@gmail.com>
2024-03-14 20:44:54 +0800

    fix

--- src/common/thread_safe_queue.h
+++ src/common/thread_safe_queue.h
@@ -20,7 +20,7 @@ public:
 
     void push(T e) {
         while (!rw_queue_.try_enqueue(e)) {
-            ZJC_DEBUG("get rw_queue size: %u", 0);
+            ZJC_DEBUG("get rw_queue size: %u", size());
             std::unique_lock<std::mutex> lock(mutex_);
             con_.wait_for(lock, std::chrono::milliseconds(100));
         }
