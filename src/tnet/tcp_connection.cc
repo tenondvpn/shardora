17d67c0818dfad3e10c622bab09a22e23f67e297
tenondvpn <jhsx5679@gmail.com>
2024-03-18 17:02:03 +0800

    fix

--- src/tnet/tcp_connection.cc
+++ src/tnet/tcp_connection.cc
@@ -31,9 +31,6 @@ TcpConnection::~TcpConnection() {
         packet_decoder_->Free();
         packet_decoder_ = nullptr;
     }
-
-    int a = 0;
-    delete &a;
 }
 
 void TcpConnection::SetPacketEncoder(PacketEncoder* encoder) {
