f33c460ab1488ed101d2e1a8ad6e171c3c3957a5
tenondvpn <jhsx5679@gmail.com>
2024-03-25 09:20:26 +0800

    add leader txs

--- src/network/route.cc
+++ src/network/route.cc
@@ -125,9 +125,11 @@ void Route::HandleMessage(const transport::MessagePtr& header_ptr) {
 }
 
 bool Route::CheckPoolsMessage(const transport::MessagePtr& header_ptr, dht::BaseDhtPtr dht_ptr) {
+    ZJC_DEBUG("pools message check route coming.");
     auto& header = header_ptr->header;
     if (header.has_broadcast()) {
         assert(false);
+        ZJC_DEBUG("pools message check route coming has broadcast.");
         return false;
     }
 
@@ -139,12 +141,14 @@ bool Route::CheckPoolsMessage(const transport::MessagePtr& header_ptr, dht::Base
     // TODO: check is this node tx message or route to nearest consensus node
     if (header_ptr->address_info->sharding_id() != common::GlobalInfo::Instance()->network_id()) {
         RouteByUniversal(header_ptr);
+        ZJC_DEBUG("pools message check route coming network invalid.");
         return false;
     }
 
     auto members = all_shard_members_[common::GlobalInfo::Instance()->network_id()];
     if (members == nullptr) {
         dht_ptr->SendToClosestNode(header_ptr);
+        ZJC_DEBUG("pools message check route coming no members.");
         return false;
     }
 
@@ -155,9 +159,11 @@ bool Route::CheckPoolsMessage(const transport::MessagePtr& header_ptr, dht::Base
             (*members)[store_member_index]->id);
         header.set_des_dht_key(dht_key.StrKey());
         dht_ptr->SendToClosestNode(header_ptr);
+        ZJC_DEBUG("pools message check route coming not this node.");
         return false;
     }
 
+    ZJC_DEBUG("pools message check route coming success this node.");
     return true;
 }
 
