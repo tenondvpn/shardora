3425bce20ec017680a2ad108a01a9724d6cc7698
许飞 <feixu@zhejianglab.com>
2024-03-27 11:47:15 +0800

    fix

--- src/main/tx_cli.cc
+++ src/main/tx_cli.cc
@@ -14,7 +14,7 @@
 using namespace shardora;
 static bool global_stop = false;
 static const std::string kBroadcastIp = "10.0.0.16";
-static const uint16_t kBroadcastPort = 14008;
+static const uint16_t kBroadcastPort = 13018;
 
 static void SignalCallback(int sig_int) {
     global_stop = true;
@@ -221,8 +221,8 @@ int tx_main(int argc, char** argv) {
     }
 
     std::string gid = common::Random::RandomString(32);
-    std::string prikey = common::Encode::HexDecode("ed8aa75374998a6fb20139171e570ae67ceb34817b87b05400023ff9f1e06532");
-    std::string to = common::Encode::HexDecode("354bbdd9589f338eb2306f0ce377e639e940d9f6");
+    std::string prikey = common::Encode::HexDecode("b5039128131f96f6164a33bc7fbc48c2f5cf425e8476b1c4d0f4d186fbd0d708");
+    std::string to = common::Encode::HexDecode("27d4c39244f26c157b5a87898569ef4ce5807413");
     uint32_t prikey_pos = 0;
     auto from_prikey = prikey;
     security->SetPrivateKey(from_prikey);
