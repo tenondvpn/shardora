5c0816b3061c837675341c666e5986589918e01a
许飞 <feixu@zhejianglab.com>
2024-04-02 17:13:57 +0800

    fix

--- src/consensus/hotstuff/view_block_chain_manager.h
+++ src/consensus/hotstuff/view_block_chain_manager.h
@@ -24,7 +24,7 @@ public:
 private:
     void ConsumeOrphanBlocks();
     inline std::shared_ptr<ViewBlockChain> Chain(const uint32_t& pool_idx) const {
-        return pool_chain_map_[pool_idx];
+        return pool_chain_map_.at(pool_idx);
     }
 
     std::unordered_map<uint32_t, std::shared_ptr<ViewBlockChain>> pool_chain_map_;
