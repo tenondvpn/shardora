f7a208f3f5fc406c6fca9104158e6881e05792cf
许飞 <feixu@zhejianglab.com>
2024-04-09 20:12:12 +0800

    fix

--- src/consensus/hotstuff/view_block_chain_manager.h
+++ src/consensus/hotstuff/view_block_chain_manager.h
@@ -15,14 +15,6 @@ public:
     ViewBlockChainManager(const ViewBlockChainManager&) = delete;
     ViewBlockChainManager& operator=(const ViewBlockChainManager&) = delete;
     
-    // Status Store(const uint32_t& pool_idx, const std::shared_ptr<ViewBlock>& view_block);
-    // Status Get(const uint32_t& pool_idx, const HashStr& hash, std::shared_ptr<ViewBlock>& view_block);
-    // bool Extends(const uint32_t& pool_idx, const std::shared_ptr<ViewBlock>& block, const std::shared_ptr<ViewBlock>& target);
-    // Status PruneTo(const uint32_t& pool_idx, const HashStr& target, std::vector<std::shared_ptr<ViewBlock>>& forked_blockes);
-
-    // void AddOrphanBlock(const uint32_t& pool_idx, const std::shared_ptr<ViewBlock>& view_block);
-    // std::shared_ptr<ViewBlock> PopOrphanBlock(const uint32_t& pool_idx);
-    // bool IsOrphanBlockTimeout(const uint32_t& pool_idx, const std::shared_ptr<ViewBlock> view_block) const;
     inline std::shared_ptr<ViewBlockChain> Chain(const uint32_t& pool_idx) const {
         return pool_chain_map_.at(pool_idx);
     }
@@ -32,9 +24,6 @@ public:
     }
     
 private:
-    // void ConsumeOrphanBlocks();
-
-
     std::unordered_map<uint32_t, std::shared_ptr<ViewBlockChain>> pool_chain_map_;
     // common::Tick tick_;
 };
