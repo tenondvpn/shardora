e7738553be6f0c86d459eff4a65d313da45bb39e
许飞 <feixu@zhejianglab.com>
2024-04-16 14:34:10 +0800

    fix

--- src/consensus/hotstuff/view_duration.h
+++ src/consensus/hotstuff/view_duration.h
@@ -13,11 +13,11 @@ public:
     ViewDuration(const ViewDuration&) = delete;
     ViewDuration& operator=(const ViewDuration&) = delete;
 
-    void ViewStarted();
+    void ViewStarted() {};
 
-    void ViewSucceeded();
+    void ViewSucceeded() {};
 
-    void ViewTimeout();
+    void ViewTimeout() {};
 
     inline uint64_t Duration() const {
         return duration_;
