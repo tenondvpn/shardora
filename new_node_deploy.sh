cc1109e1c7fbdaef3fd8cfb32035afc433f58837
lhy <jhsx5679@gmail.com>
2024-04-16 15:10:15 +0800

    fix

--- new_node_deploy.sh
+++ new_node_deploy.sh
@@ -4,16 +4,17 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-8.3.0/lib64/
 ps -ef | grep zjchain | grep new_node | awk -F' ' '{print $2}' | xargs kill -9
 
 nodes=("new_1" "new_2" "new_3" "new_4" "new_5" "new_6" "new_7" "new_8" "new_9" "new_10")
-
+rm -rf /root/zjnodes/new*
 
 for n in  "${nodes[@]}"; do
-    rm -rf /root/zjnodes/${n}/zjchain /root/zjnodes/${n}/core* /root/zjnodes/${n}/log/* /root/zjnodes/${n}/*db*
+
 
     mkdir -p "/root/zjnodes/${n}/log"
-    cp -rf /root/zjnodes/zjchain/GeoLite2-City.mmdb /root/zjnodes/${n}/conf
-    cp -rf /root/zjnodes/zjchain/conf/log4cpp.properties /root/zjnodes/${n}/conf
+    mkdir -p "/root/zjnodes/${n}/conf"
+    cp -rf /root/zjnodes/zjchain/GeoLite2-City.mmdb /root/zjnodes/${n}/conf/
+    cp -rf /root/zjnodes/zjchain/conf/log4cpp.properties /root/zjnodes/${n}/conf/
     cp -rf /root/zjnodes/zjchain/zjchain /root/zjnodes/${n}/
-    cp -rf /root/zjnodes/${n}/conf/zjchain.conf /root/zjnodes/${n}/conf/zjchain.conf
+    cp -rf ./zjnodes/${n}/conf/zjchain.conf /root/zjnodes/${n}/conf/zjchain.conf
     echo "cp $n"
 done
 
