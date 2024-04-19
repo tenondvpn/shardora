18a3ad81356e5c074eaa168be7385df933e0a484
许飞 <feixu@zhejianglab.com>
2024-04-19 11:27:31 +0800

    fix

--- src/consensus/hotstuff/tests/test_block_acceptor.cc
+++ src/consensus/hotstuff/tests/test_block_acceptor.cc
@@ -14,8 +14,11 @@ namespace hotstuff {
 namespace test {
 
 static const uint32_t POOL = 3;
-static const std::string sk =
-    "8bd064dde62eb50cdb1d8ec9c10043d6ac8a57b3c9b7b0178e4c63938d8ab0da";
+static const std::string sk_ =
+    "b5039128131f96f6164a33bc7fbc48c2f5cf425e8476b1c4d0f4d186fbd0d708";
+static const std::string pk_ = "042702ef617e594b27fac7cba1953470ee2f9774cfb73b44a43d866b36c61900351e5b41f1143be2031a6b632c34cbe37ac4e8a40c925395e44c40b99e2f6e85fc";
+static const std::string from_ = "431be10b3a0e46f8a46686c6b0c29bc743f715fa";
+
 transport::MultiThreadHandler net_handler_;
 std::shared_ptr<security::Security> security_ = nullptr;
 std::shared_ptr<block::AccountManager> account_mgr_ = nullptr;
@@ -36,7 +39,7 @@ protected:
     
     static void SetUpTestCase() {
         security_ = std::make_shared<security::Ecdsa>();
-        security_->SetPrivateKey(common::Encode::HexDecode(sk));
+        security_->SetPrivateKey(common::Encode::HexDecode(sk_));
         account_mgr_ = std::make_shared<block::AccountManager>();
         system("rm -rf ./core.* ./db");
         db_ = std::make_shared<db::Db>();
@@ -70,6 +73,13 @@ protected:
                 block_mgr_,
                 tm_block_mgr_);
 
+        auto account_info = std::make_shared<address::protobuf::AddressInfo>();
+        account_info->set_pool_index(POOL);
+        account_info->set_addr(from_);
+        account_info->set_type(address::protobuf::kNormal);
+        account_info->set_sharding_id(3);
+        account_info->set_latest_height(9);
+        account_info->set_balance(100000);
 
     }
 
@@ -91,13 +101,12 @@ protected:
         uint32_t* test_arr = (uint32_t*)random_prefix.data();
         auto tx_info = std::make_shared<pools::protobuf::TxMessage>();
         tx_info->set_step(pools::protobuf::kNormalFrom);
-        auto pk = std::string((char*)test_arr, 33);
-        tx_info->set_pubkey(pk);
-        tx_info->set_to("");
+        tx_info->set_pubkey(pk_);
+        tx_info->set_to("27d4c39244f26c157b5a87898569ef4ce5807413");
         auto gid = std::string((char*)test_arr, 32);
         tx_info->set_gid(gid);
         tx_info->set_gas_limit(0llu);
-        tx_info->set_amount(0);
+        tx_info->set_amount(1);
         tx_info->set_gas_price(common::kBuildinTransactionGasPrice);
         return tx_info;
     }
