6a21cca3e782a918d26293da9922a48789a6772b
huanying liu <lhying@zhejianglab.com>
2024-04-22 14:23:06 +0800

    auto check new accounts

--- test_accounts.py
+++ test_accounts.py
@@ -9,13 +9,15 @@ import json
 
 
 def check_addresses(addresses):
+    allcount=len(addresses)
     def check_address(address):
-        res = ''
+        res = address
         try:
             response = requests.post(f'http://localhost:8301/query_account?address={address}')
             data = response.json()
-
         # 检查返回的数据是否符合成功的条件
+            if data is None:
+                print(f"Failed address: {address} , response:{response}")
             if data.get("balance") is not None:
                 print(f"Succeeded address: {address} , balance:{data.get('balance')}")
                 res = None
@@ -34,7 +36,7 @@ def check_addresses(addresses):
             print(f"Checking {count}th addresses...")
             failed_addresses = [result for result in results if result is not None]
             if failed_addresses:
-                print(f"Some addresses failed, retrying..., {failed_addresses}")
+                print(f"Some addresses failed, retrying... all:{allcount}, try:{len(results)},fail:{len(failed_addresses)}")
                 addresses = failed_addresses
                 time.sleep(5)
             else:
