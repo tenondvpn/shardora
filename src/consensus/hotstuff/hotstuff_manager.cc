9610bde92f57ff21325f73a2fdf56c19495b4c38
许飞 <feixu@zhejianglab.com>
2024-04-25 16:11:24 +0800

    fix

--- src/consensus/hotstuff/hotstuff_manager.cc
+++ src/consensus/hotstuff/hotstuff_manager.cc
@@ -521,6 +521,7 @@ void HotstuffManager::HandleMessage(const transport::MessagePtr& msg_ptr) {
     if (header.has_hotstuff_timeout_proto()) {
         auto pool_idx = header.hotstuff_timeout_proto().pool_idx();
         auto pace = pacemaker(pool_idx);
+        ZJC_DEBUG("====1.1 msg received, pool_idx: %d", pool_idx);
         pace->OnRemoteTimeout(msg_ptr);
     }
 
--- src/consensus/hotstuff/pacemaker.cc
+++ src/consensus/hotstuff/pacemaker.cc
@@ -151,15 +151,17 @@ void Pacemaker::OnLocalTimeout() {
 void Pacemaker::OnRemoteTimeout(const transport::MessagePtr& msg_ptr) {
     // TODO ecdh decrypt
     auto msg = msg_ptr->header;
+    ZJC_DEBUG("====1.2 msg received, pool_idx: %d", pool_idx_);
     assert(msg.type() == common::kHotstuffTimeoutMessage);
     
     if (!msg.has_hotstuff_timeout_proto()) {
         return;
     }
-
+    ZJC_DEBUG("====1.3 msg received, pool_idx: %d", pool_idx_);
     if (msg.hotstuff_timeout_proto().pool_idx() != pool_idx_) {
         return;
     }
+    ZJC_DEBUG("====1.4 msg received, pool_idx: %d", pool_idx_);
     
     auto timeout_proto = msg.hotstuff_timeout_proto();
     ZJC_DEBUG("OnRemoteTimeout pool: %d, view: %d, member: %d", pool_idx_, timeout_proto.view(), timeout_proto.member_id());
