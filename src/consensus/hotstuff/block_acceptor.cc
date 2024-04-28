f0d379aa0ee920592afc43c3507122a8400197b9
许飞 <feixu@zhejianglab.com>
2024-04-28 19:40:26 +0800

    fix

--- src/consensus/hotstuff/block_acceptor.cc
+++ src/consensus/hotstuff/block_acceptor.cc
@@ -130,7 +130,7 @@ Status BlockAcceptor::Commit(std::shared_ptr<block::protobuf::Block>& block) {
     }
     if (block->leader_index() == elect_item->LocalMember()->index) {
         // leader broadcast block to other shards
-        LeaderBroadcastBlock(block);
+        // LeaderBroadcastBlock(block);
     }
     pools_mgr_->TxOver(block->pool_index(), block->tx_list());
 
