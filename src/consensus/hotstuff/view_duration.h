cc6ea8184efb75cfc9ead68221037992052ec4c7
许飞 <feixu@zhejianglab.com>
2024-04-30 10:12:07 +0800

    fix

--- src/consensus/hotstuff/view_duration.h
+++ src/consensus/hotstuff/view_duration.h
@@ -1,37 +1,3 @@
-// #pragma once
-// #include <consensus/hotstuff/types.h>
-
-// namespace shardora {
-
-// namespace hotstuff {
-
-// class ViewDuration {
-// public:
-//     ViewDuration() = default;
-//     ~ViewDuration() {};
-
-//     ViewDuration(const ViewDuration&) = delete;
-//     ViewDuration& operator=(const ViewDuration&) = delete;
-
-//     void ViewStarted() {};
-
-//     void ViewSucceeded() {};
-
-//     void ViewTimeout() {};
-
-//     inline uint64_t Duration() const {
-//         // TODO
-//         return 5000000;
-//     }
-
-// private:
-//     uint64_t duration_;
-// };
-
-// } // namespace consensus
-
-// } // namespace shardora
-    
 #pragma once
 #include <chrono>
 #include <cmath>
