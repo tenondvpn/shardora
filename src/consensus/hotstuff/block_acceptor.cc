ab106e81dadcf9c9f3de27c238fbcddd6f75b159
许飞 <feixu@zhejianglab.com>
2024-05-09 14:55:27 +0800

    fix

--- src/consensus/hotstuff/block_acceptor.cc
+++ src/consensus/hotstuff/block_acceptor.cc
@@ -128,16 +128,20 @@ Status BlockAcceptor::Commit(std::shared_ptr<block::protobuf::Block>& block) {
             *queue_item_ptr->db_batch);
     block_mgr_->ConsensusAddBlock(queue_item_ptr);
     // TODO if local node is leader, broadcast block
-    auto elect_item = elect_info_->GetElectItem(block->electblock_height());
-    if (elect_item) {
-        if (block->leader_index() == elect_item->LocalMember()->index) {
-            // leader broadcast block to other shards
-            // TODO tx_list 报错了!
-            LeaderBroadcastBlock(block);
+
+    if (block->tx_list_size() > 0) {
+        auto elect_item = elect_info_->GetElectItem(block->electblock_height());
+        if (elect_item) {
+            if (block->leader_index() == elect_item->LocalMember()->index) {
+                // leader broadcast block to other shards
+                // TODO tx_list 报错了!
+                LeaderBroadcastBlock(block);
+            }
         }
+
+        pools_mgr_->TxOver(block->pool_index(), block->tx_list());        
     }
 
-    pools_mgr_->TxOver(block->pool_index(), block->tx_list());
     // TODO tps measurement
     ZJC_DEBUG("[NEW BLOCK] hash: %s, key: %u_%u_%u_%u, timestamp:%lu, txs: %lu",
         common::Encode::HexEncode(block->hash()).c_str(),
