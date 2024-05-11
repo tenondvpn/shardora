f21516cb90d2c61e80c3888fcdd917f73e8a6744
tenondvpn <jhsx5679@gmail.com>
2024-05-11 16:08:24 +0800

    fix

--- src/consensus/hotstuff/block_acceptor.cc
+++ src/consensus/hotstuff/block_acceptor.cc
@@ -129,7 +129,14 @@ Status BlockAcceptor::Commit(std::shared_ptr<block::protobuf::Block>& block) {
             }
         }
 
-        pools_mgr_->TxOver(block->pool_index(), block->tx_list());        
+        for (uint32_t i = 0; i < block->tx_list_size(); ++i) {
+            ZJC_DEBUG("commit block tx over step: %d, to: %s, gid: %s", 
+                block->tx_list(i).step(),
+                common::Encode::HexEncode(block->tx_list(i).to()).c_str(),
+                common::Encode::HexEncode(block->tx_list(i).gid()).c_str());
+        }
+        
+        pools_mgr_->TxOver(block->pool_index(), block->tx_list());
     }
 
     // TODO tps measurement
