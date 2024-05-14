4446c7b433cfa8d9530813ecf84c95d927cd07fb
tenondvpn <jhsx5679@gmail.com>
2024-05-14 15:54:17 +0800

    fix

--- src/pools/tx_pool.cc
+++ src/pools/tx_pool.cc
@@ -738,6 +738,15 @@ std::shared_ptr<consensus::WaitingTxsItem> TxPool::GetTx(
 
 void TxPool::ConsensusAddTxs(const std::vector<pools::TxItemPtr>& txs) {
     for (uint32_t i = 0; i < txs.size(); ++i) {
+        if (txs[i]->tx_info.step() != pools::protobuf::kNormalFrom && 
+                txs[i]->tx_info.step() != pools::protobuf::kContractCreate && 
+                txs[i]->tx_info.step() != pools::protobuf::kContractGasPrepayment && 
+                txs[i]->tx_info.step() != pools::protobuf::kJoinElect && 
+                txs[i]->tx_info.step() != pools::protobuf::kCreateLibrary) {
+            ZJC_DEBUG("invalid tx add to consensus tx map: %d", txs[i]->tx_info.step());
+            continue;
+        }
+
         if (!GidValid(txs[i]->tx_info.gid())) {
             continue;
         }
