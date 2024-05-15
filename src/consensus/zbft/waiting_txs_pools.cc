37fd53abf3a10b1a9cd6cf98b532a7f75bcae234
tenondvpn <jhsx5679@gmail.com>
2024-05-15 09:57:46 +0800

    fix

--- src/consensus/zbft/waiting_txs_pools.cc
+++ src/consensus/zbft/waiting_txs_pools.cc
@@ -63,7 +63,7 @@ std::shared_ptr<WaitingTxsItem> WaitingTxsPools::GetSingleTx(uint32_t pool_index
         txs_item = GetCrossTx(pool_index, "");
     }
 
-    if (txs_item == nullptr) {
+    if (txs_item == nullptr && pool_index == 0) {
         txs_item = GetToTxs(pool_index, "1");
         ZJC_DEBUG("leader get to tx coming: %d", (txs_item != nullptr));
     }
