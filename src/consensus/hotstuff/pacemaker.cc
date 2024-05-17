412139201063d8589b6666bdbf94b99ce38dfad3
许飞 <feixu@zhejianglab.com>
2024-05-17 10:43:36 +0800

    fix

--- src/consensus/hotstuff/pacemaker.cc
+++ src/consensus/hotstuff/pacemaker.cc
@@ -23,7 +23,7 @@ Pacemaker::Pacemaker(
     pool_idx_(pool_idx), crypto_(c), leader_rotation_(lr), duration_(d) {
     
     high_qc_ = GetQCWrappedByGenesis();
-    high_tc_ = std::make_shared<TC>(nullptr, BeforeGenesisView);
+    high_tc_ = std::make_shared<TC>(nullptr, BeforeGenesisView, 1);
     cur_view_ = GenesisView;
 }
 
