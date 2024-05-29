24460929a324c1984b81cc0e87d888946621e7b0
tenondvpn <jhsx5679@gmail.com>
2024-05-29 17:00:51 +0800

    fix

--- src/block/block_manager.cc
+++ src/block/block_manager.cc
@@ -249,18 +249,18 @@ void BlockManager::HandleAllNewBlock() {
             db::DbWriteBatch db_batch;
             // TODO 更新 pool info，每次 AddNewBlock 之前需要更新 pool latest info
             // ZJC_DEBUG("LLLLLL handle new block coming sharding id: %u, pool: %d, height: %lu, tx size: %u", block_ptr->network_id(), block_ptr.pool_index(), block_ptr->height(), block_ptr->tx_list_size());
-            if (UpdateBlockItemToCache(block_ptr, db_batch)) {
-                ZJC_DEBUG("from network new block coming sharding id: %u, pool: %d, height: %lu, "
-                    "tx size: %u, hash: %s, elect height: %lu, tm height: %lu",
-                    block_ptr->network_id(),
-                    block_ptr->pool_index(),
-                    block_ptr->height(),
-                    block_ptr->tx_list_size(),
-                    common::Encode::HexEncode(block_ptr->hash()).c_str(),
-                    block_ptr->electblock_height(),
-                    block_ptr->timeblock_height());
-                AddNewBlock(block_ptr, db_batch);
-            }   
+            // if (UpdateBlockItemToCache(block_ptr, db_batch)) {
+            //     ZJC_DEBUG("from network new block coming sharding id: %u, pool: %d, height: %lu, "
+            //         "tx size: %u, hash: %s, elect height: %lu, tm height: %lu",
+            //         block_ptr->network_id(),
+            //         block_ptr->pool_index(),
+            //         block_ptr->height(),
+            //         block_ptr->tx_list_size(),
+            //         common::Encode::HexEncode(block_ptr->hash()).c_str(),
+            //         block_ptr->electblock_height(),
+            //         block_ptr->timeblock_height());
+            //     AddNewBlock(block_ptr, db_batch);
+            // }   
         }
     }
 
