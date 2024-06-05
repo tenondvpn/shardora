385c7e3130ee0a38ed199fb59646f9473f95d8d3
许飞 <feixu@zhejianglab.com>
2024-06-05 10:47:47 +0800

    fix

--- src/consensus/hotstuff/block_acceptor.cc
+++ src/consensus/hotstuff/block_acceptor.cc
@@ -125,7 +125,7 @@ Status BlockAcceptor::Commit(std::shared_ptr<block::protobuf::Block>& block) {
         if (elect_item && block->leader_index() == elect_item->LocalMember()->index) {
             // leader broadcast block to other shards
             // TODO to 交易会大量占用 CPU，先屏蔽
-            LeaderBroadcastBlock(block);
+            // LeaderBroadcastBlock(block);
 #ifndef NDEBUG                
             for (uint32_t i = 0; i < block->tx_list_size(); ++i) {
                 ZJC_DEBUG("leader broadcast commit block tx over step: %d, to: %s, gid: %s, net: %d, pool: %d, height: %lu", 
