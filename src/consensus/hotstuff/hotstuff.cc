038c7d480c7bc4aa2d2de3a65e04c98149baeac5
许飞 <feixu@zhejianglab.com>
2024-06-12 13:55:58 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -338,7 +338,7 @@ void Hotstuff::HandleVoteMsg(const transport::protobuf::Header& header) {
         vote_msg.replica_idx(),
         header.hash64());
     if (VerifyVoteMsg(vote_msg) != Status::kSuccess) {
-        ZJC_ERROR("vote message is error.");
+        ZJC_WARN("vote message is error.");
         return;
     }
     ZJC_DEBUG("====2.1 pool: %d, onVote, hash: %s, view: %lu",
@@ -437,35 +437,6 @@ void Hotstuff::HandleVoteMsg(const transport::protobuf::Header& header) {
             } else {
                 ZJC_DEBUG("pool: %d store verified view block success, view: %lu", pool_idx_, v_block->view);
             }
-
-#ifndef NDEBUG
-            std::shared_ptr<ViewBlock> block_info = nullptr;
-            Status st = view_block_chain()->Get(vote_msg.view_block_hash(), block_info);
-            if (st == Status::kSuccess && block_info != nullptr) {
-                for (int32_t i = 0; i < block_info->block->tx_list_size(); ++i) {
-                    ZJC_DEBUG("block net: %u, pool: %u, height: %lu, prehash: %s, hash: %s, step: %d, "
-                        "pacemaker pool: %d, highQC: %lu, highTC: %lu, chainSize: %lu, "
-                        "curView: %lu, vblock: %lu, txs: %lu",
-                        block_info->block->network_id(),
-                        block_info->block->pool_index(),
-                        block_info->block->height(),
-                        common::Encode::HexEncode(block_info->block->prehash()).c_str(),
-                        common::Encode::HexEncode(block_info->block->hash()).c_str(),
-                        block_info->block->tx_list(i).step(),
-                        pool_idx_,
-                        pacemaker()->HighQC()->view,
-                        pacemaker()->HighTC()->view,
-                        view_block_chain()->Size(),
-                        pacemaker()->CurView(),
-                        block_info->view,
-                        block_info->block->tx_list_size());
-                }
-            } else {
-                ZJC_DEBUG("failed get view block hash: %s",
-                    common::Encode::HexEncode(vote_msg.view_block_hash()).c_str());
-                assert(false);
-            }
-#endif
         }        
     }
 
@@ -817,7 +788,6 @@ Status Hotstuff::VerifyVoteMsg(const hotstuff::protobuf::VoteMsg& vote_msg) {
     uint32_t replica_idx = vote_msg.replica_idx();
 
     if (vote_msg.view() <= pacemaker()->HighQC()->view) {
-        ZJC_ERROR("view message is not exited.");
         return Status::kError;
     }
     
