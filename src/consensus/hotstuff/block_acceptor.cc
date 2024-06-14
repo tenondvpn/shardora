a8bcfa83f30861f751fe29eef7cb825d0aae7ec5
tenondvpn <jhsx5679@gmail.com>
2024-06-14 17:07:53 +0800

    fix

--- src/consensus/hotstuff/block_acceptor.cc
+++ src/consensus/hotstuff/block_acceptor.cc
@@ -125,25 +125,25 @@ Status BlockAcceptor::Commit(std::shared_ptr<block::protobuf::Block>& block) {
             common::GlobalInfo::Instance()->network_id(), 
             block->electblock_height());
         if (elect_item) {
-//             if (block->leader_index() == elect_item->LocalMember()->index) {
-//                 // leader broadcast block to other shards
-//                 // TODO to 交易会大量占用 CPU，先屏蔽
-//                 LeaderBroadcastBlock(block);
-//             }
-// #ifndef NDEBUG                
-//             for (uint32_t i = 0; i < block->tx_list_size(); ++i) {
-//                 ZJC_DEBUG("leader broadcast commit block tx over step: %d, "
-//                     "to: %s, gid: %s, net: %d, pool: %d, height: %lu, leader index: %u, local index: %u", 
-//                     block->tx_list(i).step(),
-//                     common::Encode::HexEncode(block->tx_list(i).to()).c_str(),
-//                     common::Encode::HexEncode(block->tx_list(i).gid()).c_str(),
-//                     block->network_id(),
-//                     block->pool_index(),
-//                     block->height(),
-//                     block->leader_index(),
-//                     elect_item->LocalMember()->index);
-//             }
-// #endif
+            if (block->leader_index() == elect_item->LocalMember()->index) {
+                // leader broadcast block to other shards
+                // TODO to 交易会大量占用 CPU，先屏蔽
+                LeaderBroadcastBlock(block);
+            }
+#ifndef NDEBUG                
+            for (uint32_t i = 0; i < block->tx_list_size(); ++i) {
+                ZJC_DEBUG("leader broadcast commit block tx over step: %d, "
+                    "to: %s, gid: %s, net: %d, pool: %d, height: %lu, leader index: %u, local index: %u", 
+                    block->tx_list(i).step(),
+                    common::Encode::HexEncode(block->tx_list(i).to()).c_str(),
+                    common::Encode::HexEncode(block->tx_list(i).gid()).c_str(),
+                    block->network_id(),
+                    block->pool_index(),
+                    block->height(),
+                    block->leader_index(),
+                    elect_item->LocalMember()->index);
+            }
+#endif
         }
     }
     return Status::kSuccess;
