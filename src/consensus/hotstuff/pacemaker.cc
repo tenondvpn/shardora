30f28f574c550b6784d1717d83e1d3294caeea75
tenondvpn <jhsx5679@gmail.com>
2024-06-18 11:42:55 +0800

    fix

--- src/consensus/hotstuff/pacemaker.cc
+++ src/consensus/hotstuff/pacemaker.cc
@@ -219,10 +219,12 @@ void Pacemaker::OnRemoteTimeout(const transport::MessagePtr& msg_ptr) {
     
     // 统计 bls 签名
     auto& timeout_proto = msg.hotstuff_timeout_proto();
-    if (timeout_proto.view() <= CurView()) {
+    if (timeout_proto.view() <= CurView() || 
+            timeout_proto.view() <= high_qc_->view || 
+            timeout_proto.view() <= high_tc_->view) {
         return;
     }
-    
+
     std::shared_ptr<libff::alt_bn128_G1> reconstructed_sign = nullptr;
     Status s = crypto_->ReconstructAndVerifyThresSign(
             timeout_proto.elect_height(),
