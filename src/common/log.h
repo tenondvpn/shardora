6dad1e603cf1d68957e76bee78669d262dc95211
tenondvpn <jhsx5679@gmail.com>
2024-06-18 17:49:46 +0800

    fix

--- src/common/log.h
+++ src/common/log.h
@@ -27,15 +27,12 @@
 #define ZJC_DEBUG(fmt, ...)
 #else
 #define DEBUG(fmt, ...)  do {\
-    LOG_INS.debug("[%s][%s][%d] " fmt, ZJC_LOG_FILE_NAME, __FUNCTION__, __LINE__, ## __VA_ARGS__);\
 } while (0)
 #define ZJC_DEBUG(fmt, ...)  do {\
-    LOG_INS.debug("[%s][%s][%d] " fmt, ZJC_LOG_FILE_NAME, __FUNCTION__, __LINE__, ## __VA_ARGS__);\
 } while (0)
 #endif
 
 #define ZJC_INFO(fmt, ...)  do {\
-    LOG_INS.info("[%s][%s][%d] " fmt, ZJC_LOG_FILE_NAME, __FUNCTION__, __LINE__, ## __VA_ARGS__);\
 } while (0)
 
 #define ZJC_WARN(fmt, ...)  do {\
@@ -59,15 +56,12 @@
 #define ZJC_DEBUG(fmt, ...)
 #else
 #define DEBUG(fmt, ...)  do {\
-    LOG_INS.info("[%s][%s][%d] " fmt, ZJC_LOG_FILE_NAME, __FUNCTION__, __LINE__, ## __VA_ARGS__);\
 } while (0)
 #define ZJC_DEBUG(fmt, ...)  do {\
-    LOG_INS.debug("[%s][%s][%d] " fmt, ZJC_LOG_FILE_NAME, __FUNCTION__, __LINE__, ## __VA_ARGS__);\
 } while (0)
 #endif
 
 #define ZJC_INFO(fmt, ...)  do {\
-    LOG_INS.info("[%s][%s][%d] " fmt, ZJC_LOG_FILE_NAME, __FUNCTION__, __LINE__, ## __VA_ARGS__);\
 } while (0)
 
 #define ZJC_WARN(fmt, ...)  do {\
