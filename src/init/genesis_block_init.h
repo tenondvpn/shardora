73e7c315ef6de05839cf87815ad554ff671b33e6
许飞 <feixu@zhejianglab.com>
2024-06-19 17:02:02 +0800

    fix

--- src/init/genesis_block_init.h
+++ src/init/genesis_block_init.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <consensus/hotstuff/types.h>
 #include <map>
 #include <unordered_map>
 #include <vector>
@@ -65,10 +66,12 @@ private:
     void ComputeG2sForNodes(const std::vector<std::string>& prikeys);
     int CreateShardNodesBlocks(
         std::unordered_map<uint32_t, std::string>& pool_prev_hash_map,
+        std::unordered_map<uint32_t, hotstuff::HashStr> pool_prev_vb_hash_map,
         const std::vector<GenisisNodeInfoPtr>& root_genesis_nodes,
         const std::vector<GenisisNodeInfoPtr>& cons_genesis_nodes,
         uint32_t net_id,
         pools::protobuf::StatisticTxItem& init_heights,
+        hotstuff::View* pool_latest_view,
         std::unordered_map<std::string, uint64_t> genesis_acount_balance_map); // 节点对应的余额
     uint32_t GetNetworkIdOfGenesisAddress(const std::string& address);
     const std::map<uint32_t, std::string> GetGenesisAccount(uint32_t net_id);
@@ -77,13 +80,20 @@ private:
     int GenerateRootSingleBlock(
         const std::vector<GenisisNodeInfoPtr>& genesis_nodes,
         FILE* root_gens_init_block_file,
-        uint64_t* root_pool_height);
+        uint64_t* root_pool_height,
+        hotstuff::View* root_pool_view);
     int GenerateShardSingleBlock(uint32_t sharding_id);
+    std::shared_ptr<hotstuff::ViewBlock> CreateViewBlock(
+            hotstuff::HashStr prehash,
+            hotstuff::View view,
+            const std::shared_ptr<block::protobuf::Block>& block);
     int CreateElectBlock(
         uint32_t shard_netid,
         std::string& root_pre_hash,
+        std::string& root_pre_vb_hash,
         uint64_t height,
         uint64_t prev_height,
+        hotstuff::View view,
         FILE* root_gens_init_block_file,
         const std::vector<GenisisNodeInfoPtr>& root_genesis_nodes,
         const std::vector<GenisisNodeInfoPtr>& genesis_nodes);
@@ -99,9 +109,57 @@ private:
     bool BlsAggSignBlock(
         const std::vector<GenisisNodeInfoPtr>& genesis_nodes,
         std::shared_ptr<block::protobuf::Block>& block);
+    bool BlsAggSignViewBlock(
+        const std::vector<GenisisNodeInfoPtr>& genesis_nodes,
+        const std::shared_ptr<hotstuff::ViewBlock>& vblock,
+        std::shared_ptr<libff::alt_bn128_G1>& agg_sign);
+    std::shared_ptr<hotstuff::QC> CreateCommitQC(
+            const std::vector<GenisisNodeInfoPtr>& genesis_nodes,
+            const std::shared_ptr<hotstuff::ViewBlock>& vblock);
     void SetPrevElectInfo(
         const elect::protobuf::ElectBlock& elect_block,
         block::protobuf::BlockTx& block_tx);
+    void StoreViewBlockWithCommitQC(
+            const std::shared_ptr<hotstuff::ViewBlock>& view_block,
+            const std::shared_ptr<hotstuff::QC>& commit_qc,
+            db::DbWriteBatch* db_batch) {
+        auto pb_v_block = std::make_shared<view_block::protobuf::ViewBlockItem>();
+        hotstuff::ViewBlock2Proto(view_block, pb_v_block.get());
+        // // 不存储 block 部分，block 已经单独存过了
+        // pb_v_block->clear_block_info();
+        // 保存 v_block 对应的 qc 到 db
+        pb_v_block->set_self_commit_qc_str(commit_qc->Serialize());
+        
+        prefix_db_->SaveViewBlockInfo(view_block->block->network_id(),
+            view_block->block->pool_index(),
+            view_block->block->height(),
+            *pb_v_block,
+            *db_batch);
+        auto st = db_->Put(*db_batch);
+        assert(st.ok());        
+    }
+
+    std::string SerializeViewBlockWithCommitQC(
+            const std::shared_ptr<hotstuff::ViewBlock>& view_block,
+            const std::shared_ptr<hotstuff::QC>& commit_qc) {
+        auto pb_v_block = std::make_shared<view_block::protobuf::ViewBlockItem>();
+        hotstuff::ViewBlock2Proto(view_block, pb_v_block.get());
+        pb_v_block->set_self_commit_qc_str(commit_qc->Serialize());
+        return pb_v_block->SerializeAsString();
+    }
+
+    bool UnserializeViewBlockWithCommitQC(
+            const std::string str,
+            std::shared_ptr<hotstuff::ViewBlock>& view_block,
+            std::shared_ptr<hotstuff::QC>& commit_qc) {
+        auto pb_v_block = std::make_shared<view_block::protobuf::ViewBlockItem>();
+        if (!pb_v_block->ParseFromString(str)) {
+            return false;
+        }
+        hotstuff::Proto2ViewBlock(*pb_v_block, view_block);
+        commit_qc->Unserialize(pb_v_block->self_commit_qc_str());
+        return true;
+    }    
 
     std::map<uint32_t, std::map<uint32_t, std::string>> net_pool_index_map_; // net => (pool => addr)
     std::map<uint32_t, std::string> root_account_with_pool_index_map_;
