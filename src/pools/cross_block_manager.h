7afa8bfdff7ad4043b1dcb522e56d9e573b3ae19
tenondvpn <jhsx5679@gmail.com>
2024-06-19 14:41:50 +0800

    fix cross

--- src/pools/cross_block_manager.h
+++ src/pools/cross_block_manager.h
@@ -217,7 +217,7 @@ private:
     volatile uint32_t max_sharding_id_ = 3;
     common::Tick tick_;
     volatile uint64_t cross_synced_max_heights_[network::kConsensusShardEndNetworkId] = { common::kInvalidUint64 };
-    volatile uint64_t cross_checked_max_heights_[network::kConsensusShardEndNetworkId] = { common::kInvalidUint64 };
+    volatile uint64_t cross_checked_max_heights_[network::kConsensusShardEndNetworkId] = { 1llu };
     bool inited_heights_ = false;
 
     DISALLOW_COPY_AND_ASSIGN(CrossBlockManager);
