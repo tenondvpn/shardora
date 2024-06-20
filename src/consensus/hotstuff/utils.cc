b87da0d425fb24d4ac90ab52a990f65f5a1901d4
许飞 <feixu@zhejianglab.com>
2024-06-20 10:05:02 +0800

    fix

--- src/consensus/hotstuff/utils.cc
+++ src/consensus/hotstuff/utils.cc
@@ -19,6 +19,12 @@ std::string GetTxMessageHash(const block::protobuf::BlockTx& tx_info) {
     message.append(std::string((char*)&gas_price, sizeof(gas_price)));
     uint32_t step = tx_info.step();
     message.append(std::string((char*)&step, sizeof(step)));
+
+    uint64_t gas_used = tx_info.gas_used();
+    message.append(std::string((char*)&gas_used, sizeof(gas_used)));
+    uint32_t status = tx_info.status();
+    message.append(std::string((char*)&status, sizeof(status)));    
+    
     for (int32_t i = 0; i < tx_info.storages_size(); ++i) {
         message.append(tx_info.storages(i).key());
         message.append(tx_info.storages(i).value());
