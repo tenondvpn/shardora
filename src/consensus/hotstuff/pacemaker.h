72ebfa5dadf948d4f774670799b8f1d5a0596142
tenondvpn <jhsx5679@gmail.com>
2024-07-01 14:53:43 +0800

    fix

--- src/consensus/hotstuff/pacemaker.h
+++ src/consensus/hotstuff/pacemaker.h
@@ -90,10 +90,6 @@ private:
     inline void StartTimeoutTimer() {
         last_time_us_ = common::TimeUtils::TimestampUs();
         duration_us_ = duration_->Duration();
-        if (pool_idx_ == 0) {
-            duration_us_ = 10000000lu;
-        }
-        
         ZJC_INFO("pool: %d duration is %lu ms", pool_idx_, duration_us_/1000);
     }
 
