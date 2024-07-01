0783b4a0785bcf8a656d7669c93ce2d67f6d9446
tenondvpn <jhsx5679@gmail.com>
2024-07-01 10:59:17 +0800

    fix

--- src/block/block_manager.cc
+++ src/block/block_manager.cc
@@ -1896,8 +1896,7 @@ void BlockManager::CreateToTx() {
     }
 
     if (latest_to_tx_ != nullptr &&
-            latest_to_tx_->to_tx != nullptr &&
-            latest_to_tx_->to_tx->timeout > now_tm_ms) {
+            latest_to_tx_->to_tx != nullptr) {
         return;
     }
 
