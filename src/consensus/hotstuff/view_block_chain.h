a24c33e7bc61b79d4bc3cdbb96b8cdecedd92d06
谢磊 <xielei@zhejianglab.org>
2024-10-12 13:36:53 +0800

    fix

--- src/consensus/hotstuff/view_block_chain.h
+++ src/consensus/hotstuff/view_block_chain.h
@@ -394,9 +394,13 @@ private:
     void AddChildrenToMap(const HashStr& parent_hash, const std::shared_ptr<ViewBlock>& view_block) {
         auto it = view_blocks_info_.find(parent_hash);
         if (it == view_blocks_info_.end()) {
-            ZJC_DEBUG("failed find parent hash: %s", common::Encode::HexEncode(parent_hash).c_str());
-            assert(false);
-            return;
+            if (latest_committed_block_ == nullptr ||
+                    latest_committed_block_->qc().view_block_hash() != parent_hash) {
+                ZJC_DEBUG("failed find parent hash: %s",
+                    common::Encode::HexEncode(parent_hash).c_str());
+                assert(false);
+                return;
+            }
         }
 
         ZJC_DEBUG("success find parent hash: %s", common::Encode::HexEncode(parent_hash).c_str());
