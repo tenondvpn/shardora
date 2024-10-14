a4494b705313f6ba3dee4b44dac0bff58a8b0ce6
谢磊 <xielei@zhejianglab.org>
2024-10-14 15:27:36 +0800

    fix

--- src/consensus/hotstuff/hotstuff.h
+++ src/consensus/hotstuff/hotstuff.h
@@ -130,7 +130,6 @@ public:
             view_block_chain()->SetLatestCommittedBlock(vblock);        
         }
 
-        view_block_chain()->Store(vblock, true, nullptr);
         view_block_chain()->UpdateHighViewBlock(vblock->qc());
     }
 
