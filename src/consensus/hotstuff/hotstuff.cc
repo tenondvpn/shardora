3174457f9311b890343bb56a922a56bf29739f48
谢磊 <xielei@zhejianglab.org>
2024-10-24 14:13:55 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -806,7 +806,7 @@ Status Hotstuff::HandleProposeMsgStep_ChainStore(std::shared_ptr<ProposeMsgWrapp
 Status Hotstuff::HandleProposeMsgStep_Vote(std::shared_ptr<ProposeMsgWrapper>& pro_msg_wrap) {
     ZJC_DEBUG("HandleProposeMsgStep_Vote called hash: %lu", pro_msg_wrap->msg_ptr->header.hash64());
     // NOTICE: pipeline 重试时，protobuf 结构体被析构，因此 pro_msg_wrap->header.hash64() 是 0
-    ZJC_INFO("pacemaker pool: %d, highQC: %lu, highTC: %lu, chainSize: %lu, "
+    ZJC_DEBUG("pacemaker pool: %d, highQC: %lu, highTC: %lu, chainSize: %lu, "
         "curView: %lu, vblock: %lu, txs: %lu, hash64: %lu, propose_debug: %s",
         pool_idx_,
         view_block_chain()->HighViewBlock()->qc().view(),
