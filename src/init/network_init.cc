3abf5be6c9e2e734abd512f808711a6c452f7408
谢磊 <xielei@zhejianglab.org>
2024-10-24 22:48:26 +0800

    fix

--- src/init/network_init.cc
+++ src/init/network_init.cc
@@ -1086,9 +1086,9 @@ void NetworkInit::AddBlockItemToCache(
     // one block must be one consensus pool
     const auto& tx_list = block->tx_list();
     for (int32_t i = 0; i < tx_list.size(); ++i) {
-        if (tx_list[i].status() != consensus::kConsensusSuccess) {
-            continue;
-        }
+        // if (tx_list[i].status() != consensus::kConsensusSuccess) {
+        //     continue;
+        // }
 
         switch (tx_list[i].step()) {
         case pools::protobuf::kNormalFrom:
