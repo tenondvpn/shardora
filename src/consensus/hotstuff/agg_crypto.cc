e514a1053de3706b104d0f3eb7dc5baf2ea2e2b6
许飞 <feixu@zhejianglab.com>
2024-10-24 16:32:40 +0800

    fix

--- src/consensus/hotstuff/agg_crypto.cc
+++ src/consensus/hotstuff/agg_crypto.cc
@@ -179,7 +179,7 @@ Status AggCrypto::VerifyMessage(const transport::MessagePtr& msg_ptr) {
         !msg_ptr->header.hotstuff().pro_msg().has_view_item()) {
         return Status::kInvalidArgument;
     }
-    auto mem_ptr = elect_item->GetMemberByIdx(msg_ptr->header.hotstuff().pro_msg().view_item().leader_idx());
+    auto mem_ptr = elect_item->GetMemberByIdx(msg_ptr->header.hotstuff().pro_msg().view_item().qc().leader_idx());
     if (mem_ptr->bls_publick_key == libff::alt_bn128_G2::zero()) {
         ZJC_DEBUG("verify sign failed, backup invalid bls pk: %s",
             common::Encode::HexEncode(mem_ptr->id).c_str());
