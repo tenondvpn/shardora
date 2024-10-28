22772beaf07399a84cb9ff1b8d162db261b39de2
谢磊 <xielei@zhejianglab.org>
2024-10-28 10:51:59 +0800

    fix

--- src/zjcvm/zjc_host.h
+++ src/zjcvm/zjc_host.h
@@ -9,6 +9,8 @@
 
 #include <evmc/evmc.hpp>
 
+#include "common/encode.h"
+
 namespace shardora {
 
 namespace contract {
@@ -108,6 +110,9 @@ public:
     int SaveKeyValue(const evmc::address& addr, const std::string& key, const std::string& val);
     int GetKeyValue(const std::string& id, const std::string& key, std::string* val);
     void SavePrevStorages(const std::string& key, const std::string& val) {
+        ZJC_DEBUG("success add prev storage key: %s, value: %s",
+            common::Encode::HexEncode(key).c_str(), 
+            common::Encode::HexEncode(val).c_str());
         prev_storages_map_[key] = val;
     }
     
