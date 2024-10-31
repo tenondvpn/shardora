ede942ac0a14d0d75341dc6045cd6cbba75653bb
谢磊 <xielei@zhejianglab.org>
2024-10-31 10:27:00 +0800

    fix

--- src/common/utils.h
+++ src/common/utils.h
@@ -382,6 +382,7 @@ static inline void CheckThreadIdValid() {
     if (count > 3) {
         static std::thread::id init_thread_id = std::this_thread::get_id();
         auto now_thread_id = std::this_thread::get_id();
+        ZJC_DEBUG("now handle thread id: %u, old: %u", now_thread_id, init_thread_id);
         assert(init_thread_id == now_thread_id);
     }
 #endif
