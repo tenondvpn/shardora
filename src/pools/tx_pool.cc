356d96d947a13fd2a6793328a6b8b7af84cfe0b2
谢磊 <xielei@zhejianglab.org>
2024-10-31 13:42:24 +0800

    fix

--- src/pools/tx_pool.cc
+++ src/pools/tx_pool.cc
@@ -128,7 +128,7 @@ uint32_t TxPool::SyncMissingBlocks(uint64_t now_tm_ms) {
 
 int TxPool::AddTx(TxItemPtr& tx_ptr) {
 //     common::AutoSpinLock auto_lock(mutex_);
-    common::CheckThreadIdValid();
+    CheckThreadIdValid();
     if (removed_gid_.find(tx_ptr->tx_info.gid()) != removed_gid_.end()) {
 #ifndef ENABLE_HOTSTUFF        
         // assert(false);
@@ -188,7 +188,7 @@ void TxPool::GetTx(
         const std::map<std::string, pools::TxItemPtr>& invalid_txs, 
         transport::protobuf::Header& header,
         uint32_t count) {
-    common::CheckThreadIdValid();
+    CheckThreadIdValid();
     std::vector<TxItemPtr> recover_txs;
     zbft::protobuf::TxBft* txbft = header.mutable_zbft()->mutable_tx_bft();
     auto iter = prio_map_.begin();
@@ -214,7 +214,7 @@ void TxPool::GetTx(
         std::map<std::string, TxItemPtr>& res_map, 
         uint32_t count, 
         std::unordered_map<std::string, std::string>& kvs) {
-    common::CheckThreadIdValid();
+    CheckThreadIdValid();
     ZJC_DEBUG("leader get tx universal_prio_map_: %u, prio_map_: %u, consensus_tx_map_: %u", 
         universal_prio_map_.size(), prio_map_.size(), consensus_tx_map_.size());
     GetTx(universal_prio_map_, res_map, count, kvs);
@@ -231,7 +231,7 @@ void TxPool::GetTxIdempotently(
         uint32_t count, 
         std::unordered_map<std::string, std::string>& kvs,
         pools::CheckGidValidFunction gid_vlid_func) {
-    common::CheckThreadIdValid();
+    CheckThreadIdValid();
     ZJC_DEBUG("now get tx universal_prio_map_ size: %u, prio_map_: %u, consensus_tx_map_: %u",
         universal_prio_map_.size(),
         prio_map_.size(),
@@ -288,7 +288,7 @@ void TxPool::GetTxIdempotently(
 void TxPool::GetTxByIds(
         const std::vector<std::string>& gids,
         std::map<std::string, TxItemPtr>& res_map) {
-    common::CheckThreadIdValid();
+    CheckThreadIdValid();
     for (const auto& gid : gids) {
         auto it = gid_map_.find(gid);
         if (it == gid_map_.end()) {
@@ -447,7 +447,7 @@ void TxPool::RemoveTx(const std::string& gid) {
 }
 
 void TxPool::TxOver(const google::protobuf::RepeatedPtrField<block::protobuf::BlockTx>& tx_list) {
-    common::CheckThreadIdValid();
+    CheckThreadIdValid();
     for (int32_t i = 0; i < tx_list.size(); ++i) {
         auto& gid = tx_list[i].gid(); 
         RemoveTx(gid);
@@ -649,7 +649,7 @@ uint64_t TxPool::UpdateLatestInfo(
         const std::string& hash,
         const std::string& prehash,
         const uint64_t timestamp) {
-    common::CheckThreadIdValid();
+    CheckThreadIdValid();
     if (!height_tree_ptr_) {
         InitHeightTree();
     }
