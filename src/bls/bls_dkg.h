afd1c825bf8c913a7601d558daa360e27ca7437b
许飞 <feixu@zhejianglab.com>
2024-11-05 14:57:43 +0800

    fix

--- src/bls/bls_dkg.h
+++ src/bls/bls_dkg.h
@@ -79,21 +79,18 @@ public:
     void TimerMessage();
 
     static std::string serializeCommonPk(const libff::alt_bn128_G2& common_pk) {
-        auto x = libBLS::ThresholdUtils::fieldElementToString(common_pk.X);
-        auto y = libBLS::ThresholdUtils::fieldElementToString(common_pk.Y);
-        return "("+x+","+y+")";
-        // auto pk = std::make_shared<bls::protobuf::BlsPublicKey>();
+        auto pk = std::make_shared<bls::protobuf::BlsPublicKey>();
 
-        // pk->set_x_c0(
-        //         libBLS::ThresholdUtils::fieldElementToString(common_pk.X.c0));
-        // pk->set_x_c1(
-        //         libBLS::ThresholdUtils::fieldElementToString(common_pk.X.c1));
-        // pk->set_y_c0(
-        //         libBLS::ThresholdUtils::fieldElementToString(common_pk.Y.c0));
-        // pk->set_y_c1(
-        //         libBLS::ThresholdUtils::fieldElementToString(common_pk.Y.c1));
+        pk->set_x_c0(
+                libBLS::ThresholdUtils::fieldElementToString(common_pk.X.c0));
+        pk->set_x_c1(
+                libBLS::ThresholdUtils::fieldElementToString(common_pk.X.c1));
+        pk->set_y_c0(
+                libBLS::ThresholdUtils::fieldElementToString(common_pk.Y.c0));
+        pk->set_y_c1(
+                libBLS::ThresholdUtils::fieldElementToString(common_pk.Y.c1));
 
-        // return pk->SerializeAsString();
+        return pk->SerializeAsString();
     }    
 private:
     void HandleVerifyBroadcast(const transport::MessagePtr& header);
