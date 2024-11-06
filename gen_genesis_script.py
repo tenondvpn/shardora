b6b449ba54268b74c2de1dded918c72398d7562d
许飞 <feixu@zhejianglab.com>
2024-11-06 11:07:05 +0800

    fix

--- gen_genesis_script.py
+++ gen_genesis_script.py
@@ -140,7 +140,7 @@ def gen_zjnodes(server_conf: dict, zjnodes_folder):
                 'net_id': node['net'],
                 'prikey': sk,
                 'show_cmd': 0,
-                'for_ck': True
+                'for_ck': False
             },
             'tx_block': {
                 'network_id': node['net']
