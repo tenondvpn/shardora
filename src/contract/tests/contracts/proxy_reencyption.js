b810c327f15290a9054e941bb843e501564b1ca9
谢磊 <xielei@zhejianglab.org>
2024-11-07 10:22:52 +0800

    fix

--- src/contract/tests/contracts/proxy_reencyption.js
+++ src/contract/tests/contracts/proxy_reencyption.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 const util = require('util')
 const kTestSellerCount = 11;  // real: kTestSellerCount - 10
 const kTestBuyerCount = 11;  // real: kTestBuyerCount - 10
-const contract_address = "48e1eab96c9e759daa3aff82b40e77cd615a41d3";
+const contract_address = "48e1eab96c9e759daa3aff82b40e77cd615a41d4";
 
 function str_to_hex(str) {
     var arr1 = [];
