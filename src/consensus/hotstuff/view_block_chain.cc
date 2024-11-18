c3419895fc71ed96c6f512c2209ff170d3b45348
谢磊 <xielei@zhejianglab.org>
2024-11-18 15:33:58 +0800

    fix

--- src/consensus/hotstuff/view_block_chain.cc
+++ src/consensus/hotstuff/view_block_chain.cc
@@ -116,7 +116,7 @@ Status ViewBlockChain::Store(
             for (auto iter = view_block_at_height_vec.begin(); iter != view_block_at_height_vec.end();) {
                 if (IsQcTcValid((*iter)->qc())) {                    
                     ZJC_DEBUG("invalid view has much more view block: %lu, "
-                        "count: %u, %u_%u_%lu, %lu hash: %s, , %u_%u_%lu, %lu new block hash: %s", 
+                        "count: %u, %u_%u_%lu, %lu hash: %s, sign x: %s, %u_%u_%lu, %lu new block hash: %s", 
                         view_block->qc().view(),
                         view_block_at_height_vec.size(),
                         (*iter)->qc().network_id(),
@@ -124,6 +124,7 @@ Status ViewBlockChain::Store(
                         (*iter)->qc().view(),
                         (*iter)->block_info().height(),
                         common::Encode::HexEncode((*iter)->qc().view_block_hash()).c_str(),
+                        common::Encode::HexEncode((*iter)->qc().sign_x()).c_str(),
                         view_block->qc().network_id(),
                         view_block->qc().pool_index(),
                         view_block->qc().view(),
