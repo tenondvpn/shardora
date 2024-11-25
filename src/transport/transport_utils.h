74ee73dca23f96683266fda4c358d36d2e20f433
谢磊 <xielei@zhejianglab.org>
2024-11-25 10:40:32 +0800

    fix

--- src/transport/transport_utils.h
+++ src/transport/transport_utils.h
@@ -24,11 +24,11 @@
 #define TRANSPORT_ERROR(fmt, ...) ZJC_ERROR("[transport]" fmt, ## __VA_ARGS__)
 
 #ifndef NDEBUG
-#define ADD_DEBUG_PROCESS_TIMESTAMP(debug_str) {\
+#define ADD_DEBUG_PROCESS_TIMESTAMP(tdebug_str) { \
     auto btime = common::TimeUtils::TimestampUs(); \
     uint64_t diff_time = 0; \
     if (msg_ptr->times_idx > 0) { diff_time = btime - msg_ptr->times[msg_ptr->times_idx - 1]; } \
-    std::string tmp_str = common::StringUtil::Format("%s:%s:%u: %s, diff time: %lu", ZJC_LOG_FILE_NAME,  __FUNCTION__, __LINE__, debug_str, diff_time); \
+    std::string tmp_str = common::StringUtil::Format("%s:%s:%u: %s, diff time: %lu", ZJC_LOG_FILE_NAME,  __FUNCTION__, __LINE__, tdebug_str, diff_time); \
     msg_ptr->times[msg_ptr->times_idx] = btime; \
     msg_ptr->debug_str[msg_ptr->times_idx] = tmp_str; \
     msg_ptr->times_idx++; \
