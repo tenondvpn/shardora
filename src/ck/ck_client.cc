ee9ef035f6724d0612ff3e81ecdae546019ea2f7
谢磊 <xielei@zhejianglab.org>
2024-11-28 16:44:54 +0800

    fix

--- src/ck/ck_client.cc
+++ src/ck/ck_client.cc
@@ -366,111 +366,111 @@ bool ClickHouseClient::HandleNewBlock(const std::shared_ptr<hotstuff::ViewBlock>
 // }
 
 void ClickHouseClient::FlushToCkWithData() try {
-    if (batch_count_ <= 0) {
-        return;
-    }
 
     auto now_tm_ms = common::TimeUtils::TimestampMs();
     if (batch_count_ >= kBatchCountToCk || (pre_time_out_ + 3000 < now_tm_ms)) {
-        clickhouse::Block trans;
-        clickhouse::Block blocks;
-        clickhouse::Block accounts;
-        clickhouse::Block account_attrs;
-        clickhouse::Block c2cs;
-        clickhouse::Block prepay;
-        blocks.AppendColumn("shard_id", block_shard_id);
-        blocks.AppendColumn("pool_index", block_pool_index);
-        blocks.AppendColumn("height", block_height);
-        blocks.AppendColumn("prehash", block_prehash);
-        blocks.AppendColumn("hash", block_hash);
-        blocks.AppendColumn("version", block_version);
-        blocks.AppendColumn("vss", block_vss);
-        blocks.AppendColumn("elect_height", block_elect_height);
-        blocks.AppendColumn("bitmap", block_bitmap);
-        blocks.AppendColumn("timestamp", block_timestamp);
-        blocks.AppendColumn("timeblock_height", block_timeblock_height);
-        blocks.AppendColumn("bls_agg_sign_x", block_bls_agg_sign_x);
-        blocks.AppendColumn("bls_agg_sign_y", block_bls_agg_sign_y);
-        blocks.AppendColumn("commit_bitmap", block_commit_bitmap);
-        blocks.AppendColumn("date", block_date);
-        blocks.AppendColumn("tx_size", block_tx_size);
+        if (batch_count_ > 0) {
+            clickhouse::Block trans;
+            clickhouse::Block blocks;
+            clickhouse::Block accounts;
+            clickhouse::Block account_attrs;
+            clickhouse::Block c2cs;
+            clickhouse::Block prepay;
+            blocks.AppendColumn("shard_id", block_shard_id);
+            blocks.AppendColumn("pool_index", block_pool_index);
+            blocks.AppendColumn("height", block_height);
+            blocks.AppendColumn("prehash", block_prehash);
+            blocks.AppendColumn("hash", block_hash);
+            blocks.AppendColumn("version", block_version);
+            blocks.AppendColumn("vss", block_vss);
+            blocks.AppendColumn("elect_height", block_elect_height);
+            blocks.AppendColumn("bitmap", block_bitmap);
+            blocks.AppendColumn("timestamp", block_timestamp);
+            blocks.AppendColumn("timeblock_height", block_timeblock_height);
+            blocks.AppendColumn("bls_agg_sign_x", block_bls_agg_sign_x);
+            blocks.AppendColumn("bls_agg_sign_y", block_bls_agg_sign_y);
+            blocks.AppendColumn("commit_bitmap", block_commit_bitmap);
+            blocks.AppendColumn("date", block_date);
+            blocks.AppendColumn("tx_size", block_tx_size);
 
-        trans.AppendColumn("shard_id", shard_id);
-        trans.AppendColumn("pool_index", pool_index);
-        trans.AppendColumn("height", height);
-        trans.AppendColumn("prehash", prehash);
-        trans.AppendColumn("hash", hash);
-        trans.AppendColumn("version", version);
-        trans.AppendColumn("vss", vss);
-        trans.AppendColumn("elect_height", elect_height);
-        trans.AppendColumn("bitmap", bitmap);
-        trans.AppendColumn("timestamp", timestamp);
-        trans.AppendColumn("timeblock_height", timeblock_height);
-        trans.AppendColumn("bls_agg_sign_x", bls_agg_sign_x);
-        trans.AppendColumn("bls_agg_sign_y", bls_agg_sign_y);
-        trans.AppendColumn("commit_bitmap", commit_bitmap);
-        trans.AppendColumn("gid", gid);
-        trans.AppendColumn("from", from);
-        trans.AppendColumn("from_pubkey", from_pubkey);
-        trans.AppendColumn("from_sign", from_sign);
-        trans.AppendColumn("to", to);
-        trans.AppendColumn("amount", amount);
-        trans.AppendColumn("gas_limit", gas_limit);
-        trans.AppendColumn("gas_used", gas_used);
-        trans.AppendColumn("gas_price", gas_price);
-        trans.AppendColumn("balance", balance);
-        trans.AppendColumn("to_add", to_add);
-        trans.AppendColumn("type", type);
-        trans.AppendColumn("attrs", attrs);
-        trans.AppendColumn("status", status);
-        trans.AppendColumn("tx_hash", tx_hash);
-        trans.AppendColumn("call_contract_step", call_contract_step);
-        trans.AppendColumn("storages", storages);
-        trans.AppendColumn("transfers", transfers);
-        trans.AppendColumn("date", date);
+            trans.AppendColumn("shard_id", shard_id);
+            trans.AppendColumn("pool_index", pool_index);
+            trans.AppendColumn("height", height);
+            trans.AppendColumn("prehash", prehash);
+            trans.AppendColumn("hash", hash);
+            trans.AppendColumn("version", version);
+            trans.AppendColumn("vss", vss);
+            trans.AppendColumn("elect_height", elect_height);
+            trans.AppendColumn("bitmap", bitmap);
+            trans.AppendColumn("timestamp", timestamp);
+            trans.AppendColumn("timeblock_height", timeblock_height);
+            trans.AppendColumn("bls_agg_sign_x", bls_agg_sign_x);
+            trans.AppendColumn("bls_agg_sign_y", bls_agg_sign_y);
+            trans.AppendColumn("commit_bitmap", commit_bitmap);
+            trans.AppendColumn("gid", gid);
+            trans.AppendColumn("from", from);
+            trans.AppendColumn("from_pubkey", from_pubkey);
+            trans.AppendColumn("from_sign", from_sign);
+            trans.AppendColumn("to", to);
+            trans.AppendColumn("amount", amount);
+            trans.AppendColumn("gas_limit", gas_limit);
+            trans.AppendColumn("gas_used", gas_used);
+            trans.AppendColumn("gas_price", gas_price);
+            trans.AppendColumn("balance", balance);
+            trans.AppendColumn("to_add", to_add);
+            trans.AppendColumn("type", type);
+            trans.AppendColumn("attrs", attrs);
+            trans.AppendColumn("status", status);
+            trans.AppendColumn("tx_hash", tx_hash);
+            trans.AppendColumn("call_contract_step", call_contract_step);
+            trans.AppendColumn("storages", storages);
+            trans.AppendColumn("transfers", transfers);
+            trans.AppendColumn("date", date);
 
-        accounts.AppendColumn("id", acc_account);
-        accounts.AppendColumn("shard_id", acc_shard_id);
-        accounts.AppendColumn("pool_index", acc_pool_index);
-        accounts.AppendColumn("balance", acc_balance);
+            accounts.AppendColumn("id", acc_account);
+            accounts.AppendColumn("shard_id", acc_shard_id);
+            accounts.AppendColumn("pool_index", acc_pool_index);
+            accounts.AppendColumn("balance", acc_balance);
 
-        account_attrs.AppendColumn("from", attr_account);
-        account_attrs.AppendColumn("shard_id", attr_shard_id);
-        account_attrs.AppendColumn("key", attr_key);
-        account_attrs.AppendColumn("value", attr_value);
-        account_attrs.AppendColumn("to", attr_to);
-        account_attrs.AppendColumn("type", attr_tx_type);
+            account_attrs.AppendColumn("from", attr_account);
+            account_attrs.AppendColumn("shard_id", attr_shard_id);
+            account_attrs.AppendColumn("key", attr_key);
+            account_attrs.AppendColumn("value", attr_value);
+            account_attrs.AppendColumn("to", attr_to);
+            account_attrs.AppendColumn("type", attr_tx_type);
 
-        c2cs.AppendColumn("seller", c2c_seller);
-        c2cs.AppendColumn("all", c2c_all);
-        c2cs.AppendColumn("now", c2c_now);
-        c2cs.AppendColumn("receivable", c2c_r);
-        c2cs.AppendColumn("mchecked", c2c_mc);
-        c2cs.AppendColumn("schecked", c2c_sc);
-        c2cs.AppendColumn("reported", c2c_report);
-        c2cs.AppendColumn("orderId", c2c_order_id);
-        c2cs.AppendColumn("height", c2c_height);
-        c2cs.AppendColumn("buyer", c2c_buyer);
-        c2cs.AppendColumn("amount", c2c_amount);
-        c2cs.AppendColumn("contract", c2c_contract_addr);
+            c2cs.AppendColumn("seller", c2c_seller);
+            c2cs.AppendColumn("all", c2c_all);
+            c2cs.AppendColumn("now", c2c_now);
+            c2cs.AppendColumn("receivable", c2c_r);
+            c2cs.AppendColumn("mchecked", c2c_mc);
+            c2cs.AppendColumn("schecked", c2c_sc);
+            c2cs.AppendColumn("reported", c2c_report);
+            c2cs.AppendColumn("orderId", c2c_order_id);
+            c2cs.AppendColumn("height", c2c_height);
+            c2cs.AppendColumn("buyer", c2c_buyer);
+            c2cs.AppendColumn("amount", c2c_amount);
+            c2cs.AppendColumn("contract", c2c_contract_addr);
 
-        prepay.AppendColumn("contract", prepay_contract);
-        prepay.AppendColumn("user", prepay_user);
-        prepay.AppendColumn("prepayment", prepay_amount);
-        prepay.AppendColumn("height", prepay_height);
+            prepay.AppendColumn("contract", prepay_contract);
+            prepay.AppendColumn("user", prepay_user);
+            prepay.AppendColumn("prepayment", prepay_amount);
+            prepay.AppendColumn("height", prepay_height);
 
-        uint32_t idx = 0;
-        clickhouse::Client ck_client(clickhouse::ClientOptions().
-            SetHost(common::GlobalInfo::Instance()->ck_host()).
-            SetPort(common::GlobalInfo::Instance()->ck_port()).
-            SetUser(common::GlobalInfo::Instance()->ck_user()).
-            SetPassword(common::GlobalInfo::Instance()->ck_pass()));
-        ck_client.Insert(kClickhouseTransTableName, trans);
-        ck_client.Insert(kClickhouseBlockTableName, blocks);
-        ck_client.Insert(kClickhouseAccountTableName, accounts);
-        ck_client.Insert(kClickhouseAccountKvTableName, account_attrs);
-        ck_client.Insert(kClickhouseC2cTableName, c2cs);
-        ck_client.Insert(kClickhousePrepaymentTableName, prepay);
+            uint32_t idx = 0;
+            clickhouse::Client ck_client(clickhouse::ClientOptions().
+                SetHost(common::GlobalInfo::Instance()->ck_host()).
+                SetPort(common::GlobalInfo::Instance()->ck_port()).
+                SetUser(common::GlobalInfo::Instance()->ck_user()).
+                SetPassword(common::GlobalInfo::Instance()->ck_pass()));
+            ck_client.Insert(kClickhouseTransTableName, trans);
+            ck_client.Insert(kClickhouseBlockTableName, blocks);
+            ck_client.Insert(kClickhouseAccountTableName, accounts);
+            ck_client.Insert(kClickhouseAccountKvTableName, account_attrs);
+            ck_client.Insert(kClickhouseC2cTableName, c2cs);
+            ck_client.Insert(kClickhousePrepaymentTableName, prepay);
+        }
+
         HandleBlsMessage();
         ZJC_DEBUG("success flush to db: %u", batch_count_);
         batch_count_ = 0;
