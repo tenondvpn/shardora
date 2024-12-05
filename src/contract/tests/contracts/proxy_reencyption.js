49468cd85624218cdacd1267802c0289a0c6014c
谢磊 <xielei@zhejianglab.org>
2024-12-05 17:24:31 +0800

    fix

--- src/contract/tests/contracts/proxy_reencyption.js
+++ src/contract/tests/contracts/proxy_reencyption.js
@@ -221,7 +221,7 @@ function create_tx(str_prikey, to, amount, gas_limit, gas_price, prepay, tx_type
     var buffer_array = [Buffer.from(gid, 'hex'),
         Buffer.from(frompk, 'hex'),
         Buffer.from(to, 'hex'),
-        amount_buf, gas_limit_buf, gas_price_buf, step_buf];
+        amount_buf, gas_limit_buf, gas_price_buf, step_buf, prepay_buf];
     if (key != null && key != "") {
         buffer_array.push(Buffer.from(key));
         if (value != null && value != "") {
