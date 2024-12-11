3f54609690fb5a322da87408c449f12a793f2acf
许飞 <feixu@zhejianglab.com>
2024-12-11 16:09:13 +0800

    fix

--- src/main/tx_cli.cc
+++ src/main/tx_cli.cc
@@ -16,10 +16,10 @@ static bool global_stop = false;
 static const std::string kBroadcastIp = "127.0.0.1";
 static const uint16_t kBroadcastPort = 13001;
 static const int shardnum = 3;
-static const int delayus = 10000;
-static const bool multi_pool = true;
+static const int delayus = 2000;
+static const bool multi_pool = false;
 static const std::string db_path = "./txclidb";
-static const std::string from_prikey = "cefc2c33064ea7691aee3e5e4f7842935d26f3ad790d81cf015e79b78958e848";   
+static const std::string from_prikey = "b5039128131f96f6164a33bc7fbc48c2f5cf425e8476b1c4d0f4d186fbd0d708";   
 
 static void SignalCallback(int sig_int) { global_stop = true; }
 
