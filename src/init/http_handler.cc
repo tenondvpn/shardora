0f8135a6e314be06add7e9da37ff89e69ee179d5
谢磊 <xielei@zhejianglab.org>
2024-12-11 15:02:57 +0800

    fix

--- src/init/http_handler.cc
+++ src/init/http_handler.cc
@@ -3,6 +3,7 @@
 #include <functional>
 
 #include <cpppbc/GT.h>
+#include <cpppbc/Pairing.h>
 #include <json/json.hpp>
 
 #include "common/encode.h"
@@ -564,6 +565,16 @@ static void GetSecAndEncData(evhtp_request_t* req, void* req_data) {
     }
 
     std::string test_data(data);
+    std::string param = ("type a\n"
+        "q 8780710799663312522437781984754049815806883199414208211028653399266475630880222957078625179422662221423155858769582317459277713367317481324925129998224791\n"
+        "h 12016012264891146079388821366740534204802954401251311822919615131047207289359704531102844802183906537786776\n"
+        "r 730750818665451621361119245571504901405976559617\n"
+        "exp2 159\n"
+        "exp1 107\n"
+        "sign1 1\n"
+        "sign0 1\n");
+    auto pairing_ptr = std::make_shared<Pairing>(param.c_str(), param.size());
+    auto& e = *pairing_ptr;
     GT m(e, test_data.c_str(), test_data.size());
     ZJC_WARN("get m data: %s, %s, %s, %s", 
         test_data.c_str(), 
