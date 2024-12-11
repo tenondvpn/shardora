87e6a7620c962fe6885c1d87837125bd45957f4c
谢磊 <xielei@zhejianglab.org>
2024-12-11 22:22:40 +0800

    fix

--- src/init/http_handler.cc
+++ src/init/http_handler.cc
@@ -564,7 +564,7 @@ static void GetSecAndEncData(evhtp_request_t* req, void* req_data) {
         return;
     }
 
-    std::string hash256 = common::Hash::Hash256(m.toString());
+    std::string hash256 = common::Hash::Hash256(data);
     std::string test_data(common::Encode::HexEncode(hash256));
     std::string pair_param = ("type a\n"
         "q 8780710799663312522437781984754049815806883199414208211028653399266475630880222957078625179422662221423155858769582317459277713367317481324925129998224791\n"
