5692cce0e83b805c8834639bd65af99d571b5d9b
谢磊 <xielei@zhejianglab.org>
2024-12-11 22:29:24 +0800

    fix

--- src/contract/contract_reencryption.cc
+++ src/contract/contract_reencryption.cc
@@ -1164,7 +1164,7 @@ int ContractReEncryption::Decryption(
         lag.push_back(Zr(e, (const unsigned char*)val.c_str(), val.size(), 0));
     }
 
-    std::string test_data = "c266fb329ddc5736dedf2de994d2eeb314cd5e6b288a3653c6179f5fcc926591";
+    std::string test_data = "e49b72939ac609cf50e1773ea4af277b29f048981ec6438a45ac38b8c7f489ff";
     GT m(e, test_data.c_str(), test_data.size());
     ZJC_WARN("dec m data src: %s, tom: %s",
         test_data.c_str(), 
