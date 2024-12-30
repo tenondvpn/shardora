439680ce9b405b39972be645c02ff15bb3ead2de
谢磊 <xielei@zhejianglab.org>
2024-12-30 11:08:20 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -451,21 +451,21 @@ int TxPoolManager::BackupConsensusAddTxs(
             continue;
         }
 
-        // if (security_->Verify(
-        //         tx_ptr->unique_tx_hash,
-        //         tx_ptr->tx_info.pubkey(),
-        //         tx_ptr->tx_info.sign()) != security::kSecuritySuccess) {
-        //     ZJC_DEBUG("verify signature failed address balance: %lu, transfer amount: %lu, "
-        //         "prepayment: %lu, default call contract gas: %lu, txid: %s, step: %d",
-        //         tx_ptr->address_info->balance(),
-        //         tx_ptr->tx_info.amount(),
-        //         tx_ptr->tx_info.contract_prepayment(),
-        //         consensus::kCallContractDefaultUseGas,
-        //         common::Encode::HexEncode(tx_ptr->tx_info.gid()).c_str(),
-        //         tx_ptr->tx_info.step());
-        //     assert(false);
-        //     continue;
-        // }
+        if (security_->Verify(
+                tx_ptr->unique_tx_hash,
+                tx_ptr->tx_info.pubkey(),
+                tx_ptr->tx_info.sign()) != security::kSecuritySuccess) {
+            ZJC_DEBUG("verify signature failed address balance: %lu, transfer amount: %lu, "
+                "prepayment: %lu, default call contract gas: %lu, txid: %s, step: %d",
+                tx_ptr->address_info->balance(),
+                tx_ptr->tx_info.amount(),
+                tx_ptr->tx_info.contract_prepayment(),
+                consensus::kCallContractDefaultUseGas,
+                common::Encode::HexEncode(tx_ptr->tx_info.gid()).c_str(),
+                tx_ptr->tx_info.step());
+            assert(false);
+            continue;
+        }
         
 
         valid_txs.push_back(tx_ptr);
