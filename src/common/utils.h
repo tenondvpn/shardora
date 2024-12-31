8e7f0754e3528fe79d96f6de5fb75c9a31f88f44
谢磊 <xielei@zhejianglab.org>
2024-12-31 16:23:16 +0800

    fix

--- src/common/utils.h
+++ src/common/utils.h
@@ -69,16 +69,16 @@ struct Construct {
 
 #ifndef NDEBUG
 #define CHECK_MEMORY_SIZE(data_map) { \
-    if (data_map.size() >= 12024) { \
+    if (data_map.size() >= 120240) { \
         ZJC_INFO("data size: %u", data_map.size()); \
-        assert(data_map.size() < 12024); \
+        assert(data_map.size() < 120240); \
     } \
 }
 
 #define CHECK_MEMORY_SIZE_WITH_MESSAGE(data_map, msg) { \
-    if (data_map.size() >= 12024) { \
+    if (data_map.size() >= 120240) { \
         ZJC_INFO("%s data size: %u, msg: %s", #data_map, data_map.size(), msg); \
-        assert(data_map.size() < 12024); \
+        assert(data_map.size() < 120240); \
     } \
 }
 #else
