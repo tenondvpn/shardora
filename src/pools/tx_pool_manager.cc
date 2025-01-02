91015692948bc9e4230244737b5c77c31ccf028f
许飞 <feixu@zhejianglab.com>
2025-01-02 18:50:06 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -105,13 +105,13 @@ int TxPoolManager::FirewallCheckMessage(transport::MessagePtr& msg_ptr) {
     }
 
     msg_ptr->msg_hash = pools::GetTxMessageHash(tx_msg);
-    if (security_->Verify(
-            msg_ptr->msg_hash,
-            tx_msg.pubkey(),
-            tx_msg.sign()) != security::kSecuritySuccess) {
-        ZJC_ERROR("verify signature failed!");
-        return transport::kFirewallCheckError;
-    }
+    // if (security_->Verify(
+    //         msg_ptr->msg_hash,
+    //         tx_msg.pubkey(),
+    //         tx_msg.sign()) != security::kSecuritySuccess) {
+    //     ZJC_ERROR("verify signature failed!");
+    //     return transport::kFirewallCheckError;
+    // }
 
     auto tmp_acc_ptr = acc_mgr_.lock();
     msg_ptr->address_info = tmp_acc_ptr->GetAccountInfo(security_->GetAddress(tx_msg.pubkey()));
