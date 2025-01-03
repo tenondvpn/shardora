781e00dd354753f94821a47e0fc4d87ff023bd7c
许飞 <feixu@zhejianglab.com>
2025-01-03 14:35:36 +0800

    fix

--- src/main/tx_cli.cc
+++ src/main/tx_cli.cc
@@ -1,3 +1,4 @@
+#include <common/encode.h>
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -227,14 +228,14 @@ int tx_main(int argc, char** argv) {
             ++prikey_pos;
             from_prikey = g_prikeys[prikey_pos % g_prikeys.size()];
             security->SetPrivateKey(from_prikey);
-            std::cout << "from: " << common::Encode::HexEncode(security->GetAddress()) << std::endl;
+            std::cout << "from: " << common::Encode::HexEncode(security->GetAddress())
+                      << "sk: " << common::Encode::HexEncode(from_prikey) << std::endl;
         }
 
         if (security->GetAddress() == common::Encode::HexDecode("f1cd7abb586966d500d91329658ec48aa2094702")) {
             ++prikey_pos;
             from_prikey = g_prikeys[prikey_pos % g_prikeys.size()];
             security->SetPrivateKey(from_prikey);
-            std::cout << "from: " << common::Encode::HexEncode(security->GetAddress()) << std::endl;
         }
 
         // uint32_t* tmp_data = (uint32_t*)to.c_str();
@@ -264,7 +265,8 @@ int tx_main(int argc, char** argv) {
             ++prikey_pos;
             from_prikey = g_prikeys[prikey_pos % g_prikeys.size()];
             security->SetPrivateKey(from_prikey);
-            std::cout << "from: " << common::Encode::HexEncode(security->GetAddress()) << std::endl;
+            std::cout << "from: " << common::Encode::HexEncode(security->GetAddress())
+                      << "sk: " << common::Encode::HexEncode(from_prikey) << std::endl;
             //usleep(10000);
         }
 
