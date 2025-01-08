275a170395421633a679efeda64057576c7bd319
许飞 <feixu@zhejianglab.com>
2025-01-08 16:37:46 +0800

    fix

--- start_container.sh
+++ start_container.sh
@@ -27,5 +27,5 @@ docker run -d \
   -e LOCAL_PORT=${local_port} \
   -e HTTP_PORT=${http_port} \
   -p ${http_port}:${http_port} \
-  -p ${tcp_port}:${tcp_port} \
+  -p ${local_port}:${local_port} \
   ${image_name}  # 替换为你的 Docker 镜像名称
