d328874e3757905de919057d711055a260df54e2
许飞 <feixu@zhejianglab.com>
2025-01-08 16:21:56 +0800

    fix

--- build_container.sh
+++ build_container.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# 检查是否传入了 shard_db_name 参数
+# 检查是否传入了 net_id 参数
 if [ -z "$1" ]; then
   echo "Error: Missing shard_db_name argument. Please specify the database name (e.g., root, shard_3)."
   exit 1
 fi
 
-shard_name=$1 # eg. root shard_3
+net_id=$1 # eg. 2, 3, 4
 target=$2
 no_build=$3
-shard_db_name="${shard_name}_db"
+
+if [ "$net_id" -eq 2 ]; then
+    shard_db_name="root_db"
+	net_name="root"
+else
+	shard_db_name="shard_${net_id}_db"
+	net_name="shard${net_id}"
+fi
 
 sh gen_docker_files.sh $target $no_build
 
 target_lower=echo $target | tr '[:upper:]' '[:lower:]'
-image_name="shardora-image-${target_lower}-${shard_name}"
+image_name="shardora-image-${net_name}-${target_lower}"
 
 docker build --build-arg SHARD_DB=$shard_db_name -t ${image_name}:1.0 ./docker_files
 
