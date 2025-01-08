ba0ee42356003d49d59263f35a68fc4103eec2a5
许飞 <feixu@zhejianglab.com>
2025-01-08 16:01:52 +0800

    fix

--- build_container.sh
+++ build_container.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # 检查是否传入了 shard_db_name 参数
-if [ -z "$2" ]; then
+if [ -z "$1" ]; then
   echo "Error: Missing shard_db_name argument. Please specify the database name (e.g., root_db, shard_3_db)."
   exit 1
 fi
 
-TARGET=Release
-if test $1 = "Debug"
-then
-    TARGET=Debug
-fi
+shard_db_name=$1 # eg. root_db shard_3_db
+target=$2
+no_build=$3
 
-sh gen_docker_files.sh $TARGET
+sh gen_docker_files.sh $target $no_build
 
-shard_db_name=$2 # eg. root_db shard_3_db
-
-docker build --build-arg SHARD_DB=$shard_db_name -t shardora-image-$TARGET-$shard_db_name:1.0 ./docker_files
+docker build --build-arg SHARD_DB=$shard_db_name -t shardora-image-$target-$shard_db_name:1.0 ./docker_files
 
 # 检查构建是否成功
 if [ $? -eq 0 ]; then
