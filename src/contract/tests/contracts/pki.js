68822c30685e3462fe890de74170ac03845d774c
谢磊 <xielei@zhejianglab.org>
2025-01-08 23:31:45 +0800

    fix

--- src/contract/tests/contracts/pki.js
+++ src/contract/tests/contracts/pki.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 const util = require('util')
 const kTestSellerCount = 11;  // real: kTestSellerCount - 10
 const kTestBuyerCount = 11;  // real: kTestBuyerCount - 10
-var contract_address = "08e1eab96c9e759daa3aff82b40e77cd615a41d5";
+var contract_address = "08e1eab96c9e759daa3aff82b40e77cd615a41d9";
 var node_host = "127.0.0.1"
 
 {
