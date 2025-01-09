2cb4beb1befb0a679f1a8986650fa756e0a9bfd7
许飞 <feixu@zhejianglab.com>
2025-01-09 10:26:43 +0800

    fix

--- deploy_genesis_container.sh
+++ deploy_genesis_container.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 for node in r1; do
-sh start_container.sh shardora-image-root-debug ./zjnodes/$node/conf/zjchain.conf 
+sh start_container.sh shardora-image-root-debug ./zjnodes/$node/conf/zjchain.conf $node 
 done
 
 sleep 3
 
 for node in r2 r3; do
-sh start_container.sh shardora-image-root-debug ./zjnodes/$node/conf/zjchain.conf 
+sh start_container.sh shardora-image-root-debug ./zjnodes/$node/conf/zjchain.conf $node
 done
 
 for node in s3_1 s3_2 s3_3 s3_4; do
-sh start_container.sh shardora-image-shard3-debug ./zjnodes/$node/conf/zjchain.conf	
+sh start_container.sh shardora-image-shard3-debug ./zjnodes/$node/conf/zjchain.conf	$node
 done
