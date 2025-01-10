5f4fb33587525551a5e0c23c1077918ddbef450e
许飞 <feixu@zhejianglab.com>
2025-01-10 20:36:01 +0800

    fix

--- load_test.sh
+++ load_test.sh
@@ -3,7 +3,7 @@
 if [[ $1 == "stop" ]];
 then
 ps -ef | grep txcli | awk -F' ' '{print $2}' | xargs kill -9	
-sshpass -p 'Xf4aGbTaf!' ssh -o StrictHostKeyChecking=no root@$server3 <<"EOF"
+sshpass -p 'Xf4aGbTaf!' ssh -o StrictHostKeyChecking=no root@192.168.0.3 <<"EOF"
 ps -ef | grep txcli | awk -F' ' '{print $2}' | xargs kill -9
 EOF
 
@@ -12,13 +12,19 @@ fi
 
 cd /root/xufei/shardora/cbuild_Release && make txcli
 
-pass="Xf4aGbTaf!"
+pass='Xf4aGbTaf!'
 
-sshpass -p $pass scp -o StrictHostKeyChecking=no -r ./txcli ../addrs root@192.168.0.3:/root/
+sshpass -p $pass ssh -f -o StrictHostKeyChecking=no root@192.168.0.3 bash -c "'\
+mkdir /root/load_test
+wait \
+'"
 
-sshpass -p $pass ssh -f -o StrictHostKeyChecking=no root@$192.168.0.3 bash -c "'\
-cd /root && ./txcli 0 3 15 192.168.0.3 13003 20 &
-cd /root && ./txcli 0 4 15 192.168.0.3 14003 20 &
+sshpass -p $pass scp -o StrictHostKeyChecking=no ./txcli root@192.168.0.3:/root/load_test
+sshpass -p $pass scp -o StrictHostKeyChecking=no ../addrs root@192.168.0.3:/root/
+
+sshpass -p $pass ssh -f -o StrictHostKeyChecking=no root@192.168.0.3 bash -c "'\
+cd /root/load_test && ./txcli 0 3 15 192.168.0.3 13003 20 &
+cd /root/load_test && ./txcli 0 4 15 192.168.0.3 14003 20 &
 wait \
 '"
 
