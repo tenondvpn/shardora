2c6d1c4c8875eaec1d70eb68f3ba2b792c5db38b
许飞 <feixu@zhejianglab.com>
2025-01-10 10:39:10 +0800

    fix

--- start_container.sh
+++ start_container.sh
@@ -29,7 +29,7 @@ echo "HTTP Port: $http_port"
 #   -e HTTP_PORT=${http_port} \
 #   -p ${http_port}:${http_port} \
 #   -p ${local_port}:${local_port} \
-#   ${image_name}go
+#   ${image_name}
 # 替换为你的 Docker 镜像名称
 
   # 替换为你的 Docker 镜像名称
