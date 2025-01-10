47ee9f28204b6281e904789e7cd9b63cb68ca924
许飞 <feixu@zhejianglab.com>
2025-01-10 20:08:00 +0800

    fix

---
+++ load_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+/root/xufei/shardora/cBuild_Release/txcli 0 3 15 192.168.0.2 13002 20 &
+/root/xufei/shardora/cBuild_Release/txcli 0 3 9 192.168.0.2 13002 20 &
+/root/xufei/shardora/cBuild_Release/txcli 0 4 14 192.168.0.2 14002 20 &
+/root/xufei/shardora/cBuild_Release/txcli 0 4 15 192.168.0.2 14002 20 &
+wait
