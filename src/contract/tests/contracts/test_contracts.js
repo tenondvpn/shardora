dd5c6b392f27259cf35ed424aa8b1629ba2e1627
许飞 <feixu@zhejianglab.com>
2025-01-10 11:07:34 +0800

    fix

--- src/contract/tests/contracts/test_contracts.js
+++ src/contract/tests/contracts/test_contracts.js
@@ -1,4 +1,5 @@
 const Web3 = require('web3')
+const Web3 = require('web3')
 var net = require('net');
 var web3 = new Web3(new Web3.providers.IpcProvider('/Users/myuser/Library/Ethereum/geth.ipc', net)); // mac os path
 const { randomBytes } = require('crypto')
@@ -483,13 +484,9 @@ function InitC2cEnv(key, value, contract_address) {
       });
 }
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 async function run_multi_contracts(times) {
 	for (var i = 0; i < times; i++) {
-		var contract_address = get_contract_address(getRandomInt(0, 10000).toString());
+		var contract_address = get_contract_address(new Date().toLocaleString());
 		var pairing_param_value = "type a\nq 8780710799663312522437781984754049815806883199414208211028653399266475630880222957078625179422662221423155858769582317459277713367317481324925129998224791\nh 12016012264891146079388821366740534204802954401251311822919615131047207289359704531102844802183906537786776\nr 730750818665451621361119245571504901405976559617\nexp2 159\nexp1 107\nsign1 1\nsign0 1";
 		InitC2cEnv("c0", pairing_param_value, contract_address);		
 	}
