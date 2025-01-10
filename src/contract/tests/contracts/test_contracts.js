964f16859e7c810f3a8a768e8f43a57ff8708988
许飞 <feixu@zhejianglab.com>
2025-01-10 11:11:26 +0800

    fix

--- src/contract/tests/contracts/test_contracts.js
+++ src/contract/tests/contracts/test_contracts.js
@@ -399,7 +399,7 @@ async function SetManagerPrepayment(contract_address) {
                 break;
             }
 
-            console.log(`${cmd} contract prepayment failed error: ${stderr} count: ${stdout}`);
+            console.log(`${cmd} contract prepayment failed error: ${stderr} count: ${stdout} ${contract_address}`);
         } catch (error) {
             console.log(error);
         }
@@ -462,7 +462,7 @@ function InitC2cEnv(key, value, contract_address) {
                             break;
                         }
                             
-                        console.log(`create contract failed ${stderr}`);
+                        console.log(`create contract failed ${stderr} ${contract_address}`);
                     } catch (error) {
                         console.log(error);
                     }
@@ -483,7 +483,7 @@ function InitC2cEnv(key, value, contract_address) {
       });
 }
 
-async function run_multi_contracts(times) {
+function run_multi_contracts(times) {
 	for (var i = 0; i < times; i++) {
 		var contract_address = get_contract_address(new Date().toLocaleString());
 		var pairing_param_value = "type a\nq 8780710799663312522437781984754049815806883199414208211028653399266475630880222957078625179422662221423155858769582317459277713367317481324925129998224791\nh 12016012264891146079388821366740534204802954401251311822919615131047207289359704531102844802183906537786776\nr 730750818665451621361119245571504901405976559617\nexp2 159\nexp1 107\nsign1 1\nsign0 1";
@@ -495,7 +495,7 @@ async function main() {
 	const args = process.argv.slice(2)
 	// SetUp：初始化算法，需要用到pbc库
 	if (args[0] == 0) {
-		for (var i = 0; i < 1; i++) {
+		for (var i = 0; i < 2; i++) {
 			await run_multi_contracts(1);
 		}
 	}
