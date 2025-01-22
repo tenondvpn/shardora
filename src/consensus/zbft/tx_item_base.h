58a89e60f6465e4e4cd76b94ee03896c84ca6196
谢磊 <xielei@zhejianglab.org>
2025-01-22 09:15:42 +0800

    fix

--- src/consensus/zbft/tx_item_base.h
+++ src/consensus/zbft/tx_item_base.h
@@ -82,6 +82,11 @@ protected:
 
         block_tx->set_amount(tx_info.amount());
         block_tx->set_status(kConsensusSuccess);
+#ifndef NDEBUG
+        for (int32_t i = 0; i < tx_info.tx_debug_tm_ms_size(); ++i) {
+            block_tx->add_tx_debug_tm_ms(tx_info.tx_debug_tm_ms(i));
+        }
+#endif
     }
 
     int GetTempAccountBalance(
