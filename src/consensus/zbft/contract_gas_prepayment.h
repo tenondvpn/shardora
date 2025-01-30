0e2be328ef2de1a12478a010dff5c58e5f3870d2
谢磊 <xielei@zhejianglab.org>
2025-01-30 22:27:50 +0800

    fix

--- src/consensus/zbft/contract_gas_prepayment.h
+++ src/consensus/zbft/contract_gas_prepayment.h
@@ -75,7 +75,9 @@ public:
             return;
         }
 
-        ZJC_DEBUG("handle ConsensusToTxs: %s", common::Encode::HexEncode(*to_txs_str).c_str());
+        ZJC_DEBUG("handle ConsensusToTxs: %s, to_txs.tos_size(): %u",
+            common::Encode::HexEncode(*to_txs_str).c_str(),
+            to_txs.tos_size());
         auto thread_idx = common::GlobalInfo::Instance()->get_thread_index();
         for (int32_t i = 0; i < to_txs.tos_size(); ++i) {
             if (to_txs.tos(i).to().size() != security::kUnicastAddressLength * 2) {
