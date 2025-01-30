8f1e88edc265b4866ee89867eb4bb6885c4eb19d
谢磊 <xielei@zhejianglab.org>
2025-01-30 21:55:47 +0800

    fix

--- src/consensus/zbft/contract_gas_prepayment.h
+++ src/consensus/zbft/contract_gas_prepayment.h
@@ -25,11 +25,21 @@ public:
             db::DbWriteBatch& db_batch) {
         auto& block = view_block.block_info();
         if (tx.status() != consensus::kConsensusSuccess) {
+            ZJC_DEBUG("tx.status() != consensus::kConsensusSuccess, gid: %s, from: %s, to: %s", 
+                common::Encode::HexEncode(tx.gid()).c_str(), 
+                common::Encode::HexEncode(tx.from()).c_str(),
+                common::Encode::HexEncode(tx.to()).c_str());
             return;
         }
 
         if (block.height() <= pools_max_heights_[view_block.qc().pool_index()]) {
 //             assert(false);
+            ZJC_DEBUG("block.height() <= pools_max_heights_[view_block.qc().pool_index()] %lu, %lu, gid: %s, from: %s, to: %s", 
+                block.height(), 
+                pools_max_heights_[view_block.qc().pool_index()],
+                common::Encode::HexEncode(tx.gid()).c_str(), 
+                common::Encode::HexEncode(tx.from()).c_str(),
+                common::Encode::HexEncode(tx.to()).c_str());
             return;
         }
 
@@ -99,6 +109,12 @@ public:
         }
 
         if (block.height() <= pools_max_heights_[view_block.qc().pool_index()]) {
+            ZJC_DEBUG("block.height() <= pools_max_heights_[view_block.qc().pool_index()] %lu, %lu, gid: %s, from: %s, to: %s", 
+                block.height(), 
+                pools_max_heights_[view_block.qc().pool_index()],
+                common::Encode::HexEncode(tx.gid()).c_str(), 
+                common::Encode::HexEncode(tx.from()).c_str(),
+                common::Encode::HexEncode(tx.to()).c_str());
             return;
         }
 
@@ -127,6 +143,12 @@ public:
             db::DbWriteBatch& db_batch) {
         auto& block = view_block.block_info();
         if (block.height() <= pools_max_heights_[view_block.qc().pool_index()]) {
+            ZJC_DEBUG("block.height() <= pools_max_heights_[view_block.qc().pool_index()] %lu, %lu, gid: %s, from: %s, to: %s", 
+                block.height(), 
+                pools_max_heights_[view_block.qc().pool_index()],
+                common::Encode::HexEncode(tx.gid()).c_str(), 
+                common::Encode::HexEncode(tx.from()).c_str(),
+                common::Encode::HexEncode(tx.to()).c_str());
             return;
         }
 
@@ -152,6 +174,12 @@ public:
             const view_block::protobuf::ViewBlockItem& view_block_item,
             const block::protobuf::BlockTx& tx,
             db::DbWriteBatch& db_batch) {
+        ZJC_DEBUG("new block with tx coming: %lu, %lu, gid: %s, from: %s, to: %s", 
+            view_block_item.block_info().height(), 
+            pools_max_heights_[view_block_item.qc().pool_index()],
+            common::Encode::HexEncode(tx.gid()).c_str(), 
+            common::Encode::HexEncode(tx.from()).c_str(),
+            common::Encode::HexEncode(tx.to()).c_str());
         if (tx.step() == pools::protobuf::kConsensusLocalTos) { // 增加 prepayment 的交易
             HandleLocalToTx(view_block_item, tx, db_batch);
             return;
