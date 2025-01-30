d9870b23a02d10135175aba6879039014d4e54b3
谢磊 <xielei@zhejianglab.org>
2025-01-31 01:11:47 +0800

    fix

--- src/consensus/hotstuff/hotstuff.h
+++ src/consensus/hotstuff/hotstuff.h
@@ -359,7 +359,7 @@ private:
     void SaveLatestProposeMessage();
     void LoadLatestProposeMessage();
 
-    static const uint64_t kLatestPoposeSendTxToLeaderPeriodMs = 100lu;
+    static const uint64_t kLatestPoposeSendTxToLeaderPeriodMs = 3000lu;
 
     uint32_t pool_idx_;
 #ifdef USE_AGG_BLS
