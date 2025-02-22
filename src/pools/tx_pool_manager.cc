f76bf948ae3600d587aa7a868c8926352513d158
谢磊 <xielei@zhejianglab.org>
2025-02-23 01:55:57 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -433,8 +433,9 @@ void TxPoolManager::HandleMessage(const transport::MessagePtr& msg_ptr) {
 // #ifndef NDEBUG
             auto now_tm = common::TimeUtils::TimestampMs();
             if (now_tm > prev_show_tm_ms_ + 3000) {
-                ZJC_INFO("pools stored message size: %d, %d, gid size: %u, tx all size: %u", 
+                ZJC_INFO("pools stored message size: %d, %d, pool index: %d, gid size: %u, tx all size: %u", 
                         thread_idx, pools_msg_queue_[thread_idx].size(),
+                        address_info->pool_index(),
                         tx_pool_[address_info->pool_index()].all_tx_size(),
                         tx_pool_[address_info->pool_index()].tx_size());
                 prev_show_tm_ms_ = now_tm;
