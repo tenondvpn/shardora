a05baf13f977d3b46000a636b8d4b1687f9e7749
谢磊 <xielei@zhejianglab.org>
2025-02-23 00:24:30 +0800

    fix

--- src/consensus/hotstuff/utils.cc
+++ src/consensus/hotstuff/utils.cc
@@ -26,26 +26,26 @@ std::string GetTxMessageHash(const block::protobuf::BlockTx& tx_info, const std:
     message.append(std::string((char*)&gas_used, sizeof(gas_used)));
     uint32_t status = tx_info.status();
     message.append(std::string((char*)&status, sizeof(status)));
-    // for (int32_t i = 0; i < tx_info.storages_size(); ++i) {
-        // message.append(tx_info.storages(i).key());
-        // message.append(tx_info.storages(i).value());
+    for (int32_t i = 0; i < tx_info.storages_size(); ++i) {
+        message.append(tx_info.storages(i).key());
+        message.append(tx_info.storages(i).value());
         // ZJC_DEBUG("add tx key: %s, %s, val: %s",
         //     tx_info.storages(i).key().c_str(),
         //     common::Encode::HexEncode(tx_info.storages(i).key()).c_str(), 
         //     common::Encode::HexEncode(tx_info.storages(i).value()).c_str());
-    // }
+    }
 
-    ZJC_DEBUG("phash: %s, gid: %s, from: %s, to: %s, balance: %lu, amount: %lu, gas_limit: %lu, "
-        "gas_price: %lu, step: %u, gas_used: %lu, status: %lu, block tx hash: %s, message: %s",
-        common::Encode::HexEncode(phash).c_str(),
-        common::Encode::HexEncode(tx_info.gid()).c_str(),
-        common::Encode::HexEncode(tx_info.from()).c_str(),
-        common::Encode::HexEncode(tx_info.to()).c_str(),
-        balance, amount, gas_limit, gas_price, step,
-        gas_used,
-        status,
-        common::Encode::HexEncode(common::Hash::keccak256(message)).c_str(),
-        common::Encode::HexEncode(message).c_str());
+    // ZJC_DEBUG("phash: %s, gid: %s, from: %s, to: %s, balance: %lu, amount: %lu, gas_limit: %lu, "
+    //     "gas_price: %lu, step: %u, gas_used: %lu, status: %lu, block tx hash: %s, message: %s",
+    //     common::Encode::HexEncode(phash).c_str(),
+    //     common::Encode::HexEncode(tx_info.gid()).c_str(),
+    //     common::Encode::HexEncode(tx_info.from()).c_str(),
+    //     common::Encode::HexEncode(tx_info.to()).c_str(),
+    //     balance, amount, gas_limit, gas_price, step,
+    //     gas_used,
+    //     status,
+    //     common::Encode::HexEncode(common::Hash::keccak256(message)).c_str(),
+    //     common::Encode::HexEncode(message).c_str());
     return common::Hash::keccak256(message);
 }
 
@@ -86,16 +86,16 @@ std::string GetBlockHash(const view_block::protobuf::ViewBlockItem &view_block) 
         }
     }
 
-    auto hash = common::Hash::keccak256(msg);
-    ZJC_DEBUG("get block hash: %s, sharding_id: %u, pool_index: %u, "
-        "phash: %s, vss_random: %lu, height: %lu, "
-        "timeblock_height: %lu, timestamp: %lu, msg: %s",
-        common::Encode::HexEncode(hash).c_str(),
-        sharding_id, pool_index, 
-        common::Encode::HexEncode(view_block.parent_hash()).c_str(), 
-        vss_random, height, timeblock_height, timestamp,
-        common::Encode::HexEncode(msg).c_str());
-    return hash;
+    // auto hash = common::Hash::keccak256(msg);
+    // ZJC_DEBUG("get block hash: %s, sharding_id: %u, pool_index: %u, "
+    //     "phash: %s, vss_random: %lu, height: %lu, "
+    //     "timeblock_height: %lu, timestamp: %lu, msg: %s",
+    //     common::Encode::HexEncode(hash).c_str(),
+    //     sharding_id, pool_index, 
+    //     common::Encode::HexEncode(view_block.parent_hash()).c_str(), 
+    //     vss_random, height, timeblock_height, timestamp,
+    //     common::Encode::HexEncode(msg).c_str());
+    return common::Hash::keccak256(msg);
 }
 
 } // namespace consensus
