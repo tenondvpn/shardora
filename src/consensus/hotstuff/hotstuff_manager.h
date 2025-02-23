f53ff7a2cbd45db195736066933a0c4a9e08d905
谢磊 <xielei@zhejianglab.org>
2025-02-23 21:54:02 +0800

    fix

--- src/consensus/hotstuff/hotstuff_manager.h
+++ src/consensus/hotstuff/hotstuff_manager.h
@@ -256,7 +256,7 @@ private:
                 prefix_db_, 
                 elect_mgr_, 
                 msg_ptr->address_info,
-                msg_ptr->header.mutable_tx_proto().pubkey(),
+                msg_ptr->header.tx_proto().pubkey(),
                 keypair->pk(),
                 keypair->proof());
     }
