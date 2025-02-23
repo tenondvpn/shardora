d7e46d632a96f227884a744f2ca364bebf38bd6a
谢磊 <xielei@zhejianglab.org>
2025-02-23 10:09:51 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -479,10 +479,10 @@ int TxPoolManager::BackupConsensusAddTxs(
             tx_pool_[pool_index].all_tx_size(), 
             common::GlobalInfo::Instance()->each_tx_pool_max_txs(), 
             tx_pool_[pool_index].tx_size());
-        return;
+    } else {
+        tx_pool_[pool_index].ConsensusAddTxs(valid_tx);
     }
 
-    tx_pool_[pool_index].ConsensusAddTxs(valid_tx);
     return kPoolsSuccess;
 }
 
