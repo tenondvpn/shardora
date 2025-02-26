2185004c0d0dc4bc19755891c857687f285bcff1
谢磊 <xielei@zhejianglab.org>
2025-02-26 22:48:42 +0800

    fix

--- src/block/account_lru_map.h
+++ src/block/account_lru_map.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "common/hash.h"
+#include "common/spin_mutex.h"
 #include "common/utils.h"
 #include "protos/prefix_db.h"
 #include "protos/address.pb.h"
@@ -30,7 +31,11 @@ public:
         item_list_.push_front(key);
         item_map_[key] = item_list_.begin();
         uint32_t index = common::Hash::Hash32(key) % kBucketSize;
-        index_data_map_[index] = value;
+        {
+            common::AutoSpinLock spinlock(spin_mutex_);
+            index_data_map_[index] = value;
+        }
+
         if (item_list_.size() > kBucketSize) {
             std::string& last = item_list_.back();
             item_map_.erase(last);
@@ -40,6 +45,7 @@ public:
 
     AccountPtr get(const std::string& key) {
         uint32_t index = common::Hash::Hash32(key) % kBucketSize;
+        common::AutoSpinLock spinlock(spin_mutex_);
         AccountPtr item_ptr = index_data_map_[index];
         if (item_ptr != nullptr && item_ptr->addr() == key) {
             return item_ptr;
@@ -52,6 +58,7 @@ private:
     std::list<std::string> item_list_;
     std::unordered_map<std::string, typename std::list<std::string>::iterator> item_map_;
     AccountPtr index_data_map_[kBucketSize] = { nullptr };
+    common::SpinMutex spin_mutex_;
 
 };
 
