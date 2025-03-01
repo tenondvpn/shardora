ff134cccff2338edb3569708fdf4a72b484d6463
许飞 <feixu@zhejianglab.org>
2025-03-01 14:34:02 +0800

    fix

--- src/main/tx_cli.cc
+++ src/main/tx_cli.cc
@@ -24,7 +24,7 @@ static const std::string db_path = "./txclidb";
 static const std::string from_prikey =
     "cefc2c33064ea7691aee3e5e4f7842935d26f3ad790d81cf015e79b78958e848";
 static std::unordered_map<uint32_t, std::unordered_map<uint32_t, std::string>> net_pool_sk_map = {
-    {3, {{15, "b5039128131f96f6164a33bc7fbc48c2f5cf425e8476b1c4d0f4d186fbd0d708"},
+    {3, {{15, "cefc2c33064ea7691aee3e5e4f7842935d26f3ad790d81cf015e79b78958e848"},
          {9, "580bb274af80b8d39b33f25ddbc911b14a1b3a2a6ec8ca376ffe9661cf809d36"}}},
     {4, {{15, "ed8aa75374998a6fb20139171e570ae67ceb34817b87b05400023ff9f1e06532"},
          {12, "c2e8fb3673f82cadd860d7523c12e71a7279faec0814803e547286bb0363d0e8"}}}
