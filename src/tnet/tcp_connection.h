54e2b72c207ab93cfe2e96a6437426a5bc5f2458
谢磊 <xielei@zhejianglab.org>
2025-03-01 23:42:33 +0800

    fix

--- src/tnet/tcp_connection.h
+++ src/tnet/tcp_connection.h
@@ -182,7 +182,7 @@ private:
     void NotifyCmdPacketAndClose(int type);
     void ReleaseByIOThread();
 
-    static const uint64_t kConnectTimeoutMs = 120000lu;
+    static const uint64_t kConnectTimeoutMs = 20000lu;
     static const int OUT_BUFFER_LIST_SIZE = 10240;
 
     common::SpinMutex spin_mutex_;
