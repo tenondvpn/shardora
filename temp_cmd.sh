1e9442cd60a64d5aafc9e4e1b6347c2cc7480373
谢磊 <xielei@zhejianglab.org>
2025-03-06 15:11:12 +0800

    fix

--- temp_cmd.sh
+++ temp_cmd.sh
@@ -20,28 +20,28 @@ for ((shard_id=$start_shard; shard_id<=$end_shard; shard_id++)); do
         prikey=`sed -n "$i""p" /root/pkg/shards$shard_id | awk -F'\t' '{print $1}'`
         pubkey=`sed -n "$i""p" /root/pkg/shards$shard_id | awk -F'\t' '{print $2}'`
         echo $prikey
-        cp -rf /root/pkg/temp /root/zjnodes/s$shard_id_$i
-        sed -i 's/PRIVATE_KEY/'$prikey'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf
-        sed -i 's/LOCAL_IP/'$local_ip'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf
-        sed -i 's/BOOTSTRAP/'$bootstrap'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf
+        cp -rf /root/pkg/temp /root/zjnodes/s$shard_id'_'$i
+        sed -i 's/PRIVATE_KEY/'$prikey'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf
+        sed -i 's/LOCAL_IP/'$local_ip'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf
+        sed -i 's/BOOTSTRAP/'$bootstrap'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf
         if ((i>=100)); then
-            sed -i 's/HTTP_PORT/2'$shard_id$i'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf
-            sed -i 's/LOCAL_PORT/1'$shard_id$i'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf
+            sed -i 's/HTTP_PORT/2'$shard_id''$i'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf
+            sed -i 's/LOCAL_PORT/1'$shard_id''$i'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf
         elif ((i>=10)); then
-            sed -i 's/HTTP_PORT/2'$shard_id0$i'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf
-            sed -i 's/LOCAL_PORT/1'$shard_id0$i'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf 
+            sed -i 's/HTTP_PORT/2'$shard_id'0'$i'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf
+            sed -i 's/LOCAL_PORT/1'$shard_id'0'$i'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf 
         else
-            sed -i 's/HTTP_PORT/2'$shard_id00$i'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf
-            sed -i 's/LOCAL_PORT/1'$shard_id00$i'/g' /root/zjnodes/s$shard_id_$i/conf/zjchain.conf 
+            sed -i 's/HTTP_PORT/2'$shard_id'00'$i'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf
+            sed -i 's/LOCAL_PORT/1'$shard_id'00'$i'/g' /root/zjnodes/s$shard_id'_'$i/conf/zjchain.conf 
         fi
 
-        echo /root/zjnodes/s$shard_id_$i/zjchain
-        ln /root/pkg/zjchain /root/zjnodes/s$shard_id_$i/zjchain
-        ln /root/pkg/GeoLite2-City.mmdb /root/zjnodes/s$shard_id_$i/conf/GeoLite2-City.mmdb
-        ln /root/pkg/log4cpp.properties /root/zjnodes/s$shard_id_$i/conf/log4cpp.properties
-        mkdir -p /root/zjnodes/s$shard_id_$i/log
-        cp -rf /root/pkg/shard_db_$shard_id /root/zjnodes/s$shard_id_$i/db
-        #cd /root/zjnodes/s$shard_id_$i/ && nohup ./zjchain -f 0 -g 0 s$shard_id_$i &
+        echo /root/zjnodes/s$shard_id''$i/zjchain
+        ln /root/pkg/zjchain /root/zjnodes/s$shard_id'_'$i/zjchain
+        ln /root/pkg/GeoLite2-City.mmdb /root/zjnodes/s$shard_id'_'$i/conf/GeoLite2-City.mmdb
+        ln /root/pkg/log4cpp.properties /root/zjnodes/s$shard_id'_'$i/conf/log4cpp.properties
+        mkdir -p /root/zjnodes/s$shard_id'_'$i/log
+        cp -rf /root/pkg/shard_db_$shard_id /root/zjnodes/s$shard_id'_'$i/db
+        #cd /root/zjnodes/s$shard_id'_'$i/ && nohup ./zjchain -f 0 -g 0 s$shard_id'_'$i &
         sleep 1
     done
 done
