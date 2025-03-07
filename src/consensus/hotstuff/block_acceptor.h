f71f172d75f2d56f1c78dda54dd122560a937fdd
谢磊 <xielei@zhejianglab.org>
2025-03-08 00:08:59 +0800

    fix

--- src/consensus/hotstuff/block_acceptor.h
+++ src/consensus/hotstuff/block_acceptor.h
@@ -65,12 +65,6 @@ public:
     virtual Status AcceptSync(const view_block::protobuf::ViewBlockItem& block) = 0;
     // Commit a block
     virtual void Commit(transport::MessagePtr msg_ptr, std::shared_ptr<block::BlockToDbItem>& queue_item_ptr) = 0;
-    // Add txs to local pool
-    // virtual Status AddTxs(
-    //     transport::MessagePtr msg_ptr, 
-    //     const google::protobuf::RepeatedPtrField<pools::protobuf::TxMessage>& txs) = 0;
-    // Return block txs to pool
-    virtual Status Return(const std::shared_ptr<view_block::protobuf::ViewBlockItem>&) = 0;
     // Handle Synced Block From KeyValueSyncer
     virtual void CommitSynced(std::shared_ptr<block::BlockToDbItem>& queue_item_ptr) = 0;
     virtual double Tps() = 0;
