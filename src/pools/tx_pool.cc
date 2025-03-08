e8c213a36d86df54faeabc8c9e98b87b864ff1c3
谢磊 <xielei@zhejianglab.org>
2025-03-08 16:32:21 +0800

    fix

--- src/pools/tx_pool.cc
+++ src/pools/tx_pool.cc
@@ -150,7 +150,6 @@ void TxPool::GetTxSyncToLeader(
         uint32_t count,
         ::google::protobuf::RepeatedPtrField<pools::protobuf::TxMessage>* txs,
         pools::CheckGidValidFunction gid_vlid_func) {
-    return;
     TxItemPtr tx_ptr;
     while (added_txs_.pop(&tx_ptr) && txs->size() < count) {
         if (gid_vlid_func != nullptr && !gid_vlid_func(tx_ptr->tx_info->gid())) {
