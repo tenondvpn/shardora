27a2d63f7b4b7ceff5a840f93a6ce6af166de79a
谢磊 <xielei@zhejianglab.org>
2025-03-08 14:23:49 +0800

    fix

--- src/transport/transport_utils.h
+++ src/transport/transport_utils.h
@@ -90,7 +90,9 @@ public:
         memset(times, 0, sizeof(times));
         times_idx = 0;
         thread_index = -1;
-        ZJC_DEBUG("memory check create new transport message: %d", testTransportMessageCount.fetch_add(1));
+        auto now_count = testTransportMessageCount.fetch_add(1);
+        assert(now_count > 0);
+        ZJC_DEBUG("memory check create new transport message: %d", now_count);
     }
 
     ~TransportMessage() {
