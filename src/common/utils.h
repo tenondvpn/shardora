002a1d41e95ed92179d4e850ea9f78ed09af52cc
谢磊 <xielei@zhejianglab.org>
2025-03-08 15:32:51 +0800

    fix

--- src/common/utils.h
+++ src/common/utils.h
@@ -54,29 +54,32 @@ struct Construct {
 #endif
 
 #ifndef NDEBUG
-#define ADD_DEBUG_PROCESS_TIMESTAMP() { \
-    if (msg_ptr) { \
-        assert(msg_ptr->times_idx < (sizeof(msg_ptr->times) / sizeof(msg_ptr->times[0]))); \
-        auto btime = common::TimeUtils::TimestampUs(); \
-        uint64_t diff_time = 0; \
-        if (msg_ptr->times_idx > 0) { diff_time = btime - msg_ptr->times[msg_ptr->times_idx - 1]; if (diff_time > 200000lu)ZJC_INFO("over handle message debug use time: %lu, type: %d", diff_time, msg_ptr->header.type());} \
-        msg_ptr->debug_str[msg_ptr->times_idx] = std::string(ZJC_LOG_FILE_NAME) + ":" + std::to_string(__LINE__); \
-        msg_ptr->times[msg_ptr->times_idx] = btime; \
-        msg_ptr->times_idx++; \
-    } \
-}
+#define ADD_DEBUG_PROCESS_TIMESTAMP()
+#define TMP_ADD_DEBUG_PROCESS_TIMESTAMP()
 
-#define TMP_ADD_DEBUG_PROCESS_TIMESTAMP() { \
-    if (msg_ptr) { \
-        assert(msg_ptr->times_idx < (sizeof(msg_ptr->times) / sizeof(msg_ptr->times[0]))); \
-        auto btime = common::TimeUtils::TimestampUs(); \
-        uint64_t diff_time = 0; \
-        if (msg_ptr->times_idx > 0) { diff_time = btime - msg_ptr->times[msg_ptr->times_idx - 1]; if (diff_time > 10000lu)ZJC_INFO("over handle message debug use time: %lu, type: %d", diff_time, msg_ptr->header.type());} \
-        msg_ptr->debug_str[msg_ptr->times_idx] = std::string(ZJC_LOG_FILE_NAME) + ":" + std::to_string(__LINE__); \
-        msg_ptr->times[msg_ptr->times_idx] = btime; \
-        msg_ptr->times_idx++; \
-    } \
-}
+// #define ADD_DEBUG_PROCESS_TIMESTAMP() { \
+//     if (msg_ptr) { \
+//         assert(msg_ptr->times_idx < (sizeof(msg_ptr->times) / sizeof(msg_ptr->times[0]))); \
+//         auto btime = common::TimeUtils::TimestampUs(); \
+//         uint64_t diff_time = 0; \
+//         if (msg_ptr->times_idx > 0) { diff_time = btime - msg_ptr->times[msg_ptr->times_idx - 1]; if (diff_time > 200000lu)ZJC_INFO("over handle message debug use time: %lu, type: %d", diff_time, msg_ptr->header.type());} \
+//         msg_ptr->debug_str[msg_ptr->times_idx] = std::string(ZJC_LOG_FILE_NAME) + ":" + std::to_string(__LINE__); \
+//         msg_ptr->times[msg_ptr->times_idx] = btime; \
+//         msg_ptr->times_idx++; \
+//     } \
+// }
+
+// #define TMP_ADD_DEBUG_PROCESS_TIMESTAMP() { \
+//     if (msg_ptr) { \
+//         assert(msg_ptr->times_idx < (sizeof(msg_ptr->times) / sizeof(msg_ptr->times[0]))); \
+//         auto btime = common::TimeUtils::TimestampUs(); \
+//         uint64_t diff_time = 0; \
+//         if (msg_ptr->times_idx > 0) { diff_time = btime - msg_ptr->times[msg_ptr->times_idx - 1]; if (diff_time > 10000lu)ZJC_INFO("over handle message debug use time: %lu, type: %d", diff_time, msg_ptr->header.type());} \
+//         msg_ptr->debug_str[msg_ptr->times_idx] = std::string(ZJC_LOG_FILE_NAME) + ":" + std::to_string(__LINE__); \
+//         msg_ptr->times[msg_ptr->times_idx] = btime; \
+//         msg_ptr->times_idx++; \
+//     } \
+// }
 #else
 #define ADD_DEBUG_PROCESS_TIMESTAMP()
 #define TMP_ADD_DEBUG_PROCESS_TIMESTAMP()
