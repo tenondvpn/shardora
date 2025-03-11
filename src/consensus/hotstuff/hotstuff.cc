bcf7a7a9ddf7be097da1eb980ad64c7b66fcc352
谢磊 <xielei@zhejianglab.org>
2025-03-12 00:27:34 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -197,7 +197,7 @@ Status Hotstuff::Propose(
     auto* hotstuff_msg = header.mutable_hotstuff();
     auto* pb_pro_msg = hotstuff_msg->mutable_pro_msg();
     Status s = ConstructProposeMsg(msg_ptr, pb_pro_msg);
-    if (s != Status::kSuccess) {
+    if (s != Status::kSuccess && tc == nullptr) {
         ZJC_DEBUG("pool: %d construct propose msg failed, %d",
             pool_idx_, s);
         return s;
@@ -1862,6 +1862,7 @@ Status Hotstuff::ConstructProposeMsg(
 
     auto new_view_block = pro_msg->mutable_view_item();
     auto* tx_propose = pro_msg->mutable_tx_propose();
+    pro_msg->set_elect_height(elect_item->ElectHeight());
     Status s = ConstructViewBlock(msg_ptr, new_view_block, tx_propose);
     if (s != Status::kSuccess) {
         ZJC_DEBUG("pool: %d construct view block failed, view: %lu, %d, member_index: %d",
@@ -1870,7 +1871,6 @@ Status Hotstuff::ConstructProposeMsg(
         return s;
     }
 
-    pro_msg->set_elect_height(elect_item->ElectHeight());
     return Status::kSuccess;
 }
 
