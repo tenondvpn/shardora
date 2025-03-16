797f014c6469b82e6d24cd50c9e3a8d9529f2211
谢磊 <xielei@zhejianglab.org>
2025-03-16 15:39:39 +0800

    fix

--- src/db/db.cc
+++ src/db/db.cc
@@ -93,14 +93,14 @@ bool Db::Init(const std::string& db_path) {
     // rocksdb::BlockBasedTableOptions table_option;
     // table_option.filter_policy.reset(rocksdb::NewBloomFilterPolicy(10, false));
     // options.table_factory.reset(rocksdb::NewBlockBasedTableFactory(table_option));
-    options.compression = rocksdb::kSnappyCompression;
-    options.bottommost_compression = rocksdb::kZlibCompression;
-    options.compaction_style = rocksdb::kCompactionStyleUniversal;
-    options.compaction_options_universal.size_ratio = 20;
-    options.level0_file_num_compaction_trigger = 4;
-    options.max_bytes_for_level_base = 64 * 1024 * 1024;
-    options.write_buffer_size = 128 * 1024 * 1024;
-    options.max_write_buffer_number = 4;
+    // options.compression = rocksdb::kSnappyCompression;
+    // options.bottommost_compression = rocksdb::kZlibCompression;
+    // options.compaction_style = rocksdb::kCompactionStyleUniversal;
+    // options.compaction_options_universal.size_ratio = 20;
+    // options.level0_file_num_compaction_trigger = 4;
+    // options.max_bytes_for_level_base = 64 * 1024 * 1024;
+    // options.write_buffer_size = 128 * 1024 * 1024;
+    // options.max_write_buffer_number = 4;
     // options.compaction_style = rocksdb::kCompactionStyleUniversal;
     // options.write_buffer_size = 67108864 / 64; // 64MB
     // options.max_write_buffer_number = 3 / 3;
@@ -116,8 +116,8 @@ bool Db::Init(const std::string& db_path) {
     // options.keep_log_file_num = 1;
     // options.max_open_files = 10 / 5;
     // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
-    // options.IncreaseParallelism();
-    // options.OptimizeLevelStyleCompaction();
+    options.IncreaseParallelism();
+    options.OptimizeLevelStyleCompaction();
 
 //     options.prefix_extractor.reset(rocksdb::NewFixedPrefixTransform(3));
 //     options.memtable_prefix_bloom_bits = 100000000;
