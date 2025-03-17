7a68be8f0e0d8b2b351aadf73340339ada82da1a
谢磊 <xielei@zhejianglab.org>
2025-03-17 19:04:50 +0800

    fix

--- simple_remote.sh
+++ simple_remote.sh
@@ -169,7 +169,7 @@ run_command() {
             sleep 3
         fi
 
-        if (($run_cmd_count >= 1)); then
+        if (($run_cmd_count >= 10)); then
             check_cmd_finished
             run_cmd_count=0
         fi
@@ -186,13 +186,13 @@ start_all_nodes() {
     run_cmd_count=0
     start_pos=1
     for ip in "${node_ips_array[@]}"; do 
-        sshpass -p $PASSWORD ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip "cd /root && tar -zxvf pkg.tar.gz && cd ./pkg && sh start_cmd.sh $ip $start_pos $each_nodes_count $bootstrap 2 $end_shard"  &
+        sshpass -p $PASSWORD ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip "cd /root && tar -zxvf pkg.tar.gz && cd ./pkg && sh start_cmd.sh $ip $start_pos $each_nodes_count $bootstrap 2 $end_shard &"  &
         run_cmd_count=$((run_cmd_count + 1))
         if ((start_pos==1)); then
             sleep 3
         fi
 
-        if (($run_cmd_count >= 1)); then
+        if (($run_cmd_count >= 10)); then
             check_cmd_finished
             run_cmd_count=0
         fi
