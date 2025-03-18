d51ab71d2b599bb37106e4b8514c878ad5e4a315
谢磊 <xielei@zhejianglab.org>
2025-03-18 14:11:12 +0800

    fix

--- src/security/gmssl/gmssl.cc
+++ src/security/gmssl/gmssl.cc
@@ -15,10 +15,8 @@ int GmSsl::SetPrivateKey(const std::string& prikey) {
     assert(prikey.size() == 32);
     str_prikey_ = prikey;
     prikey_ = std::make_shared<SM2_KEY>();
-    // 设置私钥
-    // memcpy(prikey_->private_key, prikey.c_str(), 32);
     // 根据私钥生成公钥
-    if (sm2_key_generate(prikey_.get()) != 1) {
+    if (sm2_key_set_private_key(prikey_.get(), (uint8_t*)prikey.c_str()) != 1) {
         ZJC_ERROR("Failed to generate public key from private key.");
         return kSecurityError;
     }
