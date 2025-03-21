2f09359c780e28ca9e93d75da8232a6051e070a6
谢磊 <xielei@zhejianglab.org>
2025-03-22 01:52:20 +0800

    fix

--- src/transport/multi_thread.cc
+++ src/transport/multi_thread.cc
@@ -158,6 +158,7 @@ int MultiThreadHandler::Init(std::shared_ptr<db::Db>& db, std::shared_ptr<securi
 void MultiThreadHandler::Start() {
     for (uint32_t i = 0; i < all_thread_count_; ++i) {
         thread_vec_.push_back(std::make_shared<ThreadHandler>(this, wait_con_[i], wait_mutex_[i]));
+        usleep(300000lu);
         std::unique_lock<std::mutex> lock(thread_wait_mutex_);
         thread_wait_con_.wait(lock);
     }
