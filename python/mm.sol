f98eb73e7d99f9cf1bcd8949495bba6cbfc7765c
谢磊 <xielei@zhejianglab.org>
2025-03-25 14:06:35 +0800

    fix

--- python/mm.sol
+++ python/mm.sol
@@ -30,6 +30,7 @@ contract mm {
     event DIDRegistered(string did, string userType, string created);
     event DIDUpdated(string did, string updated);
     event DIDDeleted(string did);
+    event TestDIDDeleted(uint64 tm);
     event AssetAssociated(string did, string assetId, uint8 rightType);
     event AssetDeleted(string did, string assetId);
     event BalanceUpdated(string did, uint256 newBalance);
@@ -49,6 +50,8 @@ contract mm {
         require(bytes(didDocuments[didHash].created).length == 0, "DID already exists");
 
         emit DIDDeleted("1111");
+        emit TestDIDDeleted(block.timestamp);
+        emit DIDDeleted("111100001111");
         string memory currentTime = uint2str(block.timestamp);
         emit DIDDeleted("2222");
         DIDDocument storage newDocument = didDocuments[didHash];
@@ -65,7 +68,7 @@ contract mm {
         emit DIDDeleted("5555");
         emit DIDRegistered(_did, _userType, currentTime);
     }
-    
+
     function deposit(string memory _did, uint256 amount) public {
         bytes32 didHash = keccak256(abi.encodePacked(_did));
         require(bytes(didDocuments[didHash].created).length > 0, "DID does not exist");
