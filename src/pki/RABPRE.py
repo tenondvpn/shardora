302a9349992dee0b56b81b3552c7818051ddd057
谢磊 <xielei@zhejianglab.org>
2025-03-30 15:50:41 +0800

    fix

--- src/pki/RABPRE.py
+++ src/pki/RABPRE.py
@@ -1,8 +1,20 @@
 import random
-from Crypto.Util.number import getPrime
+# from Crypto.Util.number import getPrime
 from Crypto.Util.number import inverse
+
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if (primes[p] == True):
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    prime_numbers = [p for p in range(2, n) if primes[p]]
+    return prime_numbers
+
 def SETUP(lamda):
-    p = getPrime(lamda)
+    p = sieve_of_eratosthenes(lamda)
     a = random.randint(2, p - 2)
     g=2
     b = random.randint(2, p - 2)
