0a4a3521b1d76ce1cd1df4d172332c306dbfe862
谢磊 <xielei@zhejianglab.org>
2025-03-31 10:54:39 +0800

    fix

--- src/block/block_manager.cc
+++ src/block/block_manager.cc
@@ -878,7 +878,7 @@ void BlockManager::AddNewBlock(
     //             view_block_item->qc().view_block_hash(), 
     //             db_batch);
     // #endif
-    //         prefix_db_->SaveCommittedGid(tx_list[i], db_batch);
+            prefix_db_->SaveCommittedGid(tx_list[i], db_batch);
             if (tx_list[i].step() != pools::protobuf::kConsensusCreateGenesisAcount) {
                 account_mgr_->NewBlockWithTx(*view_block_item, tx_list[i], db_batch);
             }
