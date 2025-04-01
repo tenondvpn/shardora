7eab964711380457398ada720bf223e50c15f5b4
谢磊 <xielei@zhejianglab.org>
2025-04-01 09:37:40 +0800

    fix

--- src/block/account_manager.cc
+++ src/block/account_manager.cc
@@ -53,7 +53,7 @@ void AccountManager::CreatePoolsAddressInfo() {
     root_pool_address_info_->set_balance(0);
     root_pool_address_info_->set_sharding_id(-1);
     root_pool_address_info_->set_pool_index(common::kImmutablePoolSize);
-    root_pool_address_info_->set_addr(common::kRootPoolsAddress);
+    root_pool_address_info_->set_addr(common::kRootPoolsAddressPrefix + "0000");
     root_pool_address_info_->set_type(address::protobuf::kImmutablePoolAddress);
     root_pool_address_info_->set_latest_height(0);
     uint32_t i = 0;
@@ -61,10 +61,10 @@ void AccountManager::CreatePoolsAddressInfo() {
     // 这只是为了随机分配个 addr 给 pool，但这个 addr 必须和 pool 之间有 GetAddressPoolIndex 的关系，所以遍历着去找
     // pool_address_info_ 中存有 257 个 pool address
     for (uint32_t i = 0; i < common::kInvalidUint32; ++i) {
-        std::string addr = common::kRootPoolsAddress;
+        std::string addr = common::kRootPoolsAddressPrefix;
         uint32_t* tmp_data = (uint32_t*)addr.data();
         tmp_data[0] = i;
-        auto pool_idx = common::GetAddressPoolIndex(addr);
+        auto pool_idx = common::GetAddressPoolIndex(addr)  + "0000";
 
         if (pool_idx_set.size() > common::kImmutablePoolSize) {
             break;
