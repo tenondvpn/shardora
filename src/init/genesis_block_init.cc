890ebbbbf8e51db356472c52140374dc9b8c6555
谢磊 <xielei@zhejianglab.org>
2025-04-01 13:41:39 +0800

    fix

--- src/init/genesis_block_init.cc
+++ src/init/genesis_block_init.cc
@@ -168,6 +168,8 @@ void GenesisBlockInit::CreatePoolsAddressInfo(uint16_t network_id) {
     immutable_pool_address_info_->set_type(address::protobuf::kImmutablePoolAddress);
     immutable_pool_address_info_->set_latest_height(0);
     immutable_pool_address_info_->set_nonce(0);
+    ZJC_DEBUG("init pool immutable index net: %u, base address: %s", 
+        network_id, common::Encode::HexEncode(immutable_pool_address_info_).c_str());
     uint32_t i = 0;
     std::unordered_set<uint32_t> pool_idx_set;
     for (uint32_t i = 0; i < common::kInvalidUint32; ++i) {
@@ -195,6 +197,8 @@ void GenesisBlockInit::CreatePoolsAddressInfo(uint16_t network_id) {
         pool_address_info_[pool_idx]->set_latest_height(0);
         pool_address_info_[pool_idx]->set_nonce(0);
         pool_idx_set.insert(pool_idx);
+        ZJC_DEBUG("init pool index: %u, base address: %s", 
+            pool_idx, common::Encode::HexEncode(addr).c_str());
     }
 }
 
