60e90bd7387b457307757b2d109884d36ab492d7
谢磊 <xielei@zhejianglab.org>
2025-04-02 15:53:06 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -1105,10 +1105,10 @@ void TxPoolManager::DispatchTx(uint32_t pool_index, const transport::MessagePtr&
     TMP_ADD_DEBUG_PROCESS_TIMESTAMP();
     tx_pool_[pool_index].AddTx(tx_ptr);
     TMP_ADD_DEBUG_PROCESS_TIMESTAMP();
-    ZJC_DEBUG("success add local transfer to tx pool: %u, step: %d, tx key: %s, nonce: %lu, from pk: %s, to: %s",
+    ZJC_DEBUG("trace tx success add local transfer to tx pool: %u, step: %d, addr: %s, nonce: %lu, from pk: %s, to: %s",
         pool_index,
         msg_ptr->header.tx_proto().step(),
-        common::Encode::HexEncode(tx_ptr->tx_key).c_str(),
+        common::Encode::HexEncode(tx_ptr->address_info->addr()).c_str(),
         tx_ptr->tx_info->nonce(),
         common::Encode::HexEncode(msg_ptr->header.tx_proto().pubkey()).c_str(),
         common::Encode::HexEncode(msg_ptr->header.tx_proto().to()).c_str());
