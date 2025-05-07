189dd8c0b22ebfe23a5092730e5cf8bea2eabc14
tenondvpn <jhsx5679@gmail.com>
2025-05-07 13:36:14 +0800

    fix

---
+++ check_remote.sh
@@ -0,0 +1,49 @@
+each_nodes_count=$1
+node_ips=$2
+end_shard=$3
+PASSWORD=$4
+RUNC_COMMAND_STR=$5
+
+init() {
+    if [ "$node_ips" == "" ]; then
+        echo "just use local single node."
+        node_ips='127.0.0.1'
+    fi  
+
+    if [ "$end_shard" == "" ]; then
+        end_shard=3
+    fi  
+
+    if [ "$PASSWORD" == "" ]; then
+        PASSWORD="Xf4aGbTaf!"
+    fi
+
+    if [[ "$each_nodes_count" -eq "" ]]; then
+        each_nodes_count=4 
+    fi
+
+    node_ips_array=(${node_ips//,/ })
+    nodes_count=0
+    for ip in "${node_ips_array[@]}"; do
+        nodes_count=$(($nodes_count + $each_nodes_count))
+    done
+
+    shard3_node_count=`wc -l /root/shardora/shards3 | awk -F' ' '{print $1}'`
+    if [ "$shard3_node_count" != "$nodes_count" ]; then
+        echo "new shard nodes file will create."
+    fi  
+
+    echo "node count: " $nodes_count
+}
+
+run_command() {
+    echo 'run_command start'
+    node_ips_array=(${node_ips//,/ })
+    for ip in "${node_ips_array[@]}"; do 
+        sshpass -p $PASSWORD ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip "$RUNC_COMMAND_STR"
+    done
+
+    echo 'run_command over'
+}
+
+run_command
