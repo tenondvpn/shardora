e5d93498d72bd521a2ca8d63d201680ed600bd47
tenondvpn <jhsx5679@gmail.com>
2025-05-30 00:15:24 +0800

    fix

--- src/pools/shard_statistic.cc
+++ src/pools/shard_statistic.cc
@@ -164,8 +164,10 @@ void ShardStatistic::cleanUpBlocks(PoolBlocksInfo& pool_blocks_info) {
 void ShardStatistic::HandleStatisticBlock(
         const block::protobuf::Block& block) {
     auto& elect_statistic = block.elect_statistic();
-    ZJC_INFO("success handle statistic block: %s, latest_statisticed_height_: %lu",
-        ProtobufToJson(elect_statistic).c_str(), latest_statisticed_height_);
+    ZJC_INFO("success handle statistic block: %s, latest_statisticed_height_: %lu, %d, %d",
+        ProtobufToJson(elect_statistic).c_str(), latest_statisticed_height_,
+        block.has_elect_statistic(),
+        block.has_pool_st_info());
     auto& heights = elect_statistic.height_info();
     auto st_iter = statistic_pool_info_.begin();
     while (st_iter != statistic_pool_info_.end()) {
