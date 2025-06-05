14d1e8b3468b05a312593bfec66c38e92bbf5b8f
tenondvpn <jhsx5679@gmail.com>
2025-06-05 16:37:48 +0800

    fix

--- src/pools/shard_statistic.cc
+++ src/pools/shard_statistic.cc
@@ -603,7 +603,7 @@ int ShardStatistic::StatisticWithHeights(
                 ZJC_INFO("pool_statistic_height_with_block_height_map_ tmp_pool_idx: %d, "
                     "tmp_iter->first: %lu iter->first: %lu",
                     tmp_pool_idx, tmp_iter->first, iter->first);
-                if (tmp_iter->first > iter->first) {
+                if (tmp_iter->first == iter->first) {
                     auto tmp_pool_iter = tmp_iter->second.find(tmp_pool_idx);
                     if (tmp_pool_iter != tmp_iter->second.end()) {
                         iter->second[tmp_pool_idx].statistic_max_height = tmp_pool_iter->second;
