6271474aada01607f4acaea53261489e352630a9
tenondvpn <jhsx5679@gmail.com>
2025-06-25 20:59:38 +0800

    fix

--- src/bls/bls_dkg.cc
+++ src/bls/bls_dkg.cc
@@ -744,7 +744,11 @@ void BlsDkg::CreateSwapKey(uint32_t member_idx, std::string* seckey, int32_t* se
     if (members_ == nullptr || 
             local_member_index_ >= member_count_ ||
             member_idx >= local_src_secret_key_contribution_.size()) {
-        ZJC_DEBUG("null swap key failed get ecdh key member_idx: %d.", member_idx);
+        ZJC_DEBUG("null swap key failed get ecdh key member_idx: %d, "
+            "local_member_index_: %d, member_count_: %d, "
+            "local_src_secret_key_contribution_.size(): %d", 
+            member_idx, local_member_index_, member_count_,
+            local_src_secret_key_contribution_.size());
         return;
     }
 
