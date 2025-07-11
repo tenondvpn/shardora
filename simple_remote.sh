0a02b8fb64225836a8bde189c6acc7556f5d2616
tenondvpn <jhsx5679@gmail.com>
2025-07-11 21:22:54 +0800

    fix

--- simple_remote.sh
+++ simple_remote.sh
@@ -186,26 +186,20 @@ run_command() {
 start_all_nodes() {
     echo 'start_all_nodes start'
     node_ips_array=(${node_ips//,/ })
-    run_cmd_count=0
     start_pos=1
     for ip in "${node_ips_array[@]}"; do
-        echo "start node: " $ip 
-        nodes_count=$each_nodes_count
+        echo "start node: " $ip $each_nodes_count
+        start_nodes_count=(($each_nodes_count + 0))
         if ((start_pos==1)); then
-            nodes_count=3
+            start_nodes_count=3
         fi
 
-        sshpass -p $PASSWORD ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip "cd /root && tar -zxvf pkg.tar.gz > /dev/null 2>&1 && cd ./pkg && sh start_cmd.sh $ip $start_pos $nodes_count $bootstrap 2 $end_shard &"  &
-        run_cmd_count=$((run_cmd_count + 1))
+        sshpass -p $PASSWORD ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip "cd /root && tar -zxvf pkg.tar.gz > /dev/null 2>&1 && cd ./pkg && sh start_cmd.sh $ip $start_pos $start_nodes_count $bootstrap 2 $end_shard &"  &
         if ((start_pos==1)); then
             sleep 3
         fi
 
-        if (($run_cmd_count >= 100)); then
-            check_cmd_finished
-            run_cmd_count=0
-        fi
-        start_pos=$(($start_pos+$nodes_count))
+        start_pos=$(($start_pos+$start_nodes_count))
     done
 
     check_cmd_finished
