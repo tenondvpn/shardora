a83d3463e6ab601a9e31c225ceed79110a7210ed
tenondvpn <jhsx5679@gmail.com>
2025-07-14 01:33:51 +0800

    fix

--- remote_start.sh
+++ remote_start.sh
@@ -7,87 +7,24 @@ TARGET=$5
 FIRST_NODE_COUNT=10
 
 init() {
-    if [ "$node_ips" == "" ]; then
-        echo "just use local single node."
-        node_ips='127.0.0.1'
-    fi  
-
-    if [ "$end_shard" == "" ]; then
-        end_shard=3
-    fi  
-
-    if [ "$PASSWORD" == "" ]; then
-        PASSWORD="Xf4aGbTaf!"
-    fi
-
-    if [ "$TARGET" == "" ]; then
-        TARGET=Release
-    fi
-
     killall -9 zjchain
     killall -9 txcli
-
-    sh build.sh a $TARGET
-    sudo rm -rf /root/zjnodes
-    sudo cp -rf ./zjnodes_local /root/zjnodes
-    rm -rf /root/zjnodes/*/zjchain /root/zjnodes/*/core* /root/zjnodes/*/log/* /root/zjnodes/*/*db*
-
-    cp -rf ./zjnodes_local/zjchain/GeoLite2-City.mmdb /root/zjnodes/zjchain
-    cp -rf ./zjnodes_local/zjchain/conf/log4cpp.properties /root/zjnodes/zjchain/conf
-    mkdir -p /root/zjnodes/zjchain/log
-
-
-    sudo cp -rf ./cbuild_$TARGET/zjchain /root/zjnodes/zjchain
-    sudo cp -f ./conf/genesis.yml /root/zjnodes/zjchain/genesis.yml
-
-    sudo cp -rf ./cbuild_$TARGET/zjchain /root/zjnodes/zjchain
-    if [[ "$each_nodes_count" -eq "" ]]; then
-        each_nodes_count=4 
-    fi
-
-    node_ips_array=(${node_ips//,/ })
-    nodes_count=0
-    for ip in "${node_ips_array[@]}"; do
-        nodes_count=$(($nodes_count + $each_nodes_count))
-    done
-
-    nodes_count=$(($nodes_count - $each_nodes_count + $FIRST_NODE_COUNT))
-    if ((nodes_count>1024)); then
-        nodes_count=1024
-    fi
-
-    shard3_node_count=`wc -l /root/shardora/shards3 | awk -F' ' '{print $1}'`
-    if [ "$shard3_node_count" != "$nodes_count" ]; then
-        echo "new shard nodes file will create."
-        rm -rf /root/shardora/shards*
-    fi  
-
-    echo "node count: " $nodes_count
-    cd /root/zjnodes/zjchain && ./zjchain -U -N $nodes_count
-    cd /root/zjnodes/zjchain && ./zjchain -S 3 -N $nodes_count
-
     rm -rf /root/zjnodes/r*
     rm -rf /root/zjnodes/s*
     rm -rf /root/zjnodes/new*
     rm -rf /root/zjnodes/node
     rm -rf /root/zjnodes/param
+    if [ "$TARGET" == "" ]; then
+        TARGET=Release
+    fi
 }
 
 make_package() {
-    rm -rf /root/zjnodes/zjchain/pkg
-    mkdir /root/zjnodes/zjchain/pkg
-    cp /root/zjnodes/zjchain/zjchain /root/zjnodes/zjchain/pkg
-    cp /root/zjnodes/zjchain/conf/GeoLite2-City.mmdb /root/zjnodes/zjchain/pkg
-    cp /root/zjnodes/zjchain/conf/log4cpp.properties /root/zjnodes/zjchain/pkg
-    cp /root/shardora/shards3 /root/zjnodes/zjchain/pkg
-    cp /root/shardora/root_nodes /root/zjnodes/zjchain/pkg/shards2
-    cp /root/shardora/temp_cmd.sh /root/zjnodes/zjchain/pkg
-    cp /root/shardora/start_cmd.sh /root/zjnodes/zjchain/pkg
-    cp -rf /root/zjnodes/zjchain/root_db /root/zjnodes/zjchain/pkg/shard_db_2
-    cp -rf /root/zjnodes/zjchain/shard_db_3 /root/zjnodes/zjchain/pkg
-    cp -rf /root/zjnodes/temp /root/zjnodes/zjchain/pkg
-    cp -rf /root/shardora/gdb/* /root/zjnodes/zjchain/pkg
-    cd /root/zjnodes/zjchain/ && tar -zcvf pkg.tar.gz ./pkg > /dev/null 2>&1
+    rm -rf /root/zjnodes/zjchain/pkg*
+    cp /root/shardora/pkg.tar.gz /root/zjnodes/zjchain/ 
+    cd /root/zjnodes/zjchain/ && tar -zxvf pkg.tar.gz
+    cp -rf /root/shardora/cbuild_$TARGET/zjchain /root/zjnodes/zjchain/pkg/
+    cd /root/zjnodes/zjchain/ && tar -zcvf pkg.tar.gz ./pkg > /dev/null 2>&1 
 }
 
 get_bootstrap() {
@@ -228,6 +165,8 @@ start_all_nodes() {
 }
 
 killall -9 sshpass
+init
+make_package
 clear_command
 scp_package
 get_bootstrap
