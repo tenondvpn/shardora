5c521607c60472ab11979e38e7d15c486a7ec5db
tenondvpn <jhsx5679@gmail.com>
2025-08-01 23:57:54 +0800

    fix

--- src/sync/key_value_sync.cc
+++ src/sync/key_value_sync.cc
@@ -524,6 +524,7 @@ void KeyValueSync::ProcessSyncValueResponse(const transport::MessagePtr& msg_ptr
                     pb_vblock->qc().view(),
                     pb_vblock->block_info().height(),
                     (iter->tag() == kBlockHeight ? key.c_str() : common::Encode::HexEncode(key).c_str()));
+                auto thread_idx = common::GlobalInfo::Instance()->get_thread_index();
                 vblock_queues_[thread_idx]->push(pb_vblock);
             // }  
         } while (0);
