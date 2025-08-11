ed88d59991d9698094ef38e9d846f3fa3bc3a931
tenondvpn <jhsx5679@gmail.com>
2025-08-11 12:28:19 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -1819,8 +1819,8 @@ Status Hotstuff::ConstructViewBlock(
 #ifdef TEST_FORKING_ATTACK
     auto new_prev_view = pre_v_block->qc().view();
     auto rand_count = rand() % 100;
-    if (rand_count <= 80 && new_prev_view > 4) {
-        if (rand_count <= 40) {
+    if (rand_count <= 30 && new_prev_view > 4) {
+        if (rand_count <= 15) {
             new_prev_view -= 1;
         }
 
