8b6c5d71233bd7f7af9ff6fe65da2534c985df8b
tenondvpn <jhsx5679@gmail.com>
2025-08-12 18:28:36 +0800

    fix

--- temp_cmd.sh
+++ temp_cmd.sh
@@ -48,19 +48,19 @@ init_config() {
 init_firewall() {
     iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
     DEV=eth0
-    RATE="100mbit"
-    DELAY="100ms 20ms"
-ß
+    RATE="500mbit"
+    DELAY="25ms 1ms"
+
     # 清除旧规则
     tc qdisc del dev $DEV root 2>/dev/null
 
     # 设置HTB根队列
     tc qdisc add dev $DEV root handle 1: htb default 12
-    tc class add dev $DEV parent 1: classid 1:1 htb rate 100mbit
+    tc class add dev $DEV parent 1: classid 1:1 htb rate 1000mbit
 
     # 创建子类并添加延迟
     tc class add dev $DEV parent 1:1 classid 1:12 htb rate $RATE ceil $RATE
-    tc qdisc add dev $DEV parent 1:12 handle 12: netem delay $DELAY loss 5%
+    tc qdisc add dev $DEV parent 1:12 handle 12: netem delay $DELAY
 
     # tc qdisc del dev eth0 root
     # tc qdisc add dev eth0 root netem delay 25ms
