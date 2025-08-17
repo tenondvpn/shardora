f496b04c6ed604bc7b785085fcbc8f473ac0305f
tenondvpn <jhsx5679@gmail.com>
2025-08-17 19:50:10 +0800

    fix

--- cmd.sh
+++ cmd.sh
@@ -1,44 +1,7 @@
 node_ips=$1
-
-check_cmd_finished() {
-    echo "waiting..."
-    sleep 1
-    ps -ef | grep sshpass 
-    while true
-    do
-        sshpass_count=`ps -ef | grep sshpass | grep ConnectTimeout | wc -l`
-        if [ "$sshpass_count" == "0" ]; then
-            break
-        fi
-        sleep 1
-    done
-
-    ps -ef | grep sshpass
-    echo "waiting ok"
-}
-
-
-clear_command() {
-    echo 'run_command start'
-    node_ips_array=(${node_ips//,/ })
-    run_cmd_count=0
-    start_pos=1
-    for ip in "${node_ips_array[@]}"; do 
-        sshpass -p Xf4aGbTaf! ssh -o ConnectTimeout=10 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip $2  
-        run_cmd_count=$((run_cmd_count + 1))
-        if ((start_pos==1)); then
-            sleep 3
-        fi
-
-        if (($run_cmd_count >= 50)); then
-            check_cmd_finished
-            run_cmd_count=0
-        fi
-        start_pos=$(($start_pos+$each_nodes_count))
-    done
-
-    check_cmd_finished
-    echo 'run_command over'
-}
-
-clear_command
+node_ips_array=(${node_ips//,/ })
+for ip in "${node_ips_array[@]}"; do
+    echo $ip
+    sshpass -p Xf4aGbTaf! ssh -o ConnectTimeout=10 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip $2  
+    #sleep 3
+done
