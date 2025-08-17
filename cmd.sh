5fb143701a6839e2be69c154aee5a8d3e68b9bec
tenondvpn <jhsx5679@gmail.com>
2025-08-17 19:54:18 +0800

    fix

--- cmd.sh
+++ cmd.sh
@@ -24,8 +24,8 @@ clear_command() {
     run_cmd_count=0
     start_pos=1
     for ip in "${node_ips_array[@]}"; do 
-        echo $ip
-        sshpass -p Xf4aGbTaf! ssh -o ConnectTimeout=10 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip $2  
+        echo $ip $2
+        sshpass -p Xf4aGbTaf! ssh -o ConnectTimeout=10 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip $2 
         run_cmd_count=$((run_cmd_count + 1))
         if (($run_cmd_count >= 50)); then
             check_cmd_finished
