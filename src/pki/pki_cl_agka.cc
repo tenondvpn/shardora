f7903702fa8a12bf2c6a208639d5a59f13660f22
tenondvpn <jhsx5679@gmail.com>
2025-11-12 10:25:06 +0800

    fix

--- src/pki/pki_cl_agka.cc
+++ src/pki/pki_cl_agka.cc
@@ -826,7 +826,7 @@ DecodeKey PkiClAgka::generate_d_for_list(int& index, std::vector<Msg>& msgs) {
     di *= msg_j.d_map.at(index);
   }
 
-  DecodeKey res(index, di);
+  DecodeKey res(index, std::move(di));
   return std::move(res);
 }
 
