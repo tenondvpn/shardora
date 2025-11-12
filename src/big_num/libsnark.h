ea0cffa7a99abbc579e6498588aea12969fdb52e
tenondvpn <jhsx5679@gmail.com>
2025-11-12 10:32:50 +0800

    fix

--- src/big_num/libsnark.h
+++ src/big_num/libsnark.h
@@ -70,7 +70,7 @@ public:
     /// @note adapted from OpenSSL's implementation.
     void cleanse()
     {
-        static std::atomic<unsigned char> s_cleanseCounter{ (uint8_t)0u };
+        static std::atomic<unsigned char> s_cleanseCounter{ (unsigned char)0u };
         uint8_t* p = (uint8_t*)begin();
         size_t const len = (uint8_t*)end() - p;
         size_t loop = len;
