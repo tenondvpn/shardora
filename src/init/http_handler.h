5ba1fac7f8217837d4808e4b3a13659daa2bdbf6
tenondvpn <jhsx5679@gmail.com>
2025-11-25 18:05:40 +0800

    fix

--- src/init/http_handler.h
+++ src/init/http_handler.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#define CPPHTTPLIB_RECV_BUFSIZ 10 * 1024 * 1024
-#define CPPHTTPLIB_SEND_BUFSIZ 10 * 1024 * 1024
+// #define CPPHTTPLIB_RECV_BUFSIZ 10 * 1024 * 1024
+// #define CPPHTTPLIB_SEND_BUFSIZ 10 * 1024 * 1024
+#define CPPHTTPLIB_RECV_BUFSIZ 104857600
 #include <httplib.h>
 
 #include "block/account_manager.h"
