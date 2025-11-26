bb5ad1bc0765911389c3a24f43ca58561b058afe
tenondvpn <jhsx5679@gmail.com>
2025-11-26 03:29:16 +0000

    fix

--- deploy/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ps -ef | grep shardora | awk -F' ' '{print $2}' | xargs kill -9
-services=("$@")
-
-for service in "${services[@]}"; do
-  sudo systemctl stop "shardora@${service}.service"
-done
