43ab1556a3236b0654f160cfb7612b74dc3832de
tenondvpn <jhsx5679@gmail.com>
2025-12-04 20:15:42 +0800

    fix

--- src/timeblock/time_block_manager.h
+++ src/timeblock/time_block_manager.h
@@ -36,7 +36,7 @@ public:
     }
 
     uint64_t LatestTimestampHeight() {
-        SHARDORA_INFO("latest_time_block_height_ get: %lu", latest_time_block_height_);
+        SHARDORA_INFO("latest_time_block_height_ get: %lu", static_cast<int>(latest_time_block_height_));
         return latest_time_block_height_;
     }
 
