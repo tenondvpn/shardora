c3a4e907b590936e71892024de108bd2f96c0a6b
tenondvpn <jhsx5679@gmail.com>
2025-12-08 20:54:50 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -1205,11 +1205,13 @@ void Hotstuff::HandleVoteMsg(const transport::MessagePtr& msg_ptr) {
     qc_item.set_elect_height(elect_height);
     qc_item.set_leader_idx(vote_msg.leader_idx());
     auto qc_hash = GetQCMsgHash(qc_item);
-    SHARDORA_DEBUG("success set view block hash: %s, qc_hash: %s, sign x: %s, replica: %d, %u_%u_%lu",
+    SHARDORA_DEBUG("success set view block hash: %s, qc_hash: %s, "
+        "sign x: %s, replica: %d, elect_height: %lu, %u_%u_%lu",
         common::Encode::HexEncode(qc_item.view_block_hash()).c_str(),
         common::Encode::HexEncode(qc_hash).c_str(),
         common::Encode::HexEncode(vote_msg.sign_x()).c_str(),
         replica_idx,
+        elect_height,
         qc_item.network_id(),
         qc_item.pool_index(),
         qc_item.view());
