5cb94a1288c780ed90f3c6e3c411ad78a342599c
tenondvpn <jhsx5679@gmail.com>
2025-12-27 19:25:26 +0800

    fix

--- src/pools/shard_statistic.cc
+++ src/pools/shard_statistic.cc
@@ -462,6 +462,7 @@ void ShardStatistic::HandleStatistic(
     std::string debug_str = ", height_node_collect_info_map height: ";
     for (auto titer = statistic_info_ptr->height_node_collect_info_map.begin(); 
             titer != statistic_info_ptr->height_node_collect_info_map.end(); ++titer) {
+        debug_str += std::to_string(titer->first) + ":";
         for (auto siter = titer->second.begin(); siter != titer->second.end(); ++siter) {
             debug_str += ", leader id: " + common::Encode::HexEncode(siter->first) + 
             ", block_gas: " + std::to_string(siter->second.gas_sum) + 
