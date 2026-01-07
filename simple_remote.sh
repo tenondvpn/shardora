b4486916c450b738ddd009aed2faadfe98ff1edb
tenondvpn <jhsx5679@gmail.com>
2026-01-07 11:44:48 +0800

    fix

--- simple_remote.sh
+++ simple_remote.sh
@@ -174,7 +174,7 @@ clear_command() {
     run_cmd_count=0
     start_pos=1
     for ip in "${node_ips_array[@]}"; do 
-        sshpass -p $PASSWORD ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip "cd /root && rm -rf pkg* && killall -9 shardora; killall -9 seth" &
+        sshpass -p $PASSWORD ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5  root@$ip "cd /root && rm -rf pkg* zjnodes nodes seths; killall -9 shardora; killall -9 seth" &
         run_cmd_count=$((run_cmd_count + 1))
         if ((start_pos==1)); then
             sleep 3
