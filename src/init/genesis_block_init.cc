69a799c0f722fc04181b51bab9ebad1ed8286770
tenondvpn <jhsx5679@gmail.com>
2026-01-11 22:33:02 +0800

    fix

--- src/init/genesis_block_init.cc
+++ src/init/genesis_block_init.cc
@@ -1799,10 +1799,18 @@ void GenesisBlockInit::InitShardGenesisAccount() {
     };
 
     if (!hasRunOnce) {
-        SHARDORA_DEBUG("now load init addr from 2 to: %u", network::kConsensusShardEndNetworkId);
-        for (uint32_t net_id = 2; net_id < network::kConsensusShardEndNetworkId; net_id++) {
-            load_addrs_func(net_id, (std::string("/root/shardora/init_accounts") + std::to_string(net_id)).c_str());
-            if (!load_addrs_func(net_id, (std::string("/root/shardora/shards") + std::to_string(net_id)).c_str())) {
+        SHARDORA_DEBUG("now load init addr from 2%u to: %u",
+            network::kConsensusShardBeginNetworkId, 
+            network::kConsensusShardEndNetworkId);
+        for (uint32_t net_id = network::kConsensusShardBeginNetworkId;
+                net_id < network::kConsensusShardEndNetworkId; net_id++) {
+            load_addrs_func(
+                net_id, 
+                (std::string("/root/shardora/init_accounts") + std::to_string(net_id)).c_str());
+            if (!load_addrs_func(
+                    net_id, 
+                    (std::string("/root/shardora/shards") + std::to_string(net_id)).c_str())) {
+                SHARDORA_DEBUG("failed load init shards%u", net_id);
                 break;
             }
         }
