f0e5cbe1eb02e48936de69755229b8bc64b0bd52
tenondvpn <jhsx5679@gmail.com>
2026-09-23 10:11:52 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -1173,8 +1173,8 @@ Status Hotstuff::HandleProposeMsgStep_Directly(
             true, 
             true, 
             balance_map,
-            latest_qc_item_ptr_,
-            shardora_host) != Status::kSuccess) {
+            shardora_host,
+            latest_qc_item_ptr_,) != Status::kSuccess) {
         SHARDORA_DEBUG("====1.1.2 Accept pool: %d, verify view block failed, "
             "view: %lu, hash: %s, qc_view: %lu, hash64: %lu",
             pool_idx_,
